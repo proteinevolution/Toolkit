a1d95322bf81010b586bc00ee498149008f82849
Seung-Zin Nam <seung-zin.nam@student.uni-tuebingen.de>
2016-03-19 21:35:30 +0100

    add fasta parser

---
+++ app/models/misc/parsers/Fasta.scala
@@ -0,0 +1,35 @@
+import scala.util.parsing.combinator._
+
+object FASTA {
+
+  case class Entry( description: String, sequence: String )
+
+  def fromFile( fn: String ): List[Entry] = {
+    val lines = scala.io.Source.fromFile(fn).getLines.mkString("\n")
+    fromString( lines )
+  }
+
+  def fromString( input: String ): List[Entry] =
+    Parser.parse(input)
+
+  private object Parser extends RegexParsers {
+
+    lazy val header = """>.*""".r ^^ { _.tail.trim }
+    lazy val seqLine = """[^>].*""".r ^^ { _.trim }
+
+    lazy val sequence = rep1( seqLine ) ^^ { _.mkString }
+
+    lazy val entry = header ~ sequence ^^ {
+      case h ~ s => Entry(h,s)
+    }
+
+    lazy val entries = rep1( entry )
+
+    def parse( input: String ): List[Entry]  = {
+      parseAll( entries, input ) match {
+        case Success( es , _ ) => es
+        case x: NoSuccess =>  throw new Exception(x.toString)
+      }
+    }
+  }
+}
