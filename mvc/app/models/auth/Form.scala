0d5495959f942bcdd047766a4e0e506cf99b2fe3
andrew <andrew.stephens@tuebingen.mpg.de>
2016-04-06 14:08:35 +0200

    added auth dropdown menu (Sign in)

---
+++ mvc/app/models/auth/Form.scala
@@ -0,0 +1,36 @@
+package models.auth
+
+import play.api.data.Form
+import play.api.data.Forms._
+
+/**
+  * Created by astephens on 03.04.16.
+  */
+
+/**
+  * Form mapping for the Sign in formular
+  */
+object SignIn {
+  val inputForm = Form(
+    tuple(
+      "username" -> email,
+      "password" -> nonEmptyText
+    )
+  )
+}
+
+/**
+  * Form mapping for the Sign up formular
+  */
+object SignUp {
+  val inputForm = Form(
+    tuple(
+      "namefirst"     -> nonEmptyText,
+      "namelast"      -> nonEmptyText,
+      "email"         -> email,
+      "password"      -> nonEmptyText,
+      "acceptagb"     -> boolean,
+      "passwordmatch" -> boolean
+    )
+  )
+}
