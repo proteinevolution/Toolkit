82b4184e41cff14efe3a87992629446bfe8b7b41
andrew <andrew.stephens@tuebingen.mpg.de>
2016-04-21 23:12:48 +0200

    prepared files for MAFFT support

---
+++ master/runscripts/mafft.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#export PATH=$PATH:%{CLUSTALW}
+
+trap 'kill $(jobs -p)' EXIT
+
+export MAFFT_BINARIES='!{BIO}/tools/mafft/binaries'
+
+!{BIO}/tools/mafft/scripts/mafft --op #{gapopen} --ep #{offset} #{alnorder} #{clustalw} #{other_advanced} #{infile} > results/result.pir
