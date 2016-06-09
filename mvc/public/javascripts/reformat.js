8f284c4d5cfd3cdd00fa70b21eb0c3b7403f092b
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-06-09 10:46:24 +0200

    reformat can do fasta -> clustal

--- mvc/public/javascripts/reformat.js
+++ mvc/public/javascripts/reformat.js
@@ -1,4 +1,8 @@
+/*
 
+REFORMAT
+
+ */
 
 
 
@@ -19,12 +23,40 @@
     }
 
 
+    function printClustalText(fastaText){
+
+
+        var splittedStrings = fastaText.split(">"),
+            result = [],
+            i = 1,
+            j = 0;
+
+
+        for (; j<Math.trunc(getClustalSeq(splittedStrings[i]).length/60)+1 ; j++){
+
+        for (; i < splittedStrings.length; i++) {
+
+                result += getClustalHeader(splittedStrings[i]);
+                result += "\t";
+                result += chunkString(getClustalSeq(splittedStrings[i]), 60)[j];
+                result += "\n";
+
+        }
+            result += "\n\n";
+            i = 1;
+
+        }
+
+        return result;
+    }
+
+
     function readFastaLine(fastaLine) {
 
         var splittedStrings  = fastaLine.split('\n'),
             result = {},
             i = 1;
-        result.name = splittedStrings[0].substr(1, splittedStrings[0].length - 1);
+        result.name = splittedStrings[0].substr(0, 11);
         result.sequence = '';
         for (; i < splittedStrings.length; i++) {
             result.sequence += splittedStrings[i];
@@ -33,15 +65,50 @@
     }
 
 
-    function fastaToClustalLine (fastaLine) {
+    function getClustalSeq (fastaLine) {
 
-        var line = readFastaLine(fastaLine);
-        
+        var fasta = readFastaLine(fastaLine),
+            result = {};
+
+
+        return fasta.sequence;
+
+    }
+
+    function getClustalHeader (fastaLine) {
+
+        var fasta = readFastaLine(fastaLine);
+        return fasta.name;
 
 
     }
 
+    function chunkString(str, len) {
+        var _size = Math.ceil(str.length/len),
+            _ret  = new Array(_size),
+            _offset
+            ;
 
+        for (var _i=0; _i<_size; _i++) {
+            _offset = _i * len;
+            _ret[_i] = str.substring(_offset, _offset + len);
+        }
+
+        return _ret;
+    }
+
+
+
+    function chunkSubstr(str, size) {
+        var numChunks = Math.ceil(str.length / size),
+            chunks = new Array(numChunks);
+
+        for(var i = 0, o = 0; i < numChunks; ++i, o += size) {
+            chunks[i] = str.substr(o, size);
+        }
+
+        return chunks;
+    }
 
 
 
@@ -50,18 +117,6 @@
 
         var output = JSON.stringify(readFastaText(source));
 
-        return output;
-    }
-
-
-
-
-    function writeFasta() {}
-
-
-    function writeClustal() {
-
-        var output = "";
         return output;
     }
 
