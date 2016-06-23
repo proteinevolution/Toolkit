4a921b5f0db592ef3a78105d6222297aceb16ab9
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-06-23 21:06:48 +0200

    Hmmer3 working again

--- tel/runscripts/hmmer3.sh
+++ tel/runscripts/hmmer3.sh
@@ -2,9 +2,23 @@
 
 trap 'kill $(jobs -p)' EXIT
 
-perl !{BIO}/helpers/reformat.pl -i=${alignment_format} -o=sto -f=%{alignment} -a=+{infile_sto}
-!{BIO}/tools/hmmer3/binaries/hmmbuild --cpu 4 +{infile_hmm} +{infile_sto}
-!{BIO}/tools/hmmer3/binaries/hmmsearch --cpu 4 -E 1e-1 --tblout @{tbl} --domtblout @{domtbl}  -o @{outfile} -A @{outfile_multi_sto} +{infile_hmm} !{DATA}/nr
+perl %BIOPROGS/helpers/reformat.pl -i=%alignment_format.content \
+                                   -o=sto \
+                                   -f=%alignment.path \
+                                   -a=temp/infile_sto
+
+%BIOPROGS/tools/hmmer3/binaries/hmmbuild --cpu 4 \
+                                         temp/infile_hmm \
+                                         temp/infile_sto 
+
+%BIOPROGS/tools/hmmer3/binaries/hmmsearch --cpu 4 \
+                                          -E 1e-1 \
+                                         --tblout results/tbl \
+                                         --domtblout results/domtbl \
+                                          -o results/outfile \
+                                          -A results/outfile_multi_sto \
+                                           temp/infile_hmm \
+                                           %DATABASES/standard/nr
 
 
 
