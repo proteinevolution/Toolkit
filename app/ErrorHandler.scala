18ef273a5f31c5ad124080b426c21f6da5408850
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-07-17 12:49:03 +0200

    don't send OK anymore when directed to 404 page, general error handling still missing

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,27 @@
+/**
+  * Created by zin on 17.07.16.
+  */
+
+
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+import javax.inject.Singleton
+
+@Singleton
+class ErrorHandler extends HttpErrorHandler {
+
+  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+    Future.successful(
+      Status(statusCode)("A client error occurred: " + message)
+    )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable) = {
+
+    Future.successful(
+      InternalServerError(views.html.errors.pagenotfound())
+    )
+  }
+}
