e2de8384e905b6d858b80a8e08b7e64ab47bd270
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-07-31 00:11:01 +0200

    abstract forwarding in order to use it for backend forwarding later

--- app/controllers/Resubmit.scala
+++ app/controllers/Resubmit.scala
@@ -1,8 +1,18 @@
 package controllers
 
+import javax.inject.{Inject}
+
+
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.mvc.Controller
+
 /**
   * Created by zin on 03.07.16.
   */
-class Resubmit {
+class Resubmit  @Inject() (webJarAssets: WebJarAssets,
+                           val messagesApi: MessagesApi) extends Controller with Forwarding {
+
+
+  val wja = webJarAssets
 
 }
