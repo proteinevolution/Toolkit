3dfc3549ec96b709c1cbfa82f3c612fb191a0c47
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-08-02 01:54:45 +0200

    provide proper logout action althoug the back link issue remains and has to be solved with javascript

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -5,7 +5,7 @@ dir=$(mktemp -d)
 echo "Building in $dir"
 cd "$dir"
 
-git clone https://github.com/Sanoma-CDA/maxmind-geoip2-scala --branch customBuild
+git clone https://github.com/Sanoma-CDA/maxmind-geoip2-scala
 cd maxmind-geoip2-scala
 sbt publish-local
 cd ..
