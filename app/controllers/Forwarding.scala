c8f1a63ea1c5d7ee4c5fa0f1a2ba40ac095ecffc
andrew <andrew.stephens@tuebingen.mpg.de>
2016-08-01 23:31:58 +0200

    Moved the Tool formulars from their subfolders to tools/forms

--- app/controllers/Forwarding.scala
+++ app/controllers/Forwarding.scala
@@ -7,7 +7,6 @@ import models.tools._
 
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Controller, Action}
-import play.twirl.api.Html
 
 
 /**
@@ -16,33 +15,29 @@ import play.twirl.api.Html
 
 
 private[controllers] trait Forwarding extends Controller with I18nSupport {
-
-
   protected def wja : WebJarAssets
 
-  def forward(toolname: String, output: String) = Action { implicit request =>
-
-
-    lazy val toolframe : Html = toolname match {
-      case "alnviz" => views.html.alnviz.form(Alnviz.inputForm)
-      case "tcoffee" => views.html.tcoffee.form(Tcoffee.inputForm)
-      case "hmmer3" => views.html.hmmer3.form(Hmmer3.inputForm)
-      case "psiblast" => views.html.psiblast.form(Psiblast.inputForm.fill((output, "", "", 1, 10, 11, 1, 200, "")))
-      case "mafft" => views.html.mafft.form(Mafft.inputForm)
-      case "csblast" => views.html.csblast.form(Csblast.inputForm)
-      case "hhpred" => views.html.hhpred.form(HHpred.inputForm)
-      case "reformatb" => views.html.reformatb.form(Reformatb.inputForm)
+  def forward(toolName: String, output: String) = Action { implicit request =>
+    val toolFrame = toolName match {
+      case "alnviz"     => views.html.tools.forms.alnviz(Alnviz.inputForm)
+      case "tcoffee"    => views.html.tools.forms.tcoffee(Tcoffee.inputForm)
+      case "hmmer3"     => views.html.tools.forms.hmmer3(Hmmer3.inputForm)
+      case "psiblast"   => views.html.tools.forms.psiblast(Psiblast.inputForm.fill((output, "", "", 1, 10, 11, 1, 200, "")))
+      case "mafft"      => views.html.tools.forms.mafft(Mafft.inputForm)
+      case "csblast"    => views.html.tools.forms.csblast(Csblast.inputForm)
+      case "hhpred"     => views.html.tools.forms.hhpred(HHpred.inputForm)
+      case "reformatb"  => views.html.tools.forms.reformatb(Reformatb.inputForm)
+      case "clans"      => views.html.tools.forms.clans(Clans.inputForm)
     }
 
-    lazy val section : String = toolname match {
+    lazy val section : String = toolName match {
       case _ => ""
     }
-
 
     lazy val session_id = Session.requestSessionID(request)
     lazy val user_o : Option[User] = Session.getUser(session_id)
 
-    Ok(views.html.main(wja, toolframe, section, user_o)).withSession {
+    Ok(views.html.main(wja, toolFrame, section, user_o)).withSession {
       Session.closeSessionRequest(request, session_id)
     }
   }
