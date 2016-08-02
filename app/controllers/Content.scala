92e17198319c73c6ed3b933aa44624e8cc05d6e5
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-08-02 02:28:48 +0200

    some security logging

--- app/controllers/Content.scala
+++ app/controllers/Content.scala
@@ -1,8 +1,17 @@
 package controllers
 
+import javax.inject.Inject
+
+import play.api.i18n.MessagesApi
+import play.api.mvc.Controller
+import play.modules.reactivemongo.{ReactiveMongoComponents, MongoController, ReactiveMongoApi}
+
 /**
   * Created by zin on 01.08.16.
   */
-class Content {
+class Content @Inject() (val messagesApi: MessagesApi,
+                         val reactiveMongoApi: ReactiveMongoApi) extends Controller with MongoController with ReactiveMongoComponents {
+
+
 
 }
