3de62a8c36e7c9af78ced4d94e41fbe9407f993c
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-08-14 21:01:09 +0200

    Work on HHblits. Tel now understands .dat file for SetParams

--- tel/runscripts/hhblits.sh
+++ tel/runscripts/hhblits.sh
@@ -11,4 +11,35 @@ perl %BIOPROGS/helpers/reformat.pl -i=%alignment_format.content \
                                    -f=%alignment.path \
                                    -a=temp/infile_a2m
 
+# Run HHblits
+
+exit
+
+
+%BIOPROGS/tools/hhsuite/bin/hhblits -cpu 2 \
+                                    -i temp/infile_a2m \
+                                    -d %hhblitsdb.content \
+                                    -psipred #{PSIPRED}/bin \
+                                    -psipred_data #{PSIPRED}/data \
+                                    -o #{@outfile}  \
+                                    -oa3m #{@a3m_outfile} \
+                                    -qhhm #{@qhhmfile    }
+                                    #{msa_factor}
+                                    -e #{@E_hhblits} \
+                                    -n #{@maxit} \
+                                    -p #{@Pmin} \
+                                    -Z #{@max_lines} \
+                                    -z 1  \
+                                    -b 1  \
+                                    -B #{@max_lines} \
+                                    -seq #{@max_seqs} \
+                                    -aliw #{@aliwidth} \
+                                    -#{@ali_mode} \
+                                    #{@realign} \
+                                    #{@mact}  \
+                                    #{@filter} \
+                                    #{@cov_min} 
+
+
+
 
