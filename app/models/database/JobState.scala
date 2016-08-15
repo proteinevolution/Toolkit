f22cbf28d7dc1bfbe9b8f562fb59ec58b93fd80b
Seung-Zin Nam <seung-zin.nam@student.uni-tuebingen.de>
2016-08-15 02:53:24 +0200

    fix hiding definition warning

--- app/models/database/JobState.scala
+++ app/models/database/JobState.scala
@@ -17,13 +17,12 @@ object JobState {
   case object Done extends JobState(5)
   case object Submitted extends JobState(6)
 
-  object JobState {
-    /**
-      * Object containing the reader for the job state
-      */
-    implicit object JobStateReader extends BSONReader[BSONInteger, JobState] {
-      def read(state: BSONInteger) = {
-        state match {
+  /**
+    * Object containing the reader for the job state
+    */
+  implicit object JobStateReader extends BSONReader[BSONInteger, JobState] {
+    def read(state: BSONInteger) = {
+      state match {
         case BSONInteger(0) => PartiallyPrepared
         case BSONInteger(1) => Prepared
         case BSONInteger(2) => Queued
@@ -31,16 +30,16 @@ object JobState {
         case BSONInteger(4) => Error
         case BSONInteger(5) => Done
         case BSONInteger(6) => Submitted
-        }
       }
     }
+  }
 
-    /**
-      * Object containing the writer for the job state
-      */
-    implicit object JobStateWriter extends BSONWriter[JobState, BSONInteger] {
-      def write(state : JobState)  = {
-        state match {
+  /**
+    * Object containing the writer for the job state
+    */
+  implicit object JobStateWriter extends BSONWriter[JobState, BSONInteger] {
+    def write(state : JobState)  = {
+      state match {
         case PartiallyPrepared => BSONInteger(0)
         case Prepared          => BSONInteger(1)
         case Queued            => BSONInteger(2)
@@ -48,7 +47,6 @@ object JobState {
         case Error             => BSONInteger(4)
         case Done              => BSONInteger(5)
         case Submitted         => BSONInteger(6)
-        }
       }
     }
   }
