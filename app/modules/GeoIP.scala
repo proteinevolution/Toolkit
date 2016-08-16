d107f745bc374e91e98eb41eb1a9be202a4bad6d
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-08-16 17:15:15 +0200

    Fix blocking JobManager. Include GeoIP Plugin sanoma

--- app/modules/GeoIP.scala
+++ app/modules/GeoIP.scala
@@ -1,16 +1,20 @@
 package modules
 
-//TODO import plugin from https://github.com/Sanoma-CDA/maxmind-geoip2-scala
+import com.sanoma.cda.geoip.{ MaxMindIpGeo, IpLocation }
+
 import scala.concurrent.duration._
-
-/**
-  * Created by zin on 02.08.16.
-  */
-
 
 final class GeoIP(file: String, cacheTtl: Duration) {
 
+  private val geoIp = MaxMindIpGeo(file, 0)
+  //private val cache = lila.memo.Builder.cache(cacheTtl, compute)
 
+  //private def compute(ip: String): Option[Location] =
+  //  geoIp getLocation ip map Location.apply
+
+  //def apply(ip: String): Option[Location] = cache get ip
+
+  //def orUnknown(ip: String): Location = apply(ip) | Location.unknown
 }
 
 case class Location(
@@ -18,10 +22,19 @@ case class Location(
                      region: Option[String],
                      city: Option[String]) {
 
+ // def comparable = (country, ~region, ~city)
 
+ // def shortCountry: String = ~country.split(',').headOption
+
+  //override def toString = List(shortCountry.some, region, city).flatten mkString " > "
 }
-
+c
 object Location {
 
+ // val unknown = Location("Solar System", none, none)
 
+  //val tor = Location("Tor exit node", none, none)
+
+ // def apply(ipLoc: IpLocation): Location =
+  //  Location(ipLoc.countryName | unknown.country, ipLoc.region, ipLoc.city)
 }
