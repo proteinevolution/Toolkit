f9fe9969c8daeb541bbf56139c9c32382a6f43b2
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-08-16 22:32:43 +0200

    JobManager writes Job to JSON file upon creation

--- app/controllers/Jobs.scala
+++ app/controllers/Jobs.scala
@@ -3,39 +3,31 @@ package controllers
 import akka.actor.ActorRef
 import javax.inject.{Inject, Named, Singleton}
 
-
+import models.database.Job
+import play.api.Logger
+import play.api.libs.json.JsError
+import play.api.libs.json._
 import play.api.mvc._
-
-
-
 @Singleton
 class Jobs @Inject()(@Named("jobManager") jobManager : ActorRef) extends Controller  {
 
 
   def updateJob = Action(BodyParsers.parse.json) { request =>
 
+    /*
+    val j = request.body.validate[Job]
 
+    j.fold(
 
-
+      errors => {
+        BadRequest(Json.obj("status" -> "OK", "message" -> JsError.toJson(errors)))
+      },
+      job => {
+        Logger.info("Job successfully received")
+        Ok
+      }
+    ) */
     Ok
   }
 }
-
-
-
-/*
-def saveBook = Action(BodyParsers.parse.json) { request =>
-    val b = request.body.validate[Book]
-    b.fold(
-      errors => {
-        BadRequest(Json.obj("status" -> "OK", "message" -> JsError.toFlatJson(errors)))
-      },
-      book => {
-        addBook(book)
-        Ok(Json.obj("status" -> "OK"))
-}
-
-
-
- */
 
