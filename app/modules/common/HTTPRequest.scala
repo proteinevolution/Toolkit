f851a3357733a3c8c0cdc6cf4591a040532e7498
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-08-18 18:49:29 +0200

    some more pattern matching if needed

--- app/modules/common/HTTPRequest.scala
+++ app/modules/common/HTTPRequest.scala
@@ -4,16 +4,22 @@ package modules.common
   * Created by zin on 16.08.16.
   */
 
+
 import play.api.http.HeaderNames
 import play.api.mvc.RequestHeader
+import scalaz._
+import Scalaz._
+
 
 object HTTPRequest {
+
+
 
   def isXhr(req: RequestHeader): Boolean =
     (req.headers get "X-Requested-With") contains "XMLHttpRequest"
 
   def isSocket(req: RequestHeader): Boolean =
-    (req.headers get HeaderNames.UPGRADE.toLowerCase) contains "websocket"
+    (req.headers get HeaderNames.UPGRADE) contains "websocket" // there might be problems with Microsoft Edge
 
   def isSynchronousHttp(req: RequestHeader) = !isXhr(req) && !isSocket(req)
 
@@ -23,9 +29,9 @@ object HTTPRequest {
 
   def userAgent(req: RequestHeader): Option[String] = req.headers get HeaderNames.USER_AGENT
 
-  //val isAndroid = UaMatcher("""(?i).*android.+mobile.*""".r)
-  //val isIOS = UaMatcher("""(?i).*(iphone|ipad|ipod).*""".r)
-  //val isMobile = UaMatcher("""(?i).*(iphone|ipad|ipod|android.+mobile).*""".r)
+  val isAndroid = UaMatcher("""(?i).*android.+mobile.*""".r)
+  val isIOS = UaMatcher("""(?i).*(iphone|ipad|ipod).*""".r)
+  val isMobile = UaMatcher("""(?i).*(iphone|ipad|ipod|android.+mobile).*""".r)
 
   private def uaContains(req: RequestHeader, str: String) = userAgent(req).exists(_ contains str)
   def isTrident(req: RequestHeader) = uaContains(req, "Trident/")
@@ -34,25 +40,30 @@ object HTTPRequest {
 
   def referer(req: RequestHeader): Option[String] = req.headers get HeaderNames.REFERER
 
-  //def lastRemoteAddress(req: RequestHeader): String =
-    //req.remoteAddress.split(", ").lastOption | req.remoteAddress
+  def lastRemoteAddress(req: RequestHeader): String =
+    req.remoteAddress.split(", ").lastOption | req.remoteAddress
 
-  //def sid(req: RequestHeader): Option[String] = req.session get LilaCookie.sessionId
+  def sid(req: RequestHeader): Option[String] = req.session get "sid"
 
-  /*val isBot = UaMatcher {
+  val isBot = UaMatcher {
     ("""(?i).*(googlebot|googlebot-mobile|googlebot-image|mediapartners-google|bingbot|slurp|java|wget|curl|commons-httpclient|python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|adidxbot|blekkobot|teoma|ia_archiver|gingercrawler|webmon|httrack|webcrawler|fast-webcrawler|fastenterprisecrawler|convera|biglotron|grub\.org|usinenouvellecrawler|antibot|netresearchserver|speedy|fluffy|jyxobot|bibnum\.bnf|findlink|exabot|gigabot|msrbot|seekbot|ngbot|panscient|yacybot|aisearchbot|ioi|ips-agent|tagoobot|mj12bot|dotbot|woriobot|yanga|buzzbot|mlbot|purebot|lingueebot|yandex\.com/bots|""" +
       """voyager|cyberpatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|ezooms|dotbot|mail\.ru|discobot|zombie\.js|heritrix|findthatfile|europarchive\.org|nerdbynature\.bot|sistrixcrawler|ahrefsbot|aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|yeti|retrevopageanalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|duckduckbot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnamgnamspider|web-archive-net\.com\.bot|backlinkcrawler|""" +
       """coccoc|integromedb|contentcrawlerspider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler\.com|siteexplorer\.info|elisabot|proximic|changedetection|blexbot|arabot|wesee:search|niki-bot|crystalsemanticsbot|rogerbot|360spider|psbot|interfaxscanbot|lipperheyseoservice|ccmetadatascaper|g00g1e\.net|grapeshotcrawler|urlappendbot|brainobot|fr-crawler|binlar|simplecrawler|simplecrawler|livelapbot|twitterbot|cxensebot|smtbot|facebookexternalhit|daumoa|sputnikimagebot|visionutils|yisouspider|parsijoobot|mediatoolkit\.com).*""").r
-  }*/
+  }
 
-  /*private def UaMatcher(regex: scala.util.matching.Regex): RequestHeader => Boolean = {
-    val pattern = regex.pattern
+
+  private def UaMatcher(regex: scala.util.matching.Regex): RequestHeader => Boolean = {
+
     req => {
-      userAgent(req) ?? { ua => pattern.matcher(ua).matches }
+      userAgent(req) match {
+        case `regex` => true
+        case _ => false
+      }
     }
-  }*/
+  } 
 
-  //def isHuman(req: RequestHeader) = !isBot(req)
+
+  def isHuman(req: RequestHeader) = !isBot(req)
 
   def isFacebookBot(req: RequestHeader) = userAgent(req) contains "facebookexternalhit"
 
