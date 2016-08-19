36028187fdb00a6cb851d7b03e142d4fe9125ba4
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-08-19 21:26:58 +0200

    TEL now properly DI. Start refactor of TEL Env Module.

--- app/controllers/Forwarding.scala
+++ app/controllers/Forwarding.scala
@@ -1,11 +1,13 @@
 package controllers
 
 
-import models.database.{Session, User}
-import models.tools._
+import javax.inject.{Inject, Singleton}
 
-import play.api.i18n.I18nSupport
-import play.api.mvc.{Controller, Action}
+import models.database.{Session, User}
+import models.tel.TEL
+import models.tools._
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, Controller}
 
 
 /**
@@ -13,20 +15,23 @@ import play.api.mvc.{Controller, Action}
   */
 
 
-private[controllers] trait Forwarding extends Controller with I18nSupport with Session {
-  protected def wja : WebJarAssets
+@Singleton
+class Forwarding @Inject() (val tel : TEL,
+                            val messagesApi: MessagesApi,
+                            webJarAssets: WebJarAssets) extends Controller with I18nSupport with Session {
+  //protected def wja : WebJarAssets
 
   def forward(toolName: String, output: String) = Action { implicit request =>
     val toolFrame = toolName match {
       case "alnviz"     => views.html.tools.forms.alnviz(Alnviz.inputForm)
       case "tcoffee"    => views.html.tools.forms.tcoffee(Tcoffee.inputForm)
-      case "hmmer3"     => views.html.tools.forms.hmmer3(Hmmer3.inputForm)
-      case "psiblast"   => views.html.tools.forms.psiblast(Psiblast.inputForm.fill((output, "", "", 1, 10, 11, 1, 200, "")))
+      case "hmmer3"     => views.html.tools.forms.hmmer3(tel, Hmmer3.inputForm)
+      case "psiblast"   => views.html.tools.forms.psiblast(tel, Psiblast.inputForm.fill((output, "", "", 1, 10, 11, 1, 200, "")))
       case "mafft"      => views.html.tools.forms.mafft(Mafft.inputForm)
-      case "csblast"    => views.html.tools.forms.csblast(Csblast.inputForm)
-      case "hhpred"     => views.html.tools.forms.hhpred(HHpred.inputForm)
+      case "csblast"    => views.html.tools.forms.csblast(tel, Csblast.inputForm)
+      case "hhpred"     => views.html.tools.forms.hhpred(tel, HHpred.inputForm)
       case "reformatb"  => views.html.tools.forms.reformatb(Reformatb.inputForm)
-      case "clans"      => views.html.tools.forms.clans(Clans.inputForm)
+      case "clans"      => views.html.tools.forms.clans(tel, Clans.inputForm)
     }
 
     lazy val section : String = toolName match {
@@ -36,7 +41,7 @@ private[controllers] trait Forwarding extends Controller with I18nSupport with S
     lazy val sessionID = requestSessionID
     lazy val user : User = getUser
 
-    Ok(views.html.main(wja, toolFrame, section, user)).withSession {
+    Ok(views.html.main(webJarAssets, toolFrame, section, user)).withSession {
       closeSessionRequest(request, sessionID)
     }
   }
