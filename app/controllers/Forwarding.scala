3d7b332cff9045980c17d85311c3e5fa710b59a1
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-08-20 15:37:23 +0200

    save some code redundancy until reflection is implemented, if ever

--- app/controllers/Forwarding.scala
+++ app/controllers/Forwarding.scala
@@ -6,6 +6,7 @@ import javax.inject.{Inject, Singleton}
 import models.database.{Session, User}
 import models.tel.TEL
 import models.tools._
+import modules.tools.ToolMatcher
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
 
@@ -16,23 +17,13 @@ import play.api.mvc.{Action, Controller}
 
 
 @Singleton
-class Forwarding @Inject() (val tel : TEL,
-                            val messagesApi: MessagesApi,
+class Forwarding @Inject() (val messagesApi: MessagesApi,
+                            val toolMatcher : ToolMatcher,
                             webJarAssets: WebJarAssets) extends Controller with I18nSupport with Session {
   //protected def wja : WebJarAssets
 
   def forward(toolName: String, output: String) = Action { implicit request =>
-    val toolFrame = toolName match {
-      case "alnviz"     => views.html.tools.forms.alnviz(Alnviz.inputForm)
-      case "tcoffee"    => views.html.tools.forms.tcoffee(Tcoffee.inputForm)
-      case "hmmer3"     => views.html.tools.forms.hmmer3(tel, Hmmer3.inputForm)
-      case "psiblast"   => views.html.tools.forms.psiblast(tel, Psiblast.inputForm.fill((output, "", "", 1, 10, 11, 1, 200, "")))
-      case "mafft"      => views.html.tools.forms.mafft(Mafft.inputForm)
-      case "csblast"    => views.html.tools.forms.csblast(tel, Csblast.inputForm)
-      case "hhpred"     => views.html.tools.forms.hhpred(tel, HHpred.inputForm)
-      case "reformatb"  => views.html.tools.forms.reformatb(Reformatb.inputForm)
-      case "clans"      => views.html.tools.forms.clans(tel, Clans.inputForm)
-    }
+    val toolFrame = toolMatcher.matcher(toolName)
 
     lazy val section : String = toolName match {
       case _ => ""
---
+++ app/modules/tools/ToolMatcher.scala
@@ -0,0 +1,37 @@
+package modules.tools
+import javax.inject.{Inject, Singleton}
+import models.tel.TEL
+import models.tools._
+import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.mvc.RequestHeader
+import play.twirl.api.Html
+
+/**
+  * Created by zin on 20.08.16.
+  */
+
+
+
+@Singleton
+class ToolMatcher @Inject()( val messagesApi: MessagesApi,
+                             val tel : TEL) extends I18nSupport {
+
+
+  def matcher(tool : String)(implicit request : RequestHeader) : Html = {
+    lazy val toolFrame = tool match {
+      case "alnviz" => views.html.tools.forms.alnviz(Alnviz.inputForm)
+      case "tcoffee" => views.html.tools.forms.tcoffee(Tcoffee.inputForm)
+      case "hmmer3" => views.html.tools.forms.hmmer3(tel, Hmmer3.inputForm)
+      case "psiblast" => views.html.tools.forms.psiblast(tel, Psiblast.inputForm.fill(("", "", "", 1, 10, 11, 1, 200, "")))
+      case "mafft" => views.html.tools.forms.mafft(Mafft.inputForm)
+      case "csblast" => views.html.tools.forms.csblast(tel, Csblast.inputForm)
+      case "hhpred" => views.html.tools.forms.hhpred(tel, HHpred.inputForm)
+      case "hhblits" => views.html.tools.forms.hhblits(tel, HHblits.inputForm)
+      case "reformatb" => views.html.tools.forms.reformatb(Reformatb.inputForm)
+      case "clans" => views.html.tools.forms.clans(tel, Clans.inputForm)
+    }
+
+    toolFrame
+
+  }
+}
