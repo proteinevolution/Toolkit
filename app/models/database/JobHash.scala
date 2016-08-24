3e8cc4724d7a4344ce536bd094e4d78a36dd5741
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-08-24 18:29:20 +0200

    remodel job hashing and take real parameters now

--- app/models/database/JobHash.scala
+++ app/models/database/JobHash.scala
@@ -5,30 +5,42 @@ import reactivemongo.bson.{BSONDocumentWriter, BSONDocument, BSONDocumentReader,
 /**
  *
  * Created by snam on 23.08.16.
+ *
+ * the JobHash also contains non-hashed information to avoid collision as much as possible. Only the input file and the parameters will be hashed
+ * due performance reasons. Note that not every tool/job uses a database.
+ *
  */
 
 
 case class JobHash(mainID : BSONObjectID,
-                   hashValue : String)
+                   inputHash : String,
+                   dbName : Option[String],
+                   dbMtime : Option[String])
 
 
 object JobHash {
 
   lazy val ID = "_id"
-  lazy val HASH = "hash"
+  lazy val INPUTHASH = "hash"
+  lazy val DBNAME = "dbname"
+  lazy val DBMTIME = "dbmtime"
 
 
   implicit object Reader extends BSONDocumentReader[JobHash] {
     override def read(bson: BSONDocument): JobHash = JobHash(
       mainID = bson.getAs[BSONObjectID](ID).getOrElse(BSONObjectID("Null")),
-      bson.getAs[String](HASH).getOrElse("No matching hash value found")
+      bson.getAs[String](INPUTHASH).getOrElse("No matching hash value found"),
+      bson.getAs[String](DBNAME),
+      bson.getAs[String](DBMTIME)
     )
   }
 
   implicit object Writer extends BSONDocumentWriter[JobHash] {
     override def write(jobHash: JobHash): BSONDocument = BSONDocument(
       ID  ->  jobHash.mainID,
-      HASH -> jobHash.hashValue
+      INPUTHASH -> jobHash.inputHash,
+      DBNAME -> jobHash.dbName,
+      DBMTIME -> jobHash.dbMtime
     )
   }
 }
