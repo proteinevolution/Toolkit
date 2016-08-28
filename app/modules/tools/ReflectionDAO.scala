32303bcd73c45f168c529cd7bd1ebf21ac57d518
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-08-28 06:41:44 +0200

    finally: we have a working reflection of the tool models!

--- app/modules/tools/ReflectionDAO.scala
+++ app/modules/tools/ReflectionDAO.scala
@@ -1,5 +1,7 @@
 package modules.tools
 
+import java.io.File
+
 import models.tools._
 import org.clapper.classutil.ClassFinder
 import reflect.runtime.universe
@@ -11,7 +13,7 @@ import reflect.runtime.universe
  * Created by snam on 27.08.16.
  */
 
-class ReflectionDAO {
+final class ReflectionDAO {
 
   // this method could be used in the Toolmatcher but unfortunately the inputForm is not inherent in the ToolModel trait
   // TODO either abstract over tuple arity and get the inputForm somehow into the trait or find another solution
@@ -31,14 +33,31 @@ class ReflectionDAO {
 
   }
 
+  /**
+   * gets methods from class or trait
+   */
 
-  // This uses the clapper library but does not work so far TODO get a dynamic list of all objects which implement ToolModel
-  def printTest() = {
+  def getMembers = {
 
-    val finder = ClassFinder()
-    val found = finder.getClasses().filter(_.isConcrete).filter(_.implements("models.tools.ToolModel"))
+    val typ = universe.typeOf[ToolModel]
+    val memberzz = typ.members
 
-    found.foreach(println)
+    typ.members.collect{
+     case m => println("Membertest: " + m)
+    }
+  }
+
+
+  // This uses the clapper library and gets a dynamic list of all objects which implement ToolModel
+  // this seems to be impossible with the current version of Scala's reflection API: http://stackoverflow.com/questions/28500804/get-all-classes-of-a-package
+
+  def findInstances = {
+
+    import org.clapper.classutil.ClassFinder
+    val classpath = List(".").map(new File(_))
+    val finder = ClassFinder(classpath)
+    val classes = finder.getClasses().filter(_.implements("models.tools.ToolModel")) // classes is an Iterator[ClassInfo]
+    classes.foreach(println)
 
   }
 
