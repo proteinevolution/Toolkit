917ebf539475501b0912ed3efe5e0246833b0003
lukas <luk.zim91@gmail.com>
2016-08-28 15:15:19 +0200

    TEl ENV module now reads environment files from the tel/env directory. Hot Reload is not yet supported

--- app/models/tel/Observer.scala
+++ app/models/tel/Observer.scala
@@ -4,16 +4,22 @@ package models.tel
   * Stuff for the observer pattern
   *
   */
-
 trait Observer[S] {
   def receiveUpdate(subject: S)
+  def receiveInitial(subject : S)
 }
 
+
+// TODO Add support for deleting Observers
 trait Subject[S] {
   this: S =>
   private var observers: List[Observer[S]] = Nil
 
-  def addObserver(observer: Observer[S]) = observers = observer :: observers
+  def addObserver(observer: Observer[S]) = {
+
+    observers = observer :: observers
+    observer.receiveInitial(this)
+  }
 
   def notifyObservers() = observers.foreach(_.receiveUpdate(this))
 }
--- app/modules/TELModule.scala
+++ app/modules/TELModule.scala
@@ -1,7 +1,11 @@
 package modules
 
+import javax.inject.{Inject, Provider}
+
 import com.google.inject.AbstractModule
-import models.tel.env.{Env, TELEnv}
+import models.tel.env.{Env, ExecFile, PropFile, TELEnv}
+import play.api.{Configuration, Logger}
+
 
 /**
   * Created by lukas on 8/28/16.
@@ -11,6 +15,29 @@ class TELModule  extends AbstractModule {
 
   override def configure() = {
 
-        bind(classOf[Env]).to(classOf[TELEnv])
+        bind(classOf[Env]).toProvider(classOf[TELModuleProvider])
+  }
+}
+
+class TELModuleProvider @Inject() (tv : TELEnv, configuration: Configuration) extends  Provider[TELEnv] {
+
+  override def get() = {
+
+
+    import better.files._
+    // Try loading the environment files from the configured directory
+    configuration.getString("tel.env").getOrElse {
+
+      Logger.warn("Key 'tel.env' was not found in configuration. Fall back to 'tel/env'") ; "tel/env"
+    }.toFile.list.foreach { file =>
+
+      file.extension match {
+
+        case Some(".prop") => new PropFile(file.pathAsString).addObserver(tv)
+        case Some(".sh") => new ExecFile(file.pathAsString).addObserver(tv)
+        case _ => //
+      }
+    }
+    tv
   }
 }
