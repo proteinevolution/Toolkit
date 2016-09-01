d6add47ce0d5a945c60b1eea2071d1c365f8ef6e
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-09-01 02:32:16 +0200

    fix package name

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -65,10 +65,10 @@ class Application @Inject()(webJarAssets     : WebJarAssets,
   def index = Action.async { implicit request =>
 
 
-    //toolMirror.invokeToolName("alnviz") // even though this is a runtime mirror, it seems fast enough
-    //toolMirror.listToolModels() // faster than clapper
+    toolMirror.invokeToolName("alnviz") // even though this is a runtime mirror, it seems fast enough
+    toolMirror.listToolModels() // faster than clapper
     println(ToolModel.values) // even faster than the scala api
-    //toolMirror.findInstances() // finds all tool instances in the models package but this seems to be rather slow: alternatives are macros or sealed trait enumeration
+    toolMirror.findInstances() // finds all tool instances in the models package but this seems to be rather slow: alternatives are macros or sealed trait enumeration
     getUser(request, userCollection, userCache).map { user =>
       Ok(views.html.main(webJarAssets, views.html.general.maincontent(), "Home", user))
         .withSession(sessionCookie(request, user.sessionID.get))
