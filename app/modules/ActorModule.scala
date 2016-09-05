38c60aaddc86918de2ccca9daf3d9753c24ea74d
andrew <andrew.stephens@tuebingen.mpg.de>
2016-09-05 21:55:55 +0200

    added ES Manager and basic auto complete action (server side)

--- app/modules/ActorModule.scala
+++ app/modules/ActorModule.scala
@@ -1,6 +1,6 @@
 package modules
 
-import actors.{UserManager, JobManager}
+import actors.{ESManager, UserManager, JobManager}
 import com.google.inject.AbstractModule
 import play.api.libs.concurrent.AkkaGuiceSupport
 
@@ -11,5 +11,6 @@ class ActorModule extends AbstractModule with AkkaGuiceSupport {
   def configure = {
     bindActor[JobManager]("jobManager") // Has information about the jobs
     bindActor[UserManager]("userManager") // Has information about the users
+    bindActor[ESManager]("esManager") // Has information about the Elastic search actor
   }
 }
