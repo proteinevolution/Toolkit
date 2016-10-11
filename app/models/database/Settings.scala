337f8509d443ac06eebd2e980c52c1d0db7f628c
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-10-11 15:22:36 +0200

    Remove some unused files

--- app/models/database/Settings.scala
+++
@@ -1,8 +0,0 @@
-package models.database
-
-/**
-  * Created by zin on 31.07.16.
-  */
-class Settings {
-
-}
--- app/models/database/ToolStats.scala
+++
@@ -1,8 +0,0 @@
-package models.database
-
-/**
- * Created by snam on 21.09.16.
- */
-class ToolStats {
-
-}
