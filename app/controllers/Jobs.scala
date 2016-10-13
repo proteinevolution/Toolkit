216682850d6b23804ce945fd635eb039815f1e86
davidmrau <david.rau@student.uni-tuebingen.de>
2016-10-13 16:17:46 +0200

    fixed Jobs Controller

--- app/controllers/Jobs.scala
+++ app/controllers/Jobs.scala
@@ -1,24 +1,24 @@
 package controllers
 
 import akka.actor.ActorRef
+import akka.pattern._
 import javax.inject.{Inject, Named, Singleton}
 
 import actors.JobManager
+import actors.JobManager.UpdateJobStatus
 import models.database.Job
 import play.api.Logger
 import play.api.libs.json.JsError
 import play.api.libs.json._
 import play.api.mvc._
+import reactivemongo.bson.BSONObjectID
 @Singleton
 class Jobs @Inject()(@Named("jobManager") jobManager : ActorRef) extends Controller  {
 
-  <!-- @ Lukas please do not delete this Controller-->
-
-
+  
   def updateJobStatus(jobID : String) = Action { request =>
-    JobManager.UpdateJobStatus(jobID)
-    Ok(jobID)
+    jobManager ! UpdateJobStatus(BSONObjectID(jobID))
+    Ok
   }
 
 }
-
