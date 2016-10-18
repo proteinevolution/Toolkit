6e330eee9d519284a8eae49d5689e910c21b837a
davidmrau <david.rau@student.uni-tuebingen.de>
2016-10-18 17:01:51 +0200

    fixed msaview for psiblast, gets now clu format as input

--- tel/runscripts/psiblast.sh
+++ tel/runscripts/psiblast.sh
@@ -20,10 +20,14 @@ ID=$(<JOB.json jq '._id'| awk '{ print $2; }'| sed 's/"//g'| tr -d '\n')
 
 perl %BIOPROGS/helpers/blastviz.pl results/out.psiblastp blastviz results files/$ID >> logs/blastviz.log
 
-
 # extract alignment from
 perl %BIOPROGS/helpers/alignhits_html.pl results/out.psiblastp results/out.align -e %evalue.content -fas -no_link -blastplus
 
+# reformat alignment to clustal
+perl %BIOPROGS/helpers/reformat.pl -i=fas \
+                                   -o=clu \
+                                   -f=results/out.align \
+                                   -a=results/out.align_clu
 
 #scala %BIOPROGS/helpers/psiblastpPostProcess.scala results/out.psiblastp
 
