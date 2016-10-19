b3536dec95d6bab964c986b4358fbca77db10bd0
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-10-19 22:45:40 +0200

    write first result model for psiblast following convention over configuration (evalues are embedded via twirl now)

--- app/models/results/PsiBlast.scala
+++ app/models/results/PsiBlast.scala
@@ -1,8 +1,36 @@
 package models.results
 
 /**
+ *
  * Created by snam on 18.10.16.
+ *
+ * Tool model for parsing result files
+ *
  */
-class PsiBlast {
+
+
+import models.Constants
+import scala.io.Source
+
+
+
+object PsiBlast extends Constants {
+
+
+
+  def evalues(mainID:String) = {
+
+    // Convention over configuration! No need to configure the result files in some models or to pass the job object to the view
+
+    val path = s"$jobPath$mainID/results/evalues.dat"
+
+    val lines = Source.fromFile(path).getLines().toList
+
+    lines
+
+  }
+
+
+
 
 }
