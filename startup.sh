7c21827bb04b462d89b6fd69e46b46d78ea45cfd
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-11-01 22:05:46 +0100

    Some more work on the PSI-BLAST Hit Page

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-JAVA_OPTS="-Xms1024m -Xmx3084m -XX:MaxPermSize=1024m -XX:MaxMetaspaceSize=1024m" /ebio/abt1/lzimmermann/progs/activator-dist-1.3.12/bin/activator "run 6111"
+JAVA_OPTS="-Xms2048m -Xmx3084m -XX:MaxPermSize=2048m -XX:MaxMetaspaceSize=2048m" /ebio/abt1/lzimmermann/progs/activator-dist-1.3.12/bin/activator "run 6111"
