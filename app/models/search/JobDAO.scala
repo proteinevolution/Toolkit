e8dcdcb690067c3f0e31bdafb0c9b40a4d97f9b5
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2016-11-17 19:40:56 +0100

    hack to use the termsuggester as a completionsuggester for now

--- app/models/search/JobDAO.scala
+++ app/models/search/JobDAO.scala
@@ -4,6 +4,7 @@ import javax.inject.{Named, Inject}
 import com.sksamuel.elastic4s.{SuggestMode, IndexAndType, ElasticDsl}
 import com.evojam.play.elastic4s.configuration.ClusterSetup
 import com.evojam.play.elastic4s.{PlayElasticFactory, PlayElasticJsonSupport}
+import jdk.internal.org.objectweb.asm.tree.analysis.Analyzer
 import org.elasticsearch.common.unit.Fuzziness
 
 class JobDAO @Inject()(cs: ClusterSetup, elasticFactory: PlayElasticFactory, @Named("jobs") indexAndType: IndexAndType)
@@ -54,25 +55,23 @@ class JobDAO @Inject()(cs: ClusterSetup, elasticFactory: PlayElasticFactory, @Na
   def jobIDtermSuggester(queryString : String) = { // this is a spelling correction mechanism
    client.execute {
       search in "tkplay_dev"->"jobs" suggestions {
-        termSuggestion("jobID") field "jobID" text queryString mode SuggestMode.Always
+        termSuggestion("jobID") field "jobID" text queryString size 1
       }
     }
 
   }
 
 
-
-  def jobIDcompletionSuggester(queryString : String) = { // this is a auto-completion mechanism
+  def jobIDcompletionSuggester(queryString : String) = { // this is an auto-completion mechanism
     client.execute {
       search in "tkplay_dev"->"jobs" suggestions {
-        completionSuggestion("job-completion") field "jobID" text queryString size 10
+        completionSuggestion("jobs") field "jobID" context "jobs" text queryString size 10
       }
     }
   }
 
 
-  // tries to find a matching Job with the Job ID
-  def fuzzySearchJobID(queryString : String) = {
+  def fuzzySearchJobID(queryString : String) = { // similarity search with Levensthein edit distance
     client.execute {
       search in "tkplay_dev"->"jobs" query {
         fuzzyQuery("jobID", queryString).fuzziness(Fuzziness.AUTO).prefixLength(4).maxExpansions(10)
