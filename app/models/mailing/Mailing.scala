ec3aa194801bb86001da80b648b0f27e4dead2b6
andrew <andrew.stephens@tuebingen.mpg.de>
2016-12-01 19:11:45 +0100

    eMail is sent to the provided address when registering

--- app/models/mailing/Mailing.scala
+++ app/models/mailing/Mailing.scala
@@ -1,20 +1,42 @@
 package models.mailing
 
 import models.database.User
+import play.api.libs.mailer.{MailerClient, Email}
 
 /**
   * Created by astephens on 24.05.16.
   */
+
+
+
+
 /**
-  * Template trait to the Mailing
+  * Template trait to the Mail object
   */
 sealed trait MailTemplate {
+  // User to whom the eMail should be sent
+  val user : User
   // Subject of the Email
   def subject = "Generic Information"
+
   // Text version of the Email
-  def bodyText (user : User) : String
+  val bodyText : String
+
   // HTML version of the Email
-  def bodyHtml (user : User) : String
+  val bodyHtml : String
+
+  // Send the email
+  def send(implicit mailerClient : MailerClient) {
+    val email = Email(
+      subject,
+      "Toolkit Team <toolkitmpg@gmail.com>",
+      Seq(user.getUserData.nameLogin + " <" + user.getUserData.eMail + ">"),
+      attachments = Seq(),
+      bodyText = Some(this.bodyText), // Text version of the E-Mail in case the User has no HTML E-Mail client
+      bodyHtml = Some(this.bodyHtml)  // HTML formatted E-Mail content
+    )
+    mailerClient.send(email)
+  }
 
   def bodyHtmlTemplate(subject : String, content : String) =
       s"""
@@ -27,10 +49,10 @@ sealed trait MailTemplate {
     """.stripMargin
 }
 
-final case class NewUserWelcomeMail(token : String) extends MailTemplate {
-  override def subject = "Bioinformatics Toolkit - Verification"
+case class NewUserWelcomeMail(user : User, token : String) extends MailTemplate {
+  override def subject = "Bioinformatics Toolkit - Account Verification"
 
-  def bodyText (user : User) = {
+  val bodyText = {
     s"""Welcome ${user.getUserData.nameLogin},
        |Your Registration was successful. Please take a moment and verify that this is indeed your E-Mail account.
        |To do this, visit
@@ -39,7 +61,7 @@ final case class NewUserWelcomeMail(token : String) extends MailTemplate {
      """.stripMargin
   }
 
-  def bodyHtml(user : User) = {
+  val bodyHtml = {
     super.bodyHtmlTemplate(
       s"""Welcome ${user.getUserData.nameLogin},""".stripMargin,
       s"""Your Registration was successful. Please take a moment and verify that this is indeed your E-Mail account.
