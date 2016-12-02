fe9328f09f4ff55696aad49651c07da3d45b6c77
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-12-02 13:54:59 +0100

    JobActor is now FSM

--- app/actors/JobActor.scala
+++ app/actors/JobActor.scala
@@ -1,8 +1,9 @@
 package actors
 
-import akka.actor.{Actor, Props}
-import akka.event.LoggingReceive
-import play.api.Logger
+import actors.JobActor.{Initialized, JobEvent}
+import akka.actor.{FSM, Props}
+import models.database._
+
 
 /**
   * Created by lzimmermann on 02.12.16.
@@ -11,25 +12,56 @@ import play.api.Logger
 
 object JobActor {
 
-  case object Delete
+  // Job Events that might occur
+  sealed trait JobEvent
+  case object Delete extends JobEvent
+  case object Initialized extends JobEvent
+
 
   def props(jobID : String, ownerUserID: String) = Props(new JobActor(jobID, ownerUserID))
 }
 
 
-class JobActor(val jobID: String, val ownerUserID: String) extends Actor {
-  import JobActor._
-
+class JobActor(val jobID: String, val ownerUserID: String) extends FSM[JobState, JobEvent] {
 
   // Set of sessionIDs of all users that are subscribed to this Job
   private var subscribers = Set(ownerUserID)
+  startWith(Submitted, Initialized)
 
 
-  def receive = LoggingReceive {
 
-    case Delete =>
 
-      Logger.info(s"JobID: $jobID to be deleted")
 
-  }
+
+
+
+  initialize()
 }
+
+
+
+/*
+    class Buncher extends FSM[State, Data] {
+
+      startWith(Idle, Uninitialized)
+
+      when(Idle) {
+        case Event(SetTarget(ref), Uninitialized) =>
+          stay using Todo(ref, Vector.empty)
+      }
+
+      // transition elided ...
+
+      when(Active, stateTimeout = 1 second) {
+        case Event(Flush | StateTimeout, t: Todo) =>
+          goto(Idle) using t.copy(queue = Vector.empty)
+      }
+
+      // unhandled elided ...
+
+      initialize()
+    }
+
+
+
+ */
