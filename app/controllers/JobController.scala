887809fc50bcbe8f62e7500f4ba7af5fd306e4be
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-12-02 13:30:21 +0100

    Add JobActor, JobController, JobIDProvider, and routes for some preliminary stuff to remove JobManager Singleton

---
+++ app/controllers/JobController.scala
@@ -0,0 +1,52 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+
+import actors.JobActor
+import actors.JobActor.Delete
+import akka.actor.ActorSystem
+import models.job.JobIDProvider
+import play.api.Logger
+import play.api.cache.CacheApi
+import play.api.mvc.{Action, AnyContent, Controller}
+import play.modules.reactivemongo.ReactiveMongoApi
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+/**
+  * Created by lzimmermann on 02.12.16.
+  */
+@Singleton
+class JobController @Inject() (jobIDProvider: JobIDProvider,
+                               actorSystem : ActorSystem,
+                               implicit val userCache : CacheApi,
+                               val reactiveMongoApi: ReactiveMongoApi)
+  extends Controller with UserSessions {
+
+  /**
+    * Creates a new Job by instanciating a corresponding job actor
+    *
+    */
+  def create(jobIDoption : Option[String]):Action[AnyContent] = Action.async { implicit request =>
+
+    if(jobIDoption.isDefined && !jobIDProvider.isAvailable(jobIDoption.get)) {
+
+      Future.successful(BadRequest)
+    }
+    val jobID = jobIDoption.getOrElse(jobIDProvider.provide)
+
+    getUser.map { user =>
+
+          Logger.info(s"JobID: $jobID -- Spawn JobActor")
+          val jobActor = actorSystem.actorOf(JobActor.props(jobID, user.userID.stringify), jobID)
+          Ok
+        }
+    }
+
+  def delete(jobID : String) = Action {
+
+    actorSystem.actorSelection(s"user/$jobID") ! Delete
+    Ok
+  }
+}
