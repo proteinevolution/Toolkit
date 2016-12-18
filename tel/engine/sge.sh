4670434be0eabafb2999312b1633e560638fa96a
lukas <lukas.zimmermann@tuebingen.mpg.de>
2016-12-18 19:24:02 +0100

    [FEATURE] JobActor can delete running jobs.
    Also provide _temporary_ fix for strange click handler binding
    in submission modal. (The click handlers are bound for each job submission,
    which does not make sense)

--- tel/engine/sge.sh
+++ tel/engine/sge.sh
@@ -1,4 +1,9 @@
 qsub -sync n \
      -l h_vmem=128G \
      -cwd  \
-     %r | grep -oE "[0-9]+" >> jobIDCluster
+     %r | grep -oE "[0-9]+" > jobIDCluster
+
+# Write the file to delete the execution. This is necessary for the Toolkit being able to delete the job from the gridengine
+echo "#!/bin/bash" > delete.sh
+echo "qdel $(cat jobIDCluster)" >> delete.sh
+chmod u+x delete.sh
