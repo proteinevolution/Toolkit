2a9cc1aa8d8e2126b6c793be8cf7f173da372891
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-01-12 12:41:10 +0100

    [REFACTOR] Restore TEL ENV in Version control.
    However, you must specify the the directory name with
    tel.env in your application conf. It has been removed from tel.conf

---
+++ tel/env/environment.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# JAVA
+##########################################################
+export JAVA_HOME=/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/jdk1.8.0_112
+export JRE_HOME=/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/jdk1.8.0_112/jre
+
+
+# PERL
+##########################################################
+export PERL5LIB=/ebio/abt1_share/toolkit_support1/code/bioprogs/lib
+
+# HHLIB
+export HHLIB=/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/hh-suite-build
+
+# PATH variable 
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/Python-3.5.2/bin:${PATH}" # Python binary
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/hh-suite_misc_scripts:$PATH" # helper scripts from the old Toolkit
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/hh-suite-build/scripts:${PATH}" # HHSCRIPTS
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/hh-suite-build/bin:${PATH}" # HHBINS
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/psipred:/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/psipred/bin:${PATH}" # PSIPRED
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/mmseqs2/build/bin:${PATH}" # MMSEQS
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/dssp:${PATH}" # DSSP
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/tcoffee/bin:${PATH}" # T-COFFEE
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/backtranslate:${PATH}" # Backtranslate
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/helpers:${PATH}" # Helpers
+export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/ncbi-blast-2.5.0+/bin:${PATH}" # NCBI-BLAST
+
+# PYTHONPATH FOR PDBX
+export PYTHONPATH=/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/pdbx
+
