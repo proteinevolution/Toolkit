212f28637a05d456a669fcce0409f4b374dc0a04
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-01-15 14:35:27 +0100

    [ADD] Fullscreen handle
    Result pages now can use onCollapse, onExpand, and
    onFullscreenToggle to handle the event of making the
    JobTabs to Fullscreen. See e.g. T-Coffee's AlignmentView
    Output

--- app/models/results/Tcoffee.scala
+++ app/models/results/Tcoffee.scala
@@ -27,10 +27,6 @@ object Tcoffee extends Constants {
   /* returns the clustal output to be embedded in biojs msa via twirl */
 
   def alnviz(mainID: String) : Iterator[String] = {
-
-    val outfile = s"$jobPath$mainID/results/alignment.clustalw_aln"
-
-    Source.fromFile(outfile).getLines()
+    Source.fromFile(s"$jobPath$mainID/results/alignment.clustalw_aln").getLines()
   }
 }
-
