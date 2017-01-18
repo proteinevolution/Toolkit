6bd111a89e4a480fb952cdb5468175c898fe6ecd
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-01-18 20:16:35 +0100

    [ADD] Implement first version of GLProbs

--- tel/runscripts/glprobs.sh
+++ tel/runscripts/glprobs.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+glprobs %alignment.path \
+        -o ../results/alignment.clustalw_aln \
+        -clustalw \
 
-trap 'kill $(jobs -p)' EXIT
 
-N_THREADS =1
-
-%BIOPROGS/tools/GLProbs/glprobs \
-                                %sequences.path \
-                                -o results/glprobs_aln \
-                                -num_threads 1
