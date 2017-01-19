b3f8a1571b54166f11fe97ec6d598a0c0690567e
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-01-19 19:16:32 +0100

    [UPDATE] Update environment

--- tel/env/environment.sh
+++ tel/env/environment.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 
+BIOPROGS="/ebio/abt1_share/toolkit_support1/code/bioprogs"
+DATABASES="/ebio/abt1_share/toolkit_sync/databases"
+
+
+
+# Databases
+export STANDARDNEW="${DATABASES}/standard/NewToolkitDBs"
+export TAXONOMY="${DATABASES}/standard/taxonomy"
+
 # JAVA
 ##########################################################
 export JAVA_HOME=/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/jdk1.8.0_112
@@ -15,18 +24,37 @@ export PERL5LIB=/ebio/abt1_share/toolkit_support1/code/bioprogs/lib
 export HHLIB=/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/hh-suite-build
 
 # PATH variable 
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/Python-3.5.2/bin:${PATH}" # Python binary
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/hh-suite_misc_scripts:$PATH" # helper scripts from the old Toolkit
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/hh-suite-build/scripts:${PATH}" # HHSCRIPTS
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/hh-suite-build/bin:${PATH}" # HHBINS
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/psipred:/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/psipred/bin:${PATH}" # PSIPRED
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/mmseqs2/build/bin:${PATH}" # MMSEQS
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/dssp:${PATH}" # DSSP
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/tcoffee/bin:${PATH}" # T-COFFEE
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/backtranslate:${PATH}" # Backtranslate
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/helpers:${PATH}" # Helpers
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/ncbi-blast-2.5.0+/bin:${PATH}" # NCBI-BLAST
-export PATH="/ebio/abt1_share/toolkit_support1/code/bioprogs/tools/ancescon:${PATH}" # ANCESCON
+export PATH="${BIOPROGS}/dependencies/Python-3.5.2/bin:${PATH}" # Python binary
+export PATH="${BIOPROGS}/dependencies/hh-suite_misc_scripts:$PATH" # helper scripts from the old Toolkit
+export PATH="${BIOPROGS}/tools/hh-suite-build/scripts:${PATH}" # HHSCRIPTS
+export PATH="${BIOPROGS}/tools/hh-suite-build/bin:${PATH}" # HHBINS
+export PATH="${BIOPROGS}/dependencies/psipred:/ebio/abt1_share/toolkit_support1/code/bioprogs/dependencies/psipred/bin:${PATH}" # PSIPRED
+export PATH="${BIOPROGS}/tools/mmseqs2/build/bin:${PATH}" # MMSEQS
+export PATH="${BIOPROGS}/dependencies/dssp:${PATH}" # DSSP
+export PATH="${BIOPROGS}/tools/tcoffee/bin:${PATH}" # T-COFFEE
+export PATH="${BIOPROGS}/tools/backtranslate:${PATH}" # Backtranslate
+export PATH="${BIOPROGS}/helpers:${PATH}" # Helpers
+export PATH="${BIOPROGS}/tools/ncbi-blast-2.5.0+/bin:${PATH}" # NCBI-BLAST
+export PATH="${BIOPROGS}/tools/ancescon:${PATH}" # ANCESCON
+export PATH="${BIOPROGS}/tools/clustalo/bin:${PATH}" #CLustalOmega
+export PATH="${BIOPROGS}/tools/glprobs:${PATH}" # GLProbs
+export PATH="${BIOPROGS}/tools/msaprobs:${PATH}" # MSAProbs
+export PATH="${BIOPROGS}/tools/muscle:${PATH}" # MUSCLE
+export PATH="${BIOPROGS}/tools/tprpred:${PATH}" # TPRPRED
+export PATH="${BIOPROGS}/tools/kalign:${PATH}" # KAlign
+export PATH="${BIOPROGS}/tools/mafft2/scripts:${PATH}"
+export PATH="${BIOPROGS}/tools/aln2plot:${PATH}"
+
+
+
+# Setup Blammer
+export BLAMMERJAR="${BIOPROGS}/tools/blammer/blammer.jar"
+export BLAMMERCONF="${BIOPROGS}/tools/blammer/blammer.conf"
+export HMMERBINARIES="${BIOPROGS}/tools/hmmer3/binaries"
+
+
+# Setup Mafft
+export MAFFT_BINARIES="${BIOPROGS}/tools/mafft2/binaries"
 
 
 # PYTHONPATH FOR PDBX
