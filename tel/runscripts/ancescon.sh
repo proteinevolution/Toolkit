60a7ba7778c35288ac9b51f8d7f7cd9e4047aaae
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-01-19 19:15:28 +0100

    [REFACTOR] Move to reformat.pl version from the hh-suite3

--- tel/runscripts/ancescon.sh
+++ tel/runscripts/ancescon.sh
@@ -1,4 +1,4 @@
-reformat.pl -i=fas -o=clu -f=%alignment.path -a=../results/alignment.clu
+reformat.pl fas clu %alignment.path "$(readlink -f ../results/alignment.clu)"
 
 
 # Remove CLUSTAL text in alignment.clu
