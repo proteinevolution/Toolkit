c049c4dfbd9dab492e66501c1422d6159f07dbb9
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-01-26 18:02:35 +0100

    [Refactor] HHR for hhpred loaded from the database

---
+++ app/controllers/DataController.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import javax.inject.Inject
+
+import modules.CommonModule
+import play.api.Logger
+import play.api.mvc._
+import play.modules.reactivemongo.ReactiveMongoApi
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+/**
+  * Created by lzimmermann on 26.01.17.
+  */
+class DataController  @Inject() (val reactiveMongoApi: ReactiveMongoApi)
+  extends Controller with CommonModule {
+
+  /** Check whether the user is allowed to fetch the data for the particular job and retrieves the data with
+    * stored given a particular key
+   */
+  def get(jobID: String, key: String) = Action.async {
+    Logger.info("STARTING GET")
+
+    getResult(jobID, key).map {
+      case Some(jsValue) =>
+
+        Logger.info("FINISHING GET")
+        Ok(jsValue)
+      case None => NotFound
+    }
+  }
+}
