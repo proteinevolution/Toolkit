cd1bbdfb47cc866e97827394f6fada46fd44a032
andrew <andrew.stephens@tuebingen.mpg.de>
2017-01-27 18:04:35 +0100

    deletion action will mark the job in the DB as deleted

--- app/controllers/JobController.scala
+++ app/controllers/JobController.scala
@@ -7,10 +7,11 @@ import actors.JobActor.{Delete, RunscriptData}
 import actors.Master.{CreateJob, JobMessage}
 import akka.actor.{ActorRef, ActorSystem}
 import models.Values
-import models.database.Job
+import models.database.{User, JobDeletionFlag, JobDeletion, Job}
 import models.job.JobIDProvider
 import models.search.JobDAO
 import modules.{CommonModule, LocationProvider}
+import org.joda.time.DateTime
 import play.api.Logger
 import play.api.cache.{CacheApi, NamedCache}
 import play.api.libs.json.Json
@@ -182,14 +183,93 @@ final class JobController @Inject() (jobIDProvider                              
   }
 
 
-  // TODO  Ensure that user is allowed to delete Job
-  // TODO introduce jobActor Cache
-  // TODO Set the correct delete flag in the database
-  def delete(jobID : String ) = Action {
 
+  /**
+    * Marks a Job for deletion
+    * TODO introduce jobActor Cache
+    *
+    * @return
+    */
+  def delete(jobID : String) =  Action.async { implicit request =>
     Logger.info("Delete Action in JobController reached")
+    getUser.flatMap { user =>
+      findJob(BSONDocument(Job.JOBID -> jobID)).map {
+        case Some(job) =>
+          Logger.info("Found Jobs for deletion: " + job.jobID)
+          // Check if the User owns the job
+          if (job.ownerID.contains(user.userID)) {
+            // Tell the master to delete the job
+            master ! JobMessage(jobID, Delete)
+            // Mark the job in mongoDB
+            updateJobs(BSONDocument(Job.IDDB      -> job.mainID),
+                       BSONDocument("$set"        ->
+                       BSONDocument(Job.DELETION  -> JobDeletion(JobDeletionFlag.OwnerRequest, Some(DateTime.now()))),
+                       BSONDocument("$unset"      ->
+                       BSONDocument(Job.WATCHLIST -> ""))))
+          } else {
+            // Mark public job as deleteable
+            /*
+            if(job.ownerID.isEmpty) {
+              updateJobs(BSONDocument(Job.IDDB -> job.mainID),
+                         BSONDocument("$set" ->
+                         BSONDocument(Job.DELETION -> JobDeletion(JobDeletionFlag.PublicRequest, Some(DateTime.now())))))
+            }*/
+            // Clear job which is not owned by the User
+            updateJobs(BSONDocument(Job.IDDB -> job.mainID),
+                       BSONDocument("$pull"  ->
+                       BSONDocument(Job.WATCHLIST -> user.userID)))
+          }
+          Ok
+        case None =>
+          NotFound
+      }
+    }
+  }
 
-    master ! JobMessage(jobID, Delete)
-    Ok
+  /**
+    * Marks multiple Jobs for deletion
+    * TODO introduce jobActor Cache
+    *
+    * @return
+    */
+  def deleteMulti() =  Action.async { implicit request =>
+    Logger.info("DeleteMulti Action in JobController reached")
+    getUser.map { user =>
+      // evaluate all jobIDs from the ids list
+      request.getQueryString("ids").map { str =>
+        val jobIDs = str.split(",").toList
+        jobIDs.foreach{ jobID =>
+          master ! JobMessage(jobID, Delete)
+        }
+        findJobs(BSONDocument(Job.JOBID -> BSONDocument("$in" -> jobIDs))).map { jobs =>
+          Logger.info("Found Jobs: " + jobs)
+          // filter jobs which are not owned by the user
+          val ownershipPatition = jobs.partition(_.ownerID.contains(user.userID))
+          // Remove owned jobs
+          val ownedJobs = ownershipPatition._1.map(_.mainID)
+          Logger.info("Found jobs Owned by the User: " + ownedJobs)
+          updateJobs(BSONDocument(Job.IDDB -> BSONDocument("$in" ->ownedJobs)),
+                     BSONDocument("$set"   ->
+                     BSONDocument(Job.DELETION  -> JobDeletion(JobDeletionFlag.OwnerRequest, Some(DateTime.now()))),
+                     BSONDocument("$unset" ->
+                     BSONDocument(Job.WATCHLIST -> ""))))
+          // Mark public jobs as deleteable
+          /*
+          val publicJobs = ownershipPatition._2.filter(_.ownerID.isEmpty).map(_.mainID)
+          Logger.info("Found public jobs: " + publicJobs)
+          updateJobs(BSONDocument(Job.IDDB -> BSONDocument("$in" -> publicJobs)),
+                     BSONDocument("$set"   ->
+                     BSONDocument(Job.DELETION -> JobDeletion(JobDeletionFlag.PublicRequest, Some(DateTime.now())))))
+          */
+          // Clear jobs which are not owned by the User
+          val otherJobs =  ownershipPatition._2.map(_.mainID)
+          Logger.info("Found jobs which should only be cleared: " + otherJobs)
+          updateJobs(BSONDocument(Job.IDDB -> BSONDocument("$in" -> otherJobs)),
+                     BSONDocument("$pull"  ->
+                     BSONDocument(Job.WATCHLIST -> user.userID)))
+        }
+      }
+      Ok("started")
+    }
   }
 }
