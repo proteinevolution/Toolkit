ffcbc80f153903d2006f8b6536fde670ba431d90
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-01-29 16:44:57 +0100

    retrieve session username not by RESTful query but via session cookie

--- app/controllers/UserSessions.scala
+++ app/controllers/UserSessions.scala
@@ -18,7 +18,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * Created by astephens on 24.08.16.
   */
 trait UserSessions extends CommonModule {
-  private final val SID = "sid"
+  private val SID = "sid"
+  private val USERNAME = "username"
 
   implicit val userCache : CacheApi
   implicit val locationProvider: LocationProvider
@@ -101,7 +102,7 @@ trait UserSessions extends CommonModule {
   /**
     * Handles cookie creation
     */
-  def sessionCookie(implicit request : RequestHeader, sessionID : BSONObjectID): mvc.Session = {
-    request.session + (SID -> sessionID.stringify)
+  def sessionCookie(implicit request : RequestHeader, sessionID : BSONObjectID, userName: Option[String]): mvc.Session = {
+    request.session + (SID -> sessionID.stringify) + (USERNAME -> userName.getOrElse(""))
   }
 }
