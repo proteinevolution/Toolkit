531f473833360337a2efb0bf7b81b0d0c357510a
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-02-02 00:04:18 +0100

    add global filters, but not yet a csrf filter because tokens need to be passed to the tel scripts

---
+++ app/Filters.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by snam on 01.02.17.
+  */
+
+import javax.inject.Inject
+import play.filters.csrf.CSRFFilter
+import play.api.http.DefaultHttpFilters
+import play.filters.gzip.GzipFilter
+
+class Filters @Inject() (
+                          gzip: GzipFilter,
+                          cSRFFilter: CSRFFilter
+                        ) extends DefaultHttpFilters(gzip)
