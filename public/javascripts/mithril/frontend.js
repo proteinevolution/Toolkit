9a7cc1d8db92a425d79f2dfa55d74c912faf3680
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-02-03 13:00:31 +0100

    migration step mithril 0.2 -> 1.0: move mithril related coffeescripts as javascripts into the public folder in order to use automatic code migration scripts

---
+++ public/javascripts/mithril/frontend.js
@@ -0,0 +1,257 @@
+var GeneralTabComponent, exampleSequence, fndt, renderTabs, tabsContents, tabulated;
+
+exampleSequence = ">gi|33300828|ref|NP_877456#7 putative ATP-dependent DNA ligase [Bacteriophage phiKMV]\nPEITVDGRIVGYVMGKTG-KNVGRVVGYRVELEDGSTVAATGLSEEHIQLLTCAYLNAHI\nD---EAMPNYGRIVEVSAMERSAN-TLRHPSFSRFR\n>gi|114796395|emb|CAK25951#9 putative ATP-dependent DNA ligase [Bacteriophage LKD16]\nPSLAVEGIVVGFVMGKTG-ANVGKVVGYRVDLEDGTIVSATGLTRDRIEMLTTEAELLGG\nA-DHPGMADLGRVVEVTAMERSAN-TLRHPKFSRFR\n>gi|114796457|emb|CAK24995#5 putative DNA ligase [Bacteriophage LKA1]   E=4e-40 s/c=1.7\nPGFEADGTVIDYVWGDPDKANANKIVGFRVRLEDGAEVNATGLTQDQMACYTQSYHATAY\nEVGITQTIYIGRACRVSGMERTKDGSIRHPHFDGFR\n>gi|29366706|ref|NP_813751#8 putative DNA ligase [Pseudomonas phage gh-1]   gi|29243565\nPDDNEDGFIQDVIWGTKGLANEGKVIGFKVLLESGHVVNACKISRALMDEFTDTETRLPG\n-------YYKGHTAKVTFMERYPDGSLRHPSFDSFR\n>gi|68299729|ref|YP_249578#6 DNA ligase [Vibriophage VP4]   gi|66473268|gb|AAY46277.1|\nPEGEIDGTVVGVNWGTVGLANEGKVIGFQVLLENGVVVDANGITQEQMEEYTNLVYKTGH\nD-----DCFNGRPVQVKYMEKTPKGSLRHPSFQRWR\n>gi|77118174|ref|YP_338096#3 ligase [Enterobacteria phage K1F]   gi|72527918|gb|AAZ7297\nPSEEADGHVVRPVWGTEGLANEGMVIGFDVMLENGMEVSATNISRALMSEFTENVKSDP-\n------DYYKGWACQITYMEETPDGSLRHPSFDQWR\n>gi|17570796|ref|NP_523305#4 DNA ligase [Bacteriophage T3]   gi|118769|sp|P07717|DNLI_B\nPECEADGIIQGVNWGTEGLANEGKVIGFSVLLETGRLVDANNISRALMDEFTSNVKAHGE\nD------FYNGWACQVNYMEATPDGSLRHPSFEKFR\n>gi|119637753|ref|YP_91898#2 DNA ligase [Yersinia phage Berlin]   gi|119391784|emb|CAJ\nPECEADGIIQSVNWGTPGLSNEGLVIGFNVLLETGRHVAANNISQTLMEELTANAKEHGE\nD------YYNGWACQVAYMEETSDGSLRHPSFVMFR";
+
+window.FrontendAlnvizComponent = {
+    controller: function() {
+        var submitted;
+        ({
+            pasteExample: function() {
+                return $('#alignment').val(exampleSequence);
+            }
+        });
+        submitted = false;
+        return {
+            frontendSubmit: function() {
+                if (!submitted) {
+                    return $.ajax({
+                        url: '/api/frontendSubmit/Alnviz',
+                        type: 'POST',
+                        success: function(result) {
+                            console.log('ok');
+                            submitted = true;
+                        },
+                        error: function(result) {
+                            console.warn('error');
+                            submitted = true;
+                        }
+                    });
+                } else {
+
+                }
+            },
+            initMSA: function() {
+                var alignment, defMenu, menuOpts, opts, seqs, width;
+                seqs = $('#alignment').reformat('Fasta');
+                width = $('#tool-tabs').width() - 180;
+                if (!seqs) {
+                    return;
+                }
+                opts = {
+                    colorscheme: {
+                        "scheme": "mae"
+                    },
+                    el: document.getElementById('yourDiv'),
+                    vis: {
+                        conserv: false,
+                        overviewbox: false,
+                        seqlogo: false,
+                        labels: false,
+                        labelName: true,
+                        labelId: false,
+                        labelPartition: false,
+                        labelCheckbox: false
+                    },
+                    menu: 'small'
+                };
+                opts.seqs = fasta2json(seqs);
+                opts.zoomer = {
+                    alignmentHeight: 600,
+                    alignmentWidth: width,
+                    labelNameLength: 165,
+                    labelWidth: 85,
+                    labelFontsize: "13px",
+                    labelIdLength: 75,
+                    menuFontsize: "12px",
+                    menuMarginLeft: "2px",
+                    menuPadding: "0px 10px 0px 0px",
+                    menuItemFontsize: "14px",
+                    menuItemLineHeight: "14px",
+                    autoResize: true
+                };
+                opts.zoomer = {
+                    alignmentWidth: width
+                };
+                alignment = new msa.msa(opts);
+                menuOpts = {};
+                menuOpts.el = document.getElementById('menuDiv');
+                menuOpts.msa = alignment;
+                defMenu = new msa.menu.defaultmenu(menuOpts);
+                alignment.addView('menu', defMenu);
+                alignment.render();
+                return $('#tool-tabs').tabs('option', 'active', $('#tool-tabs').tabs('option', 'active') + 1);
+            },
+            forwardTab: function() {
+                return $('#tool-tabs').tabs('option', 'active', $('#tool-tabs').tabs('option', 'active') + 1);
+            }
+        };
+    },
+    view: function(ctrl) {
+        return m("div", {
+            id: "jobview"
+        }, [
+            m("div", {
+                "class": "jobline"
+            }, m("span", {
+                "class": "toolname"
+            }, "Alignment Viewer")), m(GeneralTabComponent, {
+                tabs: ["Alignment", "Visualization"],
+                ctrl: ctrl
+            })
+        ]);
+    }
+};
+
+tabulated = function(element, isInit) {
+    if (!isInit) {
+        return $(element).tabs();
+    }
+};
+
+fndt = function(elem, isInit) {
+    if (!isInit) {
+        return $(elem).foundation();
+    }
+};
+
+window.FrontendReformatComponent = {
+    controller: function() {
+        return {
+            html: m.request({
+                method: "GET",
+                url: "/static/get/reformat",
+                deserialize: function(data) {
+                    return data;
+                }
+            })
+        };
+    },
+    view: function(ctrl) {
+        return m("div", {
+            id: "jobview",
+            config: fndt
+        }, m.trust(ctrl.html()));
+    }
+};
+
+window.FrontendRetrieveSeqComponent = {
+    model: function(args) {
+        return {
+            tabs: ["Input"],
+            content: [
+                m("div", {
+                    "class": "parameter-panel"
+                }, [m("textarea")])
+            ]
+        };
+    },
+    controller: function(args) {
+        return this.model = new ParameterAlignmentComponent.model(args);
+    },
+    view: function(ctrl) {
+        return renderTabs(ctrl.model.tabs, ctrl.model.content);
+    }
+};
+
+
+/*
+
+ $(document).foundation()
+ $("html, body").animate({ scrollTop: 0 }, "fast")
+
+ window.FrontendReformatComponent =
+ controller: ->
+
+ view: (ctrl) ->
+ m "div", {id: "jobview"}, [
+
+ m "div", {class: "jobline"},
+ m "span", {class: "toolname"}, "Reformat"
+
+ m GeneralTabComponent, {tabs: ["Alignment", "Freqs"], ctrl: ctrl}
+ ]
+ */
+
+renderTabs = function(tabs, content) {
+    return m("div", {
+        "class": "tool-tabs",
+        id: "tool-tabs",
+        config: tabulated
+    }, [
+        m("ul", tabs.map(function(tab) {
+            return m("li", {
+                id: "tab-" + tab
+            }, m("a", {
+                href: "#tabpanel-" + tab
+            }, tab));
+        })), tabs.map(function(tab, idx) {
+            return m("div", {
+                "class": "tabs-panel",
+                id: "tabpanel-" + tab
+            }, content[idx]);
+        })
+    ]);
+};
+
+GeneralTabComponent = {
+    controller: function() {},
+    view: function(ctrl, args) {
+        return m("div", {
+            "class": "tool-tabs",
+            id: "tool-tabs",
+            config: tabulated
+        }, [
+            m("ul", args.tabs.map(function(tab) {
+                return m("li", {
+                    id: "tab-" + tab
+                }, m("a", {
+                    href: "#tabpanel-" + tab
+                }, tab));
+            })), args.tabs.map(function(tab) {
+                return m("div", {
+                    "class": "tabs-panel",
+                    id: "tabpanel-" + tab
+                }, tabsContents[tab](args.ctrl));
+            })
+        ]);
+    }
+};
+
+
+/*
+ m "div", {class: "tabs-panel", id: "tabpanel-#{paramGroup[0]}"}, [
+ */
+
+tabsContents = {
+    "Alignment": function(ctrl) {
+        return m("div", {
+            "class": "parameter-panel"
+        }, [
+            m(ParameterAlignmentComponent, {
+                options: [["fas", "FASTA"]],
+                value: ""
+            }), m("div", {
+                "class": "submitbuttons",
+                onclick: ctrl.frontendSubmit
+            }, m("input", {
+                type: "button",
+                "class": "success button small submitJob",
+                value: "View Alignment",
+                onclick: ctrl.initMSA,
+                id: "viewAlignment"
+            }))
+        ]);
+    },
+    "Visualization": function(ctrl) {
+        return m("div", [
+            m("div", {
+                id: "menuDiv"
+            }), m("div", {
+                id: "yourDiv"
+            })
+        ]);
+    },
+    "Freqs": function(ctrl) {
+        return "Test";
+    }
+};
