c08f95c2eb10ace1156549de18037d59beae6ef1
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-02-08 21:28:17 +0100

    [ADD] Additions to the environment

--- tel/runscripts/hhpred_manual.sh
+++ tel/runscripts/hhpred_manual.sh
@@ -4,8 +4,21 @@
 cp ../../%parentid.content/results/hhsearch.hhr ../params
 cp ../../%parentid.content/results/query.a3m ../params
 
+mkdir -p ../results/cif
 
-checkTemplates.pl -i   ../params/hhsearch.hhr \
-                  -q   ../params/query.a3m \
-                  -pir ../results/tomodel.pir \
-                  -m   %templates.content  >   ../results/results.out
+checkTemplates2.pl -i   ../params/hhsearch.hhr \
+                   -pir ../results/tomodel.pir \
+                   -cif %CIF \
+                   -q   $(readlink -f ../params/query.a3m) \
+                   -o   $(readlink -f ../results/cif) \
+                   -m   %templates.content  >   ../results/results.out
+
+#checkTemplates.pl -i   ../params/hhsearch.hhr \
+#                  -q   ../params/query.a3m \
+#                  -pir ../results/tomodel.pir \
+#                  -m   %templates.content  >   ../results/results.out
+
+# Remove line which reveals a path from the result
+#sed -i '/create/d' $(readlink -f ../results/results.out)
+
+
