5eb9c73a703dead09c5879eb08863d949be25558
lukas <lukas.zimmermann@tuebingen.mpg.de>
2017-02-09 16:36:56 +0100

    [REFACTOR] hhhpred_manual now uses stable version of checkTemplates.pl

--- tel/runscripts/hhpred_manual.sh
+++ tel/runscripts/hhpred_manual.sh
@@ -6,19 +6,13 @@ cp ../../%parentid.content/results/query.a3m ../params
 
 mkdir -p ../results/cif
 
-checkTemplates2.pl -i   ../params/hhsearch.hhr \
-                   -pir ../results/tomodel.pir \
-                   -cif %CIF \
-                   -q   $(readlink -f ../params/query.a3m) \
-                   -o   $(readlink -f ../results/cif) \
-                   -m   %templates.content  >   ../results/results.out
-
-#checkTemplates.pl -i   ../params/hhsearch.hhr \
-#                  -q   ../params/query.a3m \
-#                  -pir ../results/tomodel.pir \
-#                  -m   %templates.content  >   ../results/results.out
+checkTemplates.pl -i   ../params/hhsearch.hhr \
+                  -pir ../results/tomodel.pir \
+                  -cif %CIF \
+                  -o   $(readlink -f ../results/cif) \
+                  -m   %templates.content  >   ../results/results.out
 
 # Remove line which reveals a path from the result
-#sed -i '/create/d' $(readlink -f ../results/results.out)
+sed -i '/create/d' $(readlink -f ../results/results.out)
 
 
