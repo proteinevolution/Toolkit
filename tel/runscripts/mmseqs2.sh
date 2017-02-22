2366432b74905a89827e93ee43114689237b27f7
Vikram Alva <vikram.alva@tuebingen.mpg.de>
2017-02-22 17:24:17 +0100

    First implementation of MMseqs2

---
+++ tel/runscripts/mmseqs2.sh
@@ -0,0 +1,35 @@
+
+#convert input sequences into the MMseqs database format
+mmseqs createdb %alignment.path \
+                ../results/seqDB \
+                --max-seq-len 30000
+
+#create temp directory
+mkdir ../results/tmp
+
+#Let the results of clustering be written in the results directory
+#cd ../results
+
+#clustering step
+mmseqs cluster  ../results/seqDB \
+                ../results/clu \
+                ../results/tmp \
+                --min-seq-id %min_seqid.content \
+                -c %min_aln_cov.content \
+                --remove-tmp-files > ../results/output
+
+
+#Generate FASTA-style output
+mmseqs createseqfiledb ../results/seqDB \
+                       ../results/clu \
+                       ../results/clu_seq \
+
+mmseqs result2flat     ../results/seqDB \
+                       ../results/seqDB \
+                       ../results/clu_seq \
+                        ../results/clu_seq.fa
+
+
+filtermmseqs.pl -i ../results/clu_seq.fa \
+                -o ../results/output
+
