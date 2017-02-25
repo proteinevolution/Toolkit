f7a3cc6fff51daf64369d3723e36f0a05685a737
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-02-25 23:35:18 +0100

    [WIP] start to work on CLANS

--- tel/runscripts/clans.sh
+++ tel/runscripts/clans.sh
@@ -1,17 +1,13 @@
-#!/bin/bash
-
-trap 'kill $(jobs -p)' EXIT
-
-%BIOPROGS/tools/ncbi-blast-2.3.0+/bin/psiblast -db %STANDARD/%standarddb.content \
-                                            -matrix %matrix.content \
-                                            -num_iterations %num_iter.content \
-                                            -evalue %evalue.content \
-                                            -num_threads 4 \
-                                            -in_msa %alignment.path \
-                                            -out results/out.psiblastp \
-                                            -outfmt 0 \
-                                            -html\
-                                            -out_pssm results/out.ksf
+psiblast -db %STANDARD/%standarddb.content \
+         -matrix %matrix.content \
+         -num_iterations %num_iter.content \
+         -evalue %evalue.content \
+         -num_threads 4 \
+         -in_msa %alignment.path \
+         -out ..results/out.psiblastp \
+         -outfmt 0 \
+         -html\
+         -out_pssm ..results/out.ksf
 
 
 
