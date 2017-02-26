7b2b28235dedab067f76eb5294da55d483a983ee
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-02-26 02:17:36 +0100

    [FEATURE] allow for dynamic h_vmem allocation from the tools.conf

--- app/modules/tel/env/TELEnv.scala
+++ app/modules/tel/env/TELEnv.scala
@@ -17,14 +17,18 @@ class TELEnv extends Env with Observer[EnvFile]    {
 
   override def get(key : String): String = this.env(key)
 
-  override def configure(key: String, value: String): Unit = {
+  override def configure(key: String, value: String) : Unit = {
     this.env = this.env + (key -> value)
   }
 
+  override def remove(key: String) : Unit = {
+    this.env -= key
+  }
 
-  override def receiveInitial(subject: EnvFile) = receiveUpdate(subject)
 
-  override def receiveUpdate(subject: EnvFile): Unit = {
+  override def receiveInitial(subject: EnvFile) : Unit = receiveUpdate(subject)
+
+  override def receiveUpdate(subject: EnvFile) : Unit = {
 
     // If the Environmental file triggers a change, reload it and add new variables to the
     // env
