75f183a711c87affff06902f15d68351aa326c03
Vikram Alva <vikram.alva@tuebingen.mpg.de>
2017-03-04 18:04:42 +0100

    [NEW]Allow searching with user MSAs with 0 iterations of HHblits

--- public/javascripts/mithril/jobmodel.js
+++ public/javascripts/mithril/jobmodel.js
@@ -24,6 +24,7 @@ window.JobModel = {
         "seq_count": 1000,
         "codon_table": "1",
         "genetic_code": "1",
+        "msa_gen_max_iter": "1",
         "grammar": "Prosite_grammar"
     },
     pushMessage: function(msg) {
