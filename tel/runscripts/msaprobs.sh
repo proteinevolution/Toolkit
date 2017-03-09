65e0f41f55309c366a64d3f03199056cd36e37ea
Vikram Alva <vikram.alva@gmail.com>
2017-03-09 18:24:49 +0100

    [WIP] Finalized MSAProbs. Output pages need to formatted

--- tel/runscripts/msaprobs.sh
+++ tel/runscripts/msaprobs.sh
@@ -1,5 +1,5 @@
 msaprobs %alignment.path \
-        -o ../results/alignment.clustalw_aln \
-        -clustalw \
+        -o ../results/alignment.fas \
 
 
+reformat_hhsuite.pl fas clu ../results/alignment.fas ../results/alignment.clustalw_aln
