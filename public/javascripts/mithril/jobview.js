86fce7949b61b01dc9bab084a52af3a3337ed89c
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-14 00:44:46 +0100

    [FIX] reenable submit button when input is valid

--- public/javascripts/mithril/jobview.js
+++ public/javascripts/mithril/jobview.js
@@ -1,4 +1,4 @@
-var JobErrorComponent, jobNoteArea, JobValidationComponent, JobRunningComponent, JobLineComponent, JobQueuedComponent, JobSubmissionComponent, JobTabsComponent, ParameterBoolComponent, ParameterNumberComponent, ParameterRadioComponent, ParameterSelectComponent, ParameterSlideComponent, SearchformComponent, alignmentUpload, closeShortcut, exampleSequence, formComponents, foundationConfig, helpModalAccess, mapParam, renderParameter, selectBoxAccess, submitModal, tabulated ;
+var JobErrorComponent, jobNoteArea, JobValidationComponent, ParameterAlignmentComponent, JobRunningComponent, JobLineComponent, JobQueuedComponent, JobSubmissionComponent, JobTabsComponent, ParameterBoolComponent, ParameterNumberComponent, ParameterRadioComponent, ParameterSelectComponent, ParameterSlideComponent, SearchformComponent, alignmentUpload, closeShortcut, exampleSequence, formComponents, foundationConfig, helpModalAccess, mapParam, renderParameter, selectBoxAccess, submitModal, tabulated ;
 
 exampleSequence = ">NP_877456#7 putative ATP-dependent DNA ligase [Bacteriophage phiKMV]\nPEITVDGRIVGYVMGKTG-KNVGRVVGYRVELEDGSTVAATGLSEE\n>CAK25951#9 putative ATP-dependent DNA ligase [Bacteriophage LKD16]\nPSLAVEGIVVGFVMGKTG-ANVGKVVGYRVDLEDGTIVSATGLTRD\n>CAK24995#5 putative DNA ligase [Bacteriophage LKA1]   E=4e-40 s/c=1.7\nPGFEADGTVIDYVWGDPDKANANKIVGFRVRLEDGAEVNATGLTQD\n>NP_813751#8 putative DNA ligase [Pseudomonas phage gh-1]   gi|29243565\nPDDNEDGFIQDVIWGTKGLANEGKVIGFKVLLESGHVVNACKISRA\n>YP_249578#6 DNA ligase [Vibriophage VP4]   gi|66473268|gb|AAY46277.1|\nPEGEIDGTVVGVNWGTVGLANEGKVIGFQVLLENGVVVDANGITQE\n>YP_338096#3 ligase [Enterobacteria phage K1F]   gi|72527918|gb|AAZ7297\nPSEEADGHVVRPVWGTEGLANEGMVIGFDVMLENGMEVSATNISRA\n>NP_523305#4 DNA ligase [Bacteriophage T3]   gi|118769|sp|P07717|DNLI_B\nPECEADGIIQGVNWGTEGLANEGKVIGFSVLLETGRLVDANNISRA\n>YP_91898#2 DNA ligase [Yersinia phage Berlin]   gi|119391784|emb|CAJ\nPECEADGIIQSVNWGTPGLSNEGLVIGFNVLLETGRHVAANNISQT";
 
@@ -787,7 +787,7 @@ window.ParameterAlignmentComponent = {
                         $("#" + ctrl.id).val(exampleSequence);
                         $('.submitJob').prop('disabled', false);
                         $("#validOrNot").removeClass("alert warning primary secondary")
-                        $("#validOrNot").css("display", "block").html("Found format: " + $("#" + ctrl.id).reformat('detect')).addClass("success");
+                        $("#validOrNot").css("display", "block").html("Found format: <b>" + $("#" + ctrl.id).reformat('detect') + "</b>").addClass("success");
                         changed = false; // resets changed validation filter
                     }
                 }), m("input", {
