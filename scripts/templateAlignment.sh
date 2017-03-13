986b43615918b21079c3500f149039ca2e5b5e59
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-13 11:03:05 +0100

    [FIX] add # for pfam accession

--- scripts/templateAlignment.sh
+++ scripts/templateAlignment.sh
@@ -7,7 +7,7 @@ then
     #successively try to fetch a3m from databases
     ffindex_get ${scopdir}scope_a3m.ffdata ${scopdir}scope_a3m.ffindex $accession >> results/$accession.a3m
     ffindex_get ${mmcifdir}mmcif70_a3m.ffdata ${mmcifdir}mmcif70_a3m.ffindex $accession >> results/$accession.a3m
-    ffindex_get ${pfamdir}pfama_a3m.ffdata ${pfamdir}pfama_a3m.ffindex $accession >> results/$accession.a3m
+    ffindex_get ${pfamdir}PfamA_a3m.ffdata ${pfamdir}PfamA_a3m.ffindex "#$accession" >> results/$accession.a3m
     hhfilter -i results/$accession.a3m -o results/$accession.template.reduced.a3m -diff 100
     reformat.pl a3m fas results/$accession.template.reduced.a3m results/$accession.template.fas
 fi
