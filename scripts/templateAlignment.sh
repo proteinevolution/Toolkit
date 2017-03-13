dedb17db207f03295417a8ff737f810c183a7168
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-13 17:46:42 +0100

    [REFACTOR] using env to source dbs

--- scripts/templateAlignment.sh
+++ scripts/templateAlignment.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # Set environment
 
-source /ebio/abt1_share/toolkit_support1/code/bioprogs/env/environment_server.sh
+source /ebio/abt1_share/toolkit_support1/code/bioprogs/env/environment.sh
+
+scopdir=${DBROOT}/scope/scope
+pfamdir=${DBROOT}/pfama/pfama
+mmcifdir=${DBROOT}/mmcif70/mmcif70
+
+
 if [ ! -e "results/$accession.template.fas" ]
 then
     #successively try to fetch a3m from databases
-    ffindex_get ${scopdir}scope_a3m.ffdata ${scopdir}scope_a3m.ffindex $accession >> results/$accession.a3m
-    ffindex_get ${mmcifdir}mmcif70_a3m.ffdata ${mmcifdir}mmcif70_a3m.ffindex $accession >> results/$accession.a3m
-    ffindex_get ${pfamdir}PfamA_a3m.ffdata ${pfamdir}PfamA_a3m.ffindex "#$accession" >> results/$accession.a3m
+    ffindex_get ${scopdir}_a3m.ffdata ${scopdir}_a3m.ffindex $accession >> results/$accession.a3m
+    ffindex_get ${mmcifdir}_a3m.ffdata ${mmcifdir}_a3m.ffindex $accession >> results/$accession.a3m
+    ffindex_get ${pfamdir}_a3m.ffdata ${pfamdir}_a3m.ffindex "#$accession" >> results/$accession.a3m
     hhfilter -i results/$accession.a3m -o results/$accession.template.reduced.a3m -diff 100
     reformat.pl a3m fas results/$accession.template.reduced.a3m results/$accession.template.fas
 fi
