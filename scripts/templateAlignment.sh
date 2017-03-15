21bd207f60743a0820d5f236c14fe3b6ba7f4072
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-15 15:08:58 +0100

    [FIX] 4. Show template alignment for pairwise comparisons

--- scripts/templateAlignment.sh
+++ scripts/templateAlignment.sh
@@ -6,6 +6,10 @@ HHSUITE=$DATABASES/hh-suite
 scopdir=${HHSUITE}/scope/scope
 pfamdir=${HHSUITE}/pfama/pfama
 mmcifdir=${HHSUITE}/mmcif70/mmcif70
+pfamdir=${HHSUITE}/pfama/pfama
+mmcifdir=${HHSUITE}/mmcif70/mmcif70
+saccharomyces=${HHSUITE}/Proteomes/Saccharomyces_cerevisiae_S288c/Saccharomyces_cerevisiae_S288c
+escherichia_coli=${HHSUITE}/Proteomes/Escherichia_coli_K12/Escherichia_coli_K12
 
 
 if [ ! -e "results/$accession.template.fas" ]
@@ -13,7 +17,9 @@ then
     #successively try to fetch a3m from databases
     ffindex_get ${scopdir}_a3m.ffdata ${scopdir}_a3m.ffindex $accession >> results/$accession.a3m
     ffindex_get ${mmcifdir}_a3m.ffdata ${mmcifdir}_a3m.ffindex $accession >> results/$accession.a3m
-    ffindex_get ${pfamdir}_a3m.ffdata ${pfamdir}_a3m.ffindex "#$accession" >> results/$accession.a3m
+    ffindex_get ${pfamdir}_a3m.ffdata ${pfamdir}_a3m.ffindex "$accession.a3m" >> results/$accession.a3m
+    ffindex_get ${escherichia_coli}_a3m.ffdata ${escherichia_coli}_a3m.ffindex "$accession" >> results/$accession.a3m
+    ffindex_get ${saccharomyces}_a3m.ffdata ${saccharomyces}_a3m.ffindex "$accession" >> results/$accession.a3m
     hhfilter -i results/$accession.a3m -o results/$accession.template.reduced.a3m -diff 100
     reformat.pl a3m fas results/$accession.template.reduced.a3m results/$accession.template.fas
 fi
