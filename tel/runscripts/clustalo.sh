917f981fe24fae775942cf1a7ca88ccb8337115d
Vikram Alva <vikram.alva@gmail.com>
2017-03-16 16:01:36 +0100

    [FIX] Updated params and output view of Clustal Omega

--- tel/runscripts/clustalo.sh
+++ tel/runscripts/clustalo.sh
@@ -1,8 +1,22 @@
-clustalo   -i %alignment.path \
+if [ %output_order.content == "input"] ; then
+
+    clustalo   -i %alignment.path \
            -o ../results/alignment.clustalw_aln \
            --outfmt=clustal \
            -v \
            --force \
-           --threads=1
+           --output-order=input-order \
+           --threads=%THREADS
+else
+    clustalo   -i %alignment.path \
+           -o ../results/alignment.clustalw_aln \
+           --outfmt=clustal \
+           -v \
+           --force \
+           --output-order=tree-order \
+           --threads=%THREADS
+fi
 
-
+reformat_hhsuite.pl clu fas ../results/alignment.clustalw_aln ../results/alignment.fas
+# Convert fasta to JSON
+fasta2json.py ../results/alignment.fas ../results/alignment.json
