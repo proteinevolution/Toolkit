a3683532fbf76847c6feaeb1c5b8f237b49e5f98
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-16 19:21:10 +0100

    minify HTML

--- app/Filters.scala
+++ app/Filters.scala
@@ -3,11 +3,23 @@
   */
 
 import javax.inject.Inject
+import com.mohiva.play.htmlcompressor.HTMLCompressorFilter
+import com.mohiva.play.xmlcompressor.XMLCompressorFilter
 import play.filters.csrf.CSRFFilter
-import play.api.http.DefaultHttpFilters
+import play.api.http.HttpFilters
+import play.api.mvc.EssentialFilter
 import play.filters.gzip.GzipFilter
 
 final class Filters @Inject() (
                           gzip: GzipFilter,
+                          htmlCompressorFilter: HTMLCompressorFilter,
+                          xmlCompressorFilter: XMLCompressorFilter,
                           cSRFFilter: CSRFFilter
-                        ) extends DefaultHttpFilters(gzip)
+                        ) extends HttpFilters {
+
+  override def filters: Seq[EssentialFilter] = Seq(
+    htmlCompressorFilter,
+    xmlCompressorFilter
+  )
+
+}
