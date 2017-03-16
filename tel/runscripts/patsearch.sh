8577a2c70793993c2b431de5f545042813a85b89
Vikram Alva <vikram.alva@gmail.com>
2017-03-16 17:03:22 +0100

    PatternSearch works again; fixed a bug

--- tel/runscripts/patsearch.sh
+++ tel/runscripts/patsearch.sh
@@ -1,5 +1,5 @@
 patsearch.pl        -i  %alignment.path \
                     -d %STANDARD/%standarddb.content \
                     -o ../results/output.fas \
-                    -sc %seq_count.content \
+                    -sc %seqcount.content \
                     -%grammar.content > report_patsearch
