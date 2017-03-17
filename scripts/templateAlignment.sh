6a454833c7cf4f31d671d7a1579ea0193af3d32f
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-17 21:34:00 +0100

    [FIX] included accessions again

--- scripts/templateAlignment.sh
+++ scripts/templateAlignment.sh
@@ -12,16 +12,16 @@ saccharomyces=${HHSUITE}/Proteomes/Saccharomyces_cerevisiae_S288c/Saccharomyces_
 escherichia_coli=${HHSUITE}/Proteomes/Escherichia_coli_K12/Escherichia_coli_K12
 
 
-if [ ! -e "results/$accession.template.fas" ]
+if [ ! -e "results/$number.fas" ]
 then
     #successively try to fetch a3m from databases
-    ffindex_get ${scopdir}_a3m.ffdata ${scopdir}_a3m.ffindex $accession >> results/$accession.a3m
-    ffindex_get ${mmcifdir}_a3m.ffdata ${mmcifdir}_a3m.ffindex $accession >> results/$accession.a3m
-    ffindex_get ${pfamdir}_a3m.ffdata ${pfamdir}_a3m.ffindex "$accession.a3m" >> results/$accession.a3m
-    ffindex_get ${escherichia_coli}_a3m.ffdata ${escherichia_coli}_a3m.ffindex "$accession" >> results/$accession.a3m
-    ffindex_get ${escherichia_coli}_a3m.ffdata ${escherichia_coli}_a3m.ffindex "$accession" >> results/$accession.a3m
-    ffindex_get results/db_a3m.ffdata results/db_a3m.ffindex "db.a3m" >> results/"$accession".a3m
-    ffindex_get ${saccharomyces}_a3m.ffdata ${saccharomyces}_a3m.ffindex "$accession" >> results/$accession.a3m
-    hhfilter -i results/$accession.a3m -o results/$accession.reduced.a3m -diff 100
-    reformat.pl a3m fas results/$accession.reduced.a3m results/$accession.fas
+    ffindex_get ${scopdir}_a3m.ffdata ${scopdir}_a3m.ffindex $accession >> results/$number.a3m
+    ffindex_get ${mmcifdir}_a3m.ffdata ${mmcifdir}_a3m.ffindex $accession >> results/$number.a3m
+    ffindex_get ${pfamdir}_a3m.ffdata ${pfamdir}_a3m.ffindex "$accession.a3m" >> results/$number.a3m
+    ffindex_get ${escherichia_coli}_a3m.ffdata ${escherichia_coli}_a3m.ffindex "$accession" >> results/$number.a3m
+    ffindex_get ${escherichia_coli}_a3m.ffdata ${escherichia_coli}_a3m.ffindex "$accession" >> results/$number.a3m
+    ffindex_get results/db_a3m.ffdata results/db_a3m.ffindex "db.a3m" >> results/$number.a3m
+    ffindex_get ${saccharomyces}_a3m.ffdata ${saccharomyces}_a3m.ffindex "$accession" >> results/$number.a3m
+    hhfilter -i results/$number.a3m -o results/$number.reduced.a3m -diff 100
+    reformat.pl a3m fas results/$number.reduced.a3m results/$number.fas
 fi
