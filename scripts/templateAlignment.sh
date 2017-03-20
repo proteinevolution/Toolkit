2a4b4664ec2296c8f753861d8a80df2eab0dbbdf
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-20 11:11:14 +0100

    improved fetching of template alignments, writing selected dbs into file. Problem remaining: if accession contains #

--- scripts/templateAlignment.sh
+++ scripts/templateAlignment.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 # Set environment
-
 source /ebio/abt1_share/toolkit_support1/code/bioprogs/env/environment.sh
-HHSUITE=$DATABASES/hh-suite
-scopdir=${HHSUITE}/scope/scope
-pfamdir=${HHSUITE}/pfama/pfama
-mmcifdir=${HHSUITE}/mmcif70/mmcif70
-pfamdir=${HHSUITE}/pfama/pfama
-mmcifdir=${HHSUITE}/mmcif70/mmcif70
-saccharomyces=${HHSUITE}/Proteomes/Saccharomyces_cerevisiae_S288c/Saccharomyces_cerevisiae_S288c
-escherichia_coli=${HHSUITE}/Proteomes/Escherichia_coli_K12/Escherichia_coli_K12
+HHSUITE=${DATABASES}/hh-suite/
+PFAMREGEX="(pfam[0-9]+)|(^PF[0-9]+ ?(.[0-9]+))"
+FILESTRING=$(cat  params/dbs|tr "\n" " ")
 
+
+DBS=($FILESTRING)
+
+echo $FILESTRING
 
 if [ ! -e "results/$number.fas" ]
 then
-    #successively try to fetch a3m from databases
-    ffindex_get ${scopdir}_a3m.ffdata ${scopdir}_a3m.ffindex $accession >> results/$number.a3m
-    ffindex_get ${mmcifdir}_a3m.ffdata ${mmcifdir}_a3m.ffindex $accession >> results/$number.a3m
-    ffindex_get ${pfamdir}_a3m.ffdata ${pfamdir}_a3m.ffindex "$accession.a3m" >> results/$number.a3m
-    ffindex_get ${escherichia_coli}_a3m.ffdata ${escherichia_coli}_a3m.ffindex "$accession" >> results/$number.a3m
-    ffindex_get ${escherichia_coli}_a3m.ffdata ${escherichia_coli}_a3m.ffindex "$accession" >> results/$number.a3m
-    ffindex_get results/db_a3m.ffdata results/db_a3m.ffindex "db.a3m" >> results/$number.a3m
-    ffindex_get ${saccharomyces}_a3m.ffdata ${saccharomyces}_a3m.ffindex "$accession" >> results/$number.a3m
-    hhfilter -i results/$number.a3m -o results/$number.reduced.a3m -diff 100
-    reformat.pl a3m fas results/$number.reduced.a3m results/$number.fas
+    for i in "${DBS[@]}"
+    do
+    :
+    echo $i $accession
+    if [[ "$accession" =~ $PFAMREGEX ]]
+    then
+        ffindex_get ${HHSUITE}${i}_a3m.ffdata ${HHSUITE}${i}_a3m.ffindex "$accession.a3m" >> results/$number.a3m
+    else
+        ffindex_get ${HHSUITE}${i}_a3m.ffdata ${HHSUITE}${i}_a3m.ffindex $accession >> results/$number.a3m
+    fi
+    done
+
+
+    # Align two sequences or MSAs
+    if [[ $FILESTRING == "" ]]
+    then
+        ffindex_get results/db_a3m.ffdata results/db_a3m.ffindex "db.a3m" >> results/$number.a3m
+        ffindex_get results/db_a3m.ffdata results/db_a3m.ffindex $accession >> results/$number.a3m
+    fi
 fi
+
+hhfilter -i results/$number.a3m -o results/$number.reduced.a3m -diff 100
+reformat.pl a3m fas results/$number.reduced.a3m results/$number.fas
