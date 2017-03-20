94a0cbb1fbeed8d8b6a1355c8240e24139d526dd
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-20 17:38:50 +0100

    improved pfam regex, added ncbi regex + link, HHpred specific links, calculatePercent function

--- app/models/results/BlastVisualization.scala
+++ app/models/results/BlastVisualization.scala
@@ -23,20 +23,19 @@ object BlastVisualization extends Constants {
   private val scopReg = """([defgh][0-9a-zA-Z\.\_]+)""".r
   private val mmcifReg = """(...._[a-zA-Z])""".r
   private val mmcifShortReg = """([0-9]...)""".r
-  private val refseqReg = """[NXMRP.]+._[0-9]+}?((\#[0-9]{1})|(.[0-9]))""".r
-  private val pfamReg = """(pfam[0-9]+)|(^PF[0-9]+ ?(.[0-9]+))""".r
+  private val pfamReg = """(pfam[0-9]+|PF[0-9]+(\.[0-9]+)?)""".r
+  private val ncbiReg = """[A-Z]{2}_?[0-9]+\.?\#?([0-9]+)?""".r
 
-  private val ncbiRefseqBaseLink = "https://www.ncbi.nlm.nih.gov/Structure/cdd/wrpsb.cgi?seqinput=";
   private val pdbBaseLink = "http://pdb.rcsb.org/pdb/explore.do?structureId="
   private val pdbeBaseLink = "http://www.ebi.ac.uk/pdbe/entry/pdb/"
   private val ncbiBaseLink = "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?SUBMIT=y&db=structure&orig_db=structure&term="
+  private val ncbiProteinBaseLink = "https://www.ncbi.nlm.nih.gov/protein/"
   private val ebiBaseLink = "http://www.ebi.ac.uk/pdbe-srv/view/entry/"
   private val pubmedBaseLink = "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?CMD=search&db=pubmed&term="
   private val scopBaseLink = "http://scop.berkeley.edu/sid="
   private val scopLineageBaseLink = "http://scop.berkeley.edu/sccs="
   private val pfamBaseLink = "http://pfam.xfam.org/family/"
   private val cddBaseLink = "http://www.ncbi.nlm.nih.gov/Structure/cdd/cddsrv.cgi?uid="
-
 
   /**
     * Renders file content as plain HTML. Can be used for scripts that produce HTML from the old Toolkit
@@ -124,24 +123,25 @@ object BlastVisualization extends Constants {
       var idPfam = id.replaceAll("am.*$||..*", "")
       link += generateLink(pfamBaseLink, idPfam + "#tabview=tab1", id)
     }
-    else if(db == "refseq"){
-      link += generateLink(ncbiRefseqBaseLink, id, id)
+    else if(db == "ncbi"){
+      link += generateLink(ncbiProteinBaseLink, id, id)
     }
     else{
-      link = id;
+      link = id
     }
 
     Html(link)
   }
 
 
-  def getLinks(id : String, number: Int) : Html = {
+  def getLinksHHpred(id : String) : Html = {
     val db = identifyDatabase(id)
     var links = new ArrayBuffer[String]()
 
     var idPdb = id.replaceAll("_.*$", "").toLowerCase
     var idTrimmed = id.substring(1, 5)
     var idCDD = id.replaceAll("PF", "pfam")
+    var idNcbi = id.replaceAll("#", ".") + "?report=fasta"
     links +=  "<a data-open=\"templateAlignmentModal\" onclick=\"templateAlignment(\'" + id + "\')\">Template alignment</a>"
     if(db == "scop") {
 
@@ -157,25 +157,58 @@ object BlastVisualization extends Constants {
       idCDD = idCDD.replaceAll("\\..*","")
       links += generateLink(cddBaseLink, idCDD, "CDD")
     }
-    else if (db == "refseq"){
+    else if (db == "ncbi"){
+      links += generateLink(ncbiProteinBaseLink, idNcbi, "NCBI Fasta")
     }
     Html(links.mkString(" | "))
   }
 
-  def generateLink(baseLink : String, id : String, name : String) : String = "<a href='"+baseLink+id+ "' target='_blank'>"+name+"</a>"
+  def getLinks(id : String) : Html = {
+    val db = identifyDatabase(id)
+    var links = new ArrayBuffer[String]()
+    var idNcbi = id.replaceAll("#", ".") + "?report=fasta"
+    var idPdb = id.replaceAll("_.*$", "").toLowerCase
+    var idTrimmed = id.substring(1, 5)
+    var idCDD = id.replaceAll("PF", "pfam")
+    if(db == "scop") {
+      links += generateLink(scopBaseLink, id, "SCOP")
+      links += generateLink(ncbiBaseLink, idTrimmed, "NCBI")
+    }
+    else if(db == "mmcif") {
+      links += generateLink(pdbeBaseLink, idPdb, "PDBe")
+    }
+    else if (db == "pfam"){
+      idCDD = idCDD.replaceAll("\\..*","")
+      links += generateLink(cddBaseLink, idCDD, "CDD")
+    }
+    else if (db == "ncbi"){
+      links += generateLink(ncbiProteinBaseLink, idNcbi , "NCBI Fasta")
+    }
+
+    Html(links.mkString(" | "))
+  }
+
+  def generateLink(baseLink : String, id : String, name : String) : String = "<a href='"+baseLink+id+"' target='_blank'>"+name+"</a>"
 
   def identifyDatabase(id : String) : String = id match {
     case scopReg(_) => "scop"
+    case mmcifShortReg(_) => "mmcif"
     case mmcifReg(_) => "mmcif"
     case pfamReg(_,_) => "pfam"
-    case mmcifShortReg(_) => "mmcif"
-    case refseqReg(_) => "refseq"
+    case ncbiReg(_) => "ncbi"
     case e : String => Logger.info("Struc: ("+e+") could not be matched against any database!");""
   }
 
   def percentage(str : String) : String = {
-    var num = str.toDouble
+    val num = str.toDouble
     val percent = (num * 100).toInt.toString + " %"
+    percent
+  }
+
+  def calculatePercentage(str1 : String, str2 : String): String ={
+    val num1 = str1.toDouble
+    val num2 = str2.toDouble
+    val percent = ((num1/num2) * 100).toInt.toString + " %"
     percent
   }
 }
