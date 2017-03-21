4b06451e260174cc0159dc90ca3bd55255ed0c4a
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-21 13:50:30 +0100

    [NEW] write evalues to file

--- tel/runscripts/psiblast.sh
+++ tel/runscripts/psiblast.sh
@@ -22,20 +22,13 @@ blastJson2tab.py ../results/output_psiblastp.json ../results/output_psiblastp.ta
 blastviz_json.pl ../results/output_psiblastp.tab %jobid.content ../results/ ../results/ >> ../logs/blastviz.log
 # Generate Query in JSON
 fasta2json.py %alignment.path ../results/query.json
-# extract alignment from
 
-#PERL5LIB=%PERLLIB
+# extract alignment from
 #alignhits_html.pl ../results/out.psiblastp ../results/out.align -e %evalue.content -fas -no_link -blastplus
 
-## reformat alignment to clustal
-#reformat.pl fas \
-#            clu \
-#            "$(readlink -f ../results/out.align)" \
-#            "$(readlink -f ../results/out.align_clu)"
-#
 ## Produces the alignment fasta output
 #%SCALA %HELPER/psiblastpPostProcess.scala ../results/out.psiblastp
 
-# Produce new PSIBLAST Overview and also the Evalues list
-#parse_BLAST_HTML.py ../results/out.psiblastp > ../results/out.psiblastp_overview
+# Produce Evalues list
+awk {'print $(NF-6)'} ../results/output_psiblastp.tab >> ../results/evalues
 
