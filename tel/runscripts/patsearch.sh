d5ead2380f2d8a01b5a41a57cd8f2880d9783675
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-22 17:38:36 +0100

    [NEW] view for patternsearch, added wrapping again

--- tel/runscripts/patsearch.sh
+++ tel/runscripts/patsearch.sh
@@ -1,5 +1,5 @@
 patsearch.pl        -i  %alignment.path \
                     -d %STANDARD/%standarddb.content \
-                    -o ../results/output.fas \
+                    -o ../results/output.json \
                     -sc %seqcount.content \
                     -%grammar.content > report_patsearch
