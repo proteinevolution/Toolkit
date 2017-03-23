5b2e4085640bf01fb478c14dc6f137f983eebb86
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-23 06:41:27 +0100

    [WIP] gauge for cluster load

--- app/assets/ts/mithril/livetable.ts
+++ app/assets/ts/mithril/livetable.ts
@@ -1,3 +1,28 @@
+import ElementConfig = Mithril.ElementConfig;
+
+
+let gaugeConfig = function(elem: Element, isInit: boolean, ctx: any) {
+
+    if(!isInit){
+
+        let g = new JustGage({
+            id: "clusterLoad",
+            value:32,
+            min:0,
+            max:100,
+            title:"Cluster load",
+            label:"",
+            showMinMax:false,
+            gaugeColor:"#fff",
+            levelColors:["#a9d70b", "#f9c802", "#ff0000"],
+            refreshAnimationType:"linear"
+        });
+
+    }
+
+};
+
+
 let JobTable = {
     controller: function(args : any) {
 
@@ -30,20 +55,17 @@ let JobTable = {
 
     },
     view: function(ctrl : any, args : any) {
-        return m('div', [
-            m('table.liveTable', [
+        return m('div', {"class" : "row"}, [
+            m('div#clusterLoad', {"class" : "column large-4", config: gaugeConfig}, ''),
+            m('table.liveTable', {"class" : "column large-5"}, [
                 m('thead', [
                     m('tr', [
-                        m('th#run', 'Cluster load'),
-                        m('th', 'Users'),
                         m('th#lastJob', 'Last own job'),
                         m('th#lastJobs', 'Total own jobs')
                     ])]
                 )], [
                 m('tbody',
                     [m('tr', [
-                        m('td', '16'),
-                        m('td', '46'),
                         m('td#lastJobName', m('a[href="/#/jobs/' + ctrl.lastJob.jobID + '"]', ctrl.lastJob.toolnameLong)),
                         m('td', ctrl.totalJobs)
                     ])]
