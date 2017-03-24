134cff7aa5e544c4d64df99c78a5971979bfe85a
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-24 19:44:20 +0100

    [ADD] controller function to get alignment with bash script

--- app/controllers/PsiblastController.scala
+++ app/controllers/PsiblastController.scala
@@ -20,12 +20,13 @@ import models.Constants
 class PsiblastController @Inject()(webJarAssets : WebJarAssets) extends Controller with Constants {
   private val serverScripts = ConfigFactory.load().getString("serverScripts")
   private val retrieveFullSeq = (serverScripts + "/retrieveFullSeq.sh").toFile
+  private val alignSeqs = (serverScripts + "/align.sh").toFile
   private final val filePermissions = Set(PosixFilePermission.OWNER_EXECUTE, PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE)
 
   retrieveFullSeq.setPermissions(filePermissions)
 
 
-  def runScript(jobID : String) = Action.async { implicit request =>
+  def retrieveSeqs(jobID : String) = Action.async { implicit request =>
 
     if(!retrieveFullSeq.isExecutable) {
       Future.successful(BadRequest)
@@ -38,6 +39,28 @@ class PsiblastController @Inject()(webJarAssets : WebJarAssets) extends Controll
         val accessionsStr = (json \ "accessionsStr").get.as[String]
 
         Process(retrieveFullSeq.pathAsString, (jobPath + jobID).toFile.toJava, "jobID" -> jobID, "accessionsStr" -> accessionsStr, db -> "db").run().exitValue() match {
+
+          case 0 => Ok
+          case _ => BadRequest
+        }
+      }
+    }
+  }
+
+  def getAlignment(jobID : String) = Action.async { implicit request =>
+
+    if(!alignSeqs.isExecutable) {
+      Future.successful(BadRequest)
+      throw FileException(s"File ${alignSeqs.name} is not executable.")
+    }
+    else {
+      Future.successful{
+        val json =  request.body.asJson.get
+        val start = (json \ "start").get.as[String]
+        val end = (json \ "end").get.as[String]
+        val accessionsStr = (json \ "accessionsStr").get.as[String]
+
+        Process(alignSeqs.pathAsString, (jobPath + jobID).toFile.toJava, "jobID" -> jobID, "accessionsStr" -> accessionsStr, start -> "start" , end -> "end").run().exitValue() match {
 
           case 0 => Ok
           case _ => BadRequest
