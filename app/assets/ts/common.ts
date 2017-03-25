cccc980c364bd066f7d6c0c263fd1219f79baac9
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-25 18:12:15 +0100

    clean up

--- app/assets/ts/common.ts
+++ app/assets/ts/common.ts
@@ -12,3 +12,18 @@ let color_regex = /(?:[WYF]+|[LIVM]+|[AST]+|[KR]+|[DE]+|[QN]+|H+|C+|P+|G+)/g;
 function color_regex_replacer(match : string) {
     return ["<span class=\"aa_", match.charAt(0), "\">", match, "</span>"].join('');
 }
+
+// scroll arrow
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 200) {
+        $('#scrollTop').show();
+    } else {
+        $('#scrollTop').hide();
+    }
+});
+
+$('#scrollTop').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 200);
+    return false;
+});
+
