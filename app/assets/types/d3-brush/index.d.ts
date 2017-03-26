0fcc513136657bbf19a35c06f9879121260b8062
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-26 05:39:45 +0200

    fix more dependencies

--- app/assets/types/d3-brush/index.d.ts
+++ app/assets/types/d3-brush/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Tom Wanzek <https://github.com/tomwanzek>, Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { ArrayLike, Selection, TransitionLike, ValueFn } from '../d3-selection';
+import { ArrayLike, Selection, TransitionLike, ValueFn } from '../d3-selection/index';
 
 /**
  * Type alias for a BrushSelection. For a two-dimensional brush, it must be defined as [[x0, y0], [x1, y1]],
