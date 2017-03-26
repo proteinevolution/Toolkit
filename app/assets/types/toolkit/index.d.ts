985ea932366b5107faa8cea7ee208f8104db31d4
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-26 01:58:16 +0100

    migrate websocket.js which had some syntax issues anyways

--- app/assets/types/toolkit/index.d.ts
+++ app/assets/types/toolkit/index.d.ts
@@ -7,5 +7,8 @@ declare class JobListComponent {
 
     public static reloadList() : any;
     public static lastUpdatedJob : any;
+    public static removeJob(param : any) : any
+    public static pushJob(param : any) : any
+    public static Job(param : any) : any
 
 }
