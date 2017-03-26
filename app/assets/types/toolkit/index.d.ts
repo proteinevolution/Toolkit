7706ab9871dd2aee32646faf7dd097d8eeac22ad
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-26 19:21:38 +0200

    some work on typescript

--- app/assets/types/toolkit/index.d.ts
+++ app/assets/types/toolkit/index.d.ts
@@ -1,14 +1,36 @@
-declare class Toolkit {
+import ControllerFunction = Mithril.ControllerFunction;
 
-
-}
 
 declare class JobListComponent {
 
     public static reloadList() : any;
     public static lastUpdatedJob : any;
-    public static removeJob(param : any) : any
-    public static pushJob(param : any) : any
-    public static Job(param : any) : any
+    public static removeJob(param : any) : any;
+    public static pushJob(param : any) : any;
+    public static pushJob(param: any, bool : boolean) : any;
+    public static Job(param : any) : any;
+    public static selectedJobID : any;
+    public static contains(jobID : string) : any;
+
+    public static controller : any;
+    public static view : any;
 
 }
+
+
+declare class FrontendAlnvizComponent {
+
+    public static controller : any;
+    public static view: any;
+
+}
+
+declare class FrontendReformatComponent {}
+
+declare class JobViewComponent {
+
+    public static controller: any;
+    public static view: any;
+
+}
+
