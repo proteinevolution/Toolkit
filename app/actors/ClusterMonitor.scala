c58dd4b2d79654d751489deb9e248ca7c2aa5880
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-26 17:25:30 +0200

    use companion object

--- app/actors/ClusterMonitor.scala
+++ app/actors/ClusterMonitor.scala
@@ -3,9 +3,11 @@ package actors
 
 import javax.inject.{Inject, Singleton}
 
+import actors.ClusterMonitor._
 import akka.actor.{Actor, ActorLogging, ActorRef, Cancellable}
 import akka.event.LoggingReceive
 import models.sge.Cluster
+import org.joda.time.DateTime
 
 import scala.collection.immutable.HashSet
 import scala.concurrent.duration._
@@ -18,13 +20,18 @@ import scala.concurrent.duration._
 @Singleton
 final class ClusterMonitor @Inject()(cluster: Cluster) extends Actor with ActorLogging {
 
+
+  case class RecordedTick(load: Double, timestamp : DateTime)
+
   private val fetchLatestInterval = 75.millis
+  private val recordingInterval = 10.minutes
   protected[this] var watchers: HashSet[ActorRef] = HashSet.empty[ActorRef]
   // Fetch the latest qhost status every 375ms
   val Tick : Cancellable = {
     // scheduler should use the system dispatcher
     context.system.scheduler.schedule(Duration.Zero, fetchLatestInterval, self, FetchLatest)(context.system.dispatcher)
   }
+
 
   override def receive = LoggingReceive {
 
@@ -39,17 +46,27 @@ final class ClusterMonitor @Inject()(cluster: Cluster) extends Actor with ActorL
       watchers.foreach(_ ! UpdateLoad(load))
       //watchers.foreach(_ ! ConnectedUsers(watchers.size))
 
+    case Recording =>
+      val load = cluster.getLoad.loadEst
+      val currentTimestamp = DateTime.now()
+
+
   }
 
 }
 
+object ClusterMonitor {
 
-case object Disconnect
+  case object Disconnect
 
-case object Connect
+  case object Connect
 
-case object FetchLatest
+  case object FetchLatest
 
-case class UpdateLoad(load : Double)
+  case object Recording
 
-case class ConnectedUsers(users : Int)
+  case class UpdateLoad(load: Double)
+
+  case class ConnectedUsers(users: Int)
+
+}
