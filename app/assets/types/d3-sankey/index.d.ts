77e4781eda404fd3433a11c2f8983e33b2d5ee18
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-26 04:56:08 +0200

    add d3 type definitions

---
+++ app/assets/types/d3-sankey/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for D3JS d3-sankey module v0.2.0
+// Project: https://github.com/d3/d3-sankey/
+// Definitions by:
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
---
+++ app/assets/types/d3-tile/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for D3JS d3-tile module v0.0.3
+// Project: https://github.com/d3/d3-tile/
+// Definitions by:
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
