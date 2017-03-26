4fe6547d24acb3662153495a543da0c46d24b07c
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-26 04:56:08 +0200

    add d3 type definitions

---
+++ app/assets/types/d3-geo-projection/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for D3JS d3-geo-projection module v1.0.3
+// Project: https://github.com/d3/d3-geo-projection/
+// Definitions by:
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
---
+++ app/assets/types/d3-hexbin/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for D3JS d3-hexbin module v0.2.0
+// Project: https://github.com/d3/d3-hexbin/
+// Definitions by:
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
