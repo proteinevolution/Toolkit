adf481e5e547366ced3fac5133824b2ce65a6b32
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-03-27 19:11:32 +0200

    prepend

--- app/assets/ts/mithril/toolkit.ts
+++ app/assets/ts/mithril/toolkit.ts
@@ -11,19 +11,6 @@ let a = ['0', 'p', 'q', 'r', 'e', 'd', 'i'];
 let jobs = {};
 */
 
-// Velocity animation config
-
-let fadesIn = function(element : any, isInitialized : boolean, context : any) {
-
-    let url = window.location.href;
-    let parts = url.split("/");
-    let isJob = parts[parts.length-2] == "jobs";
-
-    if (!isInitialized && !isJob) {
-        element.style.opacity = 0;
-        $(element).velocity({opacity: 1, top: "50%"}, 750);
-    }
-};
 
 /*
 
