a425e427b766f33a24d2d5b18034d420d33ef676
davidmrau <david.rau@student.uni-tuebingen.de>
2017-03-31 11:19:33 +0200

    [FIX] hhpred template selection, adapted names

--- tel/runscripts/hhpred_manual.sh
+++ tel/runscripts/hhpred_manual.sh
@@ -1,10 +1,10 @@
 # fetch parameters from the parent here
-cp ../../%parentid.content/results/hhsearch.hhr ../params
+cp ../../%parentid.content/results/${JOBID}.hhr ../params
 cp ../../%parentid.content/results/query.a3m ../params
 
 mkdir -p ../results/cif
 
-checkTemplates.pl -i   ../params/hhsearch.hhr \
+checkTemplates.pl -i   ../params/${JOBID}.hhr \
                   -pir ../results/tomodel.pir \
                   -cif %CIF \
                   -o   $(readlink -f ../results/cif) \
