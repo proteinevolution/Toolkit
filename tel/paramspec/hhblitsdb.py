1c54080bff9efe59917b1c7b69251886052b0b7d
Vikram Alva <vikram.alva@gmail.com>
2017-04-01 19:29:45 +0200

    [FIX] Toolkit works now. HHBLITSDB parser needs to be refactored again
    HHBLITSDB parser used the old logic, as used by HHSUITE parser. We
    however, updated the DB file for HHblits to contain the entries in a
    single, without refactoring the code to deal with this.

--- tel/paramspec/hhblitsdb.py
+++ tel/paramspec/hhblitsdb.py
@@ -2,27 +2,22 @@
 import sys
 import os
 
-def print_args(args):
-    print(args[-1] + ' '+  '_'.join(args[:-1]))
-
-
 def main(argv):
     path = "%HHBLITS/DB"
+
     # If directory does not exist, print random stuff
-    if not os.path.exists(path):
+    if not os.path.exists(path): #todo: implement fallback mechanism
         print("foo bar")
         return
-    args = []
+
     with open(path, 'r') as infile:
         for line in infile:
             line = line.strip()
-            if not line or line.startswith('#'):
-                continue
-            if line.startswith('$'):
-                print_args(args)
-                args = []
-            else:
-                args.append(line)
+            if line and not line.startswith('#') and not line.startswith('$'):
+#todo: delete two lines
+                to_print = 'hhblits/%s %s' % (line, line)
+                print(to_print)
+#                print(line)
 
 if __name__ == '__main__':
     main(sys.argv)
