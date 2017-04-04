02bc3d5f9f2384e46e446779222acb08248dbfed
Vikram Alva <vikram.alva@gmail.com>
2017-04-04 18:29:26 +0200

    [FIX] Server side input validation for TPRpred

--- tel/runscripts/tprpred.sh
+++ tel/runscripts/tprpred.sh
@@ -1,6 +1,10 @@
 JOBID=%jobid.content
 
-tprpred_wrapper.pl -in %alignment.path \
+reformat_hhsuite.pl fas fas %alignment.path ${JOBID}.fas -l 32000 -uc
+
+mv ${JOBID}.fas ../results
+
+tprpred_wrapper.pl -in ../results/${JOBID}.fas \
                    -v 1 \
                    -cut %eval_tpr.content > ../results/${JOBID}.tpr
 
