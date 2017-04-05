05029c86630dd0fcc34ddea28549d48cfbbd7173
Vikram Alva <vikram.alva@gmail.com>
2017-04-05 13:35:05 +0200

    [FINALIZE] 6FrameTranslation

--- tel/runscripts/6frametranslation.sh
+++ tel/runscripts/6frametranslation.sh
@@ -1,6 +1,8 @@
+JOBID=%jobid.content
+
 java -Xmx4G -cp $BIOPROGS/tools/sixframe translate \
                       -i %alignment.path \
-                      -o ../results/output \
+                      -o ../results/${JOBID}.out \
                       -seq %inc_nucl.content \
                       -mode %codon_table.content \
                       -annot %amino_nucl_rel.content &> ../results/status.log
