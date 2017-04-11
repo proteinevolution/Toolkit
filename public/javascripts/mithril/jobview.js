bd20ee1fe84cab21e332237a98068e636ac2986d
davidmrau <david.rau@student.uni-tuebingen.de>
2017-04-11 14:12:04 +0200

    [ADD] general error message, if script failed besides the explicit process logs

--- public/javascripts/mithril/jobview.js
+++ public/javascripts/mithril/jobview.js
@@ -115,11 +115,25 @@ JobErrorComponent = {
                 if(logElem == "")
                     return;
                 logElem = logElem.split("\n");
-                if(logElem.length > 1){
-                    if(logElem[1] == "done"){
+                // delete empty entries from array
+                logElem = logElem.filter(Boolean);
+                var len = logElem.length-1;
+                console.log(len)
+                if(len > 0){
+                    if(len > 1){
+                        return [m("div", {class: "logElem"},
+                            m("i", {class: "icon-check_circle logElemDone"}),
+                            m("div", {class: "logElemText"}, logElem[0])),
+                            m("div", {class: "logElem"},
+                            m("i", {class: "icon-cancel_circle logElemError"}),
+                            m("div", {class: "logElemText"}, "error in runscript"))
+                            ]
+                    }
+                    else if(logElem[1] == "done"){
                         return m("div", {class: "logElem"},
                             m("i", {class: "icon-check_circle logElemDone"}),
                             m("div", {class: "logElemText"}, logElem[0]))
+
                     }
                     else if(logElem[1] == "error"){
                         return m("div", {class: "logElem"},
