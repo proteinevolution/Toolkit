1ed1db65c4ee5264e573efbccc5a2e93b2493774
andrew <andrew.stephens@tuebingen.mpg.de>
2017-04-11 19:59:23 +0200

    [ADD] Final additions to Password Reset from a non logged in user and users are required to use the email verification

--- app/models/database/users/UserToken.scala
+++ app/models/database/users/UserToken.scala
@@ -11,6 +11,7 @@ case class UserToken(tokenType    : Int,
                      token        : String           = RandomString.randomAlphaNumString(10),
                      passwordHash : Option[String]   = None,
                      eMail        : Option[String]   = None,
+                     userID       : Option[BSONObjectID] = None,
                      changeDate   : Option[DateTime] = Some(DateTime.now()))
 
 object UserToken {
@@ -18,6 +19,7 @@ object UserToken {
   lazy val TOKEN            = "token"
   lazy val NEWPASSWORDHASH  = "nPWH"
   lazy val NEWEMAIL         = "newEMail"
+  lazy val USERID           = "userID"
   lazy val CHANGEDATE       = "changeDate"
 
   /**
@@ -29,6 +31,7 @@ object UserToken {
       token        = doc.getAs[String](TOKEN).get,
       passwordHash = doc.getAs[String](NEWPASSWORDHASH),
       eMail        = doc.getAs[String](NEWEMAIL),
+      userID       = doc.getAs[BSONObjectID](USERID),
       changeDate   = doc.getAs[BSONDateTime](CHANGEDATE).map(dt => new DateTime(dt.value)))
   }
 
@@ -41,6 +44,7 @@ object UserToken {
       TOKEN           -> userToken.token,
       NEWPASSWORDHASH -> userToken.passwordHash,
       NEWEMAIL        -> userToken.eMail,
+      USERID          -> userToken.userID,
       CHANGEDATE      -> BSONDateTime(userToken.changeDate.fold(-1L)(_.getMillis)))
   }
 }
