9601d9303053d01d873942e49ad6f4ca98b7a988
davidmrau <david.rau@student.uni-tuebingen.de>
2017-04-13 14:23:08 +0200

    [FIX] HHblits template alignment scrit

--- scripts/templateAlignmentHHblits.sh
+++ scripts/templateAlignmentHHblits.sh
@@ -2,16 +2,20 @@
 # Set environment
 source /ebio/abt1_share/toolkit_support1/code/bioprogs/env/environment.sh
 
-HHBLITS=${DATABASES}/hhblits/
-DB=$( grep "^[^#]" ${DATABASES}/hhblits/DB )
+
+if [ ! -e "results/$accession.fas" ]
+then
+    HHBLITS=${DATABASES}/hhblits/
+    DB=$( grep "^[^#]" ${DATABASES}/hhblits/DB | awk 'NR==1{print $1}')
 
 
-MAPPINGFILE="uniclust_uniprot_mapping.tsv"
-echo $HHBLITS$MAPPINGFILE
-MAPPEDID=`grep ${accession} ${HHBLITS}${MAPPINGFILE} | awk '{print $1}'`
+    MAPPINGFILE="uniclust_uniprot_mapping.tsv"
+    echo $HHBLITS$MAPPINGFILE
+    MAPPEDID=`grep ${accession} ${HHBLITS}${MAPPINGFILE} | awk '{print $1}'`
 
-ffindex_get ${HHBLITS}${DB}_a3m.ffdata ${HHBLITS}${DB}_a3m.ffindex $MAPPEDID >> results/$accession.a3m
+    ffindex_get ${HHBLITS}${DB}_a3m.ffdata ${HHBLITS}${DB}_a3m.ffindex $MAPPEDID >> results/$accession.a3m
 
 
-hhfilter -i results/$accession.a3m -o results/$accession.reduced.a3m -diff 100
-reformat.pl a3m fas results/$accession.reduced.a3m results/$accession.fas
+    hhfilter -i results/$accession.a3m -o results/$accession.reduced.a3m -diff 100
+    reformat.pl a3m fas results/$accession.reduced.a3m results/$accession.fas
+fi
