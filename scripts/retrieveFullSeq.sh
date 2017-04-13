928acd60dfd907d9f3ab190be921da3afb40ff22
davidmrau <david.rau@student.uni-tuebingen.de>
2017-04-13 14:27:51 +0200

    [ADD] skip script if result already exists

--- scripts/retrieveFullSeq.sh
+++ scripts/retrieveFullSeq.sh
@@ -2,17 +2,22 @@
 # Set environment
 source /ebio/abt1_share/toolkit_support1/code/bioprogs/env/environment.sh
 
-#separate accessions in string by whitespace
-ACCESSIONS=$(echo $accessionsStr | tr " " "\n")
 
-# write accessions to be retrieved in file
-printf "${ACCESSIONS[@]}" > results/accessionsToRetrieve
+if [ ! -e "results/sequences.fa" ]
+then
 
-# read in file that contains the chosen db
-DB=$(<params/db)
+    #separate accessions in string by whitespace
+    ACCESSIONS=$(echo $accessionsStr | tr " " "\n")
 
-#retrieve full length sequences
-seq_retrieve.pl -i results/accessionsToRetrieve \
-                -o results/sequences.fa \
-                -d ${STANDARDNEW}/${DB} \
-                -unique 1 > results/unretrievable
+    # write accessions to be retrieved in file
+    printf "${ACCESSIONS[@]}" > results/accessionsToRetrieve
+
+    # read in file that contains the chosen db
+    DB=$(<params/db)
+
+    #retrieve full length sequences
+    seq_retrieve.pl -i results/accessionsToRetrieve \
+                    -o results/sequences.fa \
+                    -d ${STANDARDNEW}/${DB} \
+                    -unique 1 > results/unretrievable
+fi
