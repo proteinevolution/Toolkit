c51883b21c1f526921177efd577c7499794b2b3c
davidmrau <david.rau@student.uni-tuebingen.de>
2017-04-14 15:20:34 +0200

    [FIX] template selection | adapted filename of query

--- tel/runscripts/hhpred_automatic.sh
+++ tel/runscripts/hhpred_automatic.sh
@@ -4,13 +4,13 @@ JOBID=%jobid.content
 
 # fetch parameters from the parent here
 cp ../../%parentid.content/results/%parentid.content.hhr ../params
-cp ../../%parentid.content/results/query.a3m ../params
+cp ../../%parentid.content/results/%parentid.content.reduced.a3m ../params
 cp ../../%parentid.content/results/hhsearch.start.tab ../params
 
 
 # Rename according to the requirements of selectTemplates
 mv ../params/%parentid.content.hhr       ../params/${JOBID}.hhr
-mv ../params/query.a3m          ../params/${JOBID}.a3m
+mv ../params/%parentid.content.reduced.a3m ../params/${JOBID}.a3m
 mv ../params/hhsearch.start.tab ../params/${JOBID}.start.tab
 
 hhmake -i $(readlink -f ../params/${JOBID}.a3m) \
