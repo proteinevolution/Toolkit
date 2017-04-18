ce8711c479c74d751c6c51ba3df81253dbbafe8b
Vikram Alva <vikram.alva@gmail.com>
2017-04-18 12:45:19 +0200

    [FINALIZE] ANCESCON

--- tel/runscripts/ancescon.sh
+++ tel/runscripts/ancescon.sh
@@ -1,21 +1,35 @@
-reformat_hhsuite.pl fas clu %alignment.path "$(readlink -f ../results/alignment.clu)"
+JOBID=%jobid.content
 
+reformat_hhsuite.pl fas fas $(readlink -f %alignment.path) $(readlink -f %alignment.path) -uc -l 32000
+SEQ_COUNT=$(egrep '^>' ../params/alignment  -c)
+
+echo "#Query is an MSA with ${SEQ_COUNT} sequences." >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+
+echo "done" >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+
+prepareForAncescon.pl %alignment.path ../results/${JOBID}.in ../results/${JOBID}.names
+
+reformat_hhsuite.pl fas clu "$(readlink -f ../results/${JOBID}.in)" "$(readlink -f ../results/${JOBID}.clu)"
 
 # Remove CLUSTAL text in alignment.clu
-sed -i '/CLUSTAL/Id' ../results/alignment.clu
+sed -i '/CLUSTAL/Id' ../results/${JOBID}.clu
 
+echo "Running ANCESCON on query MSA." >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
-#Write namesfile
-< ../results/alignment.clu  awk 'BEGIN{i=0;count=0}{if($2){i=1; $2=$1;$1=count;count++;print;next};if(!$2 && i==1)exit}' > ../results/names.dat
+ancestral -i ../results/${JOBID}.clu \
+          -o ../results/${JOBID}.anc_out
 
+echo "done" >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
-# Rename sequences
-< ../results/alignment.clu  awk 'BEGIN{i=0}{if(/^\s*$/){i=0} else {$1=sequence".i;i++}; print }' > ../results/alignment2.clu
+echo "#Preparing output." >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
+echo "done" >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
-ancestral -i ../results/alignment2.clu \
-          -o ../results/anc_out 
-
-cp ../results/alignment2.clu.tre  ../results/alignment2.clu.orig.tre
-ancescontreemerger.pl -n ../results/names.dat -t ../results/alignment2.clu.tre
-
+cp ../results/${JOBID}.clu.tre  ../results/${JOBID}.clu.orig.tre
+ancescontreemerger.pl -n ../results/${JOBID}.names -t ../results/${JOBID}.clu.tre
