92c932073d0537f69378832b3bacb774a5bf1ca5
Vikram Alva <vikram.alva@gmail.com>
2017-04-20 10:42:13 +0200

    [FINALIZE] PhyML

--- tel/runscripts/phyml.sh
+++ tel/runscripts/phyml.sh
@@ -13,6 +13,7 @@ reformat_phylip.pl -i=fas \
 echo "done" >> ../results/process.log
 curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
+
 echo "#Running PhyML." >> ../results/process.log
 curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
@@ -20,7 +21,17 @@ PhyML-3.1_linux64 -i ../results/${JOBID}.phy \
                   -d aa \
                   - %matrix_phyml.content \
                   -b %no_replicates.content \
-                  -a %gamma_rate.content \
-                  -v 0.0
+                  -a e \
+                  -v e \
+                  -s SPR
+
+if [ "%no_replicates.content" -gt "0" ] ; then
+    cat ../results/*phyml_stats.txt ../results/*phy_phyml_boot_stats.txt > ../results/${JOBID}.stats
+    rm ../results/*phyml_boot*
+    rm ../results/*phyml_stats.txt
+else
+    mv ../results/*phyml_stats.txt ../results/${JOBID}.stats
+fi
+
 echo "done" >> ../results/process.log
 curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
