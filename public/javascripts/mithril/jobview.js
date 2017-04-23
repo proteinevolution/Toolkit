f3f578e477151d2be3c37403e4c0fa1d296c5ad8
andrew <andrew.stephens@tuebingen.mpg.de>
2017-04-23 20:24:01 +0200

    [FIX] Job Submission blocking is now unlocked after successful submit

--- public/javascripts/mithril/jobview.js
+++ public/javascripts/mithril/jobview.js
@@ -516,7 +516,10 @@ JobSubmissionComponent = {
         }
         return {
             submit: function(startJob) {
-                if (JobSubmissionComponent.submitting || !JobSubmissionComponent.jobIDValid) return;
+                if (JobSubmissionComponent.submitting || !JobSubmissionComponent.jobIDValid) {
+                    console.log("Job Submission is blocked: ", JobSubmissionComponent.submitting, JobSubmissionComponent.jobIDValid);
+                    return;
+                }
                 JobSubmissionComponent.submitting = true; // ensure that the submission is not reinitiated while a submission is ongoing
                 var form = document.getElementById("jobform");
 
@@ -580,7 +583,8 @@ JobSubmissionComponent = {
                         });
                         modal.on('closed.zf.reveal', function(){
                             modal.unbind('closed.zf.reveal');
-                            JobSubmissionComponent.submitting = false ;
+                            console.log("unsetting job submission Blocking.");
+                            JobSubmissionComponent.submitting = false;
                         }).foundation('open');
                         return
                     } else {
@@ -596,7 +600,11 @@ JobSubmissionComponent = {
                             url: submitRoute.url,
                             data: formData,
                             extract: extractStatus,
-                            serialize: function(data) { m.route("/jobs/" + jobID); return data; }
+                            serialize: function(data) {
+                                console.log("unsetting job submission Blocking.");
+                                JobSubmissionComponent.submitting = false;
+                                m.route("/jobs/" + jobID); return data;
+                            }
                         });
                         return
                     }
