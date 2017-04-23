afda9bba7a09360ce590cb9ef599c799c3405bff
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-04-23 18:23:32 +0200

    dynamic source in template shell scripts

--- lib/retrieveFullSeq.sh
+++ lib/retrieveFullSeq.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # Set environment
-source /ebio/abt1_share/toolkit_support1/code/bioprogs/env/environment.sh
+
+
+if [ "$HOSTNAME" = "olt" ]
+  then
+    source /ebio/abt1_share/toolkit_support1/code/bioprogs/env/environment.sh
+
+elif [ "$HOSTNAME" = "rye" ]
+  then
+    source /cluster/toolkit/production/bioprogs/env/environment_prod.sh
+  fi
+
+
+
+
 
     #separate accessions in string by whitespace
     ACCESSIONS=$(echo $accessionsStr | tr " " "\n")
