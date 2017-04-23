82b937f8f9f388e97009678ea48493b14a1aa33c
Vikram Alva <vikram.alva@gmail.com>
2017-04-23 23:06:32 +0200

    [ADD] Process log for CLANS

--- tel/runscripts/clans.sh
+++ tel/runscripts/clans.sh
@@ -2,6 +2,9 @@ JOBID=%jobid.content
 
 SEQ_COUNT=$(egrep '^>' ../params/alignment  -c)
 
+echo "#Input contains ${SEQ_COUNT} sequences." >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+
 # set gapopen and gapextend costs depending on given matrix
 GAPOPEN=11
 GAPEXT=1
@@ -18,8 +21,14 @@ if [ "%matrix.content" = "BLOSUM45" ] ; then
     GAPEXT=2
 fi
 
+echo "done" >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+
 #remove '-' from sequences, merge sequences split into multiple lines, create an indexed copy of the FASTA file
 prepareForClans.pl %alignment.path ../results/${JOBID}.0.fas ../results/${JOBID}.1.fas
+
+echo "#Performing ${SEQ_COUNT} X ${SEQ_COUNT} pairwise BLAST+ comparisons." >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
 #BLAST formatted database
 makeblastdb -in ../results/${JOBID}.1.fas -dbtype prot
@@ -37,8 +46,17 @@ blastp -query ../results/${JOBID}.1.fas \
        -out ../results/${JOBID}.nxnblast \
        -seg no \
        -num_threads %THREADS
+       
+echo "done" >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+
+echo "#Generating CLANS file." >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1       
 
 blast2clans.pl ../results/${JOBID} ../results/${JOBID}.0.fas ${SEQ_COUNT}
 
 rm ../results/${JOBID}.nxnblast
 rm ../results/${JOBID}*fas*
+
+echo "done" >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
