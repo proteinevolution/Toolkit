e0d23fb05d05c114b03cb2f5ed6cb99d5b5c7170
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-04-29 10:21:30 +0200

    fix missing types

--- app/controllers/Backend.scala
+++ app/controllers/Backend.scala
@@ -12,7 +12,7 @@ import play.api.cache._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json
 import play.api.libs.json.Json
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, AnyContent, Controller}
 import play.modules.reactivemongo.ReactiveMongoApi
 import reactivemongo.bson.{BSONDateTime, BSONDocument, BSONObjectID}
 
@@ -40,7 +40,7 @@ final class Backend @Inject()(webJarAssets                                      
     *
     * @return
     */
-  def access = Action.async { implicit request =>
+  def access : Action[AnyContent] = Action.async { implicit request =>
     getUser.map { user =>
       if (user.isSuperuser) {
         connectedUsers.put(user.userID, DateTime.now().plusMinutes(10))
@@ -52,7 +52,7 @@ final class Backend @Inject()(webJarAssets                                      
     }
   }
 
-  def logOut = Action.async { implicit request =>
+  def logOut : Action[AnyContent] = Action.async { implicit request =>
     getUser.map { user =>
       val timeOpt = connectedUsers.remove(user.userID)
       NoCache(Redirect(routes.Application.index()))
@@ -60,7 +60,7 @@ final class Backend @Inject()(webJarAssets                                      
   }
 
 
-  def settings = Action.async { implicit request =>
+  def settings : Action[AnyContent] = Action.async { implicit request =>
     getUser.map { user =>
       if (CheckBackendPath) {
         NoCache(Ok(views.html.backend.backend(webJarAssets, views.html.backend.settings(settingsController.clusterMode), "Backend")))
@@ -71,7 +71,7 @@ final class Backend @Inject()(webJarAssets                                      
   }
 
 
-  def edit = Action.async { implicit request =>
+  def edit : Action[AnyContent] = Action.async { implicit request =>
     getUser.map { user =>
       val content = "<h6> Recent Updates </h6>\n<hr>\n<b>February, 2016</b>\n <p class=\"message\" align=\"justify\"> PfamA 29.0 database is now also available for HHpred. </p>\n<b>January, 2016</b>\n<p class=\"message\" align=\"justify\"> BLAST+ has been updated to version 2.3.0 and MODELLER to version\n9.16. FRpred uses the BioJS Tree Viewer for displaying UPGMA dendrograms and the multiple sequence alignment\ntool GLProbs has been added to our alignment section. HHpred bugfix: probability cut-off is working again. New PatternSearch feature: found patterns are highlighted also in the export file.</p>\n<b>December, 2015</b>\n<p class=\"message\" align=\"justiy\"> ANCESCON and AlignmentViewer now use the BioJS Tree Viewer and\nthe BioJS MSA Viewer instead of the Archaeopteryx Tree Viewer applet and instead of the JalView applet,\nrespectively. Now, these tools are also fully functional in the Chrome browser. PSI-BLAST+ and ProtBLAST+ now use BLAST+ 2.2.31. </p>" // TODO get the current content from the db
       if (CheckBackendPath) {
@@ -83,7 +83,7 @@ final class Backend @Inject()(webJarAssets                                      
   }
 
   //TODO currently working mithril routes for the backend
-  def index = Action.async { implicit request =>
+  def index : Action[AnyContent] = Action.async { implicit request =>
     getUser.map { user =>
       if (user.isSuperuser) {
         //CheckBackendPath && user.isSuperuser && connectedUsers.get(user.userID).get.isBeforeNow) {
@@ -95,7 +95,7 @@ final class Backend @Inject()(webJarAssets                                      
     }
   }
 
-  def statistics = Action.async { implicit request =>
+  def statistics : Action[AnyContent] = Action.async { implicit request =>
     getUser.flatMap { user =>
       if (user.isSuperuser) {
         val dateTimeFirstOfMonth : DateTime = DateTime.now().dayOfMonth().withMinimumValue().withTimeAtStartOfDay()
@@ -116,7 +116,7 @@ final class Backend @Inject()(webJarAssets                                      
     }
   }
 
-  def pushMonthlyStatistics = Action.async { implicit request =>
+  def pushMonthlyStatistics : Action[AnyContent] = Action.async { implicit request =>
     getUser.flatMap { user =>
       if (user.isSuperuser) {
         getStatistics.map { toolStatisticList: List[ToolStatistic] =>
@@ -132,7 +132,7 @@ final class Backend @Inject()(webJarAssets                                      
     }
   }
 
-  def cms = Action.async { implicit request =>
+  def cms : Action[AnyContent] = Action.async { implicit request =>
     getUser.flatMap { user =>
       if (user.isSuperuser) {
         getArticles(-1).map { articles =>
@@ -144,7 +144,7 @@ final class Backend @Inject()(webJarAssets                                      
     }
   }
 
-  def users = Action.async { implicit request =>
+  def users : Action[AnyContent] = Action.async { implicit request =>
     getUser.flatMap { user =>
       if (user.isSuperuser) {
         findUsers(BSONDocument(User.USERDATA -> BSONDocument("$exists" -> true))).map { users =>
