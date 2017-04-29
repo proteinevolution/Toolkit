d37f399f45a94da2b1e0faa06a027f51fbb69781
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-04-29 04:37:55 +0200

    production startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-JAVA_OPTS="-server -XX:MaxPermSize=2048m -Xms2048m -Xmx2048m -XX:MetaspaceSize=1024m" activator
+JAVA_OPTS="-server -XX:MaxPermSize=2048m -Xms2048m -Xmx2048m -XX:MetaspaceSize=1024m" activator -Dhttp.proxyHost="dmzproxy.local"\
+ -Dhttp.proxyPort="8080" -Dhttps.proxyHost="dmzproxy" -Dhttps.proxyPort="8080" -Dhttp.nonProxyHosts="localhost|127.0.0.1"
 
