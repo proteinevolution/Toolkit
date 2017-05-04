096ccd8c6e2f8fd370c65f56694a60f0ac7d2c01
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-05-04 17:41:04 +0200

    refactor and delete conf and some js calls

---
+++ app/assets/ts/configs.ts
@@ -0,0 +1,9 @@
+// file for config of js plugins
+
+$(document).ready(function(){
+
+    // slick navigation slider
+    let slickConf = {infinite: false, slidesToShow: 7, slidesToScroll: 0, variableWidth: true, arrows: false };
+    $('.slicky').slick(slickConf);
+    
+});
