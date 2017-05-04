073e907323fa7acc7de9269b994be86e1f04e917
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-05-04 19:26:13 +0200

    remove inline js code from main template

--- app/assets/ts/configs.ts
+++ app/assets/ts/configs.ts
@@ -5,5 +5,7 @@ $(document).ready(function(){
     // slick navigation slider
     let slickConf = {infinite: false, slidesToShow: 7, slidesToScroll: 0, variableWidth: true, arrows: false };
     $('.slicky').slick(slickConf);
+    $(document).foundation();
+    $('.tooltipster').tooltipster();
 
 });
