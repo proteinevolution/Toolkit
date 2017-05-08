c0a782c41fc84c18a599b13da2805786a9d09634
davidmrau <david.rau@student.uni-tuebingen.de>
2017-05-08 14:42:02 +0200

    [WIP] news page, [ADD] textlong to article model in order to display full text in news page

--- app/controllers/DataController.scala
+++ app/controllers/DataController.scala
@@ -55,9 +55,9 @@ class DataController  @Inject() (val reactiveMongoApi: ReactiveMongoApi, psiblas
   /**
     * Action to write an article into the database
     */
-  def writeArticle(title: String, text: String, link: String, imagePath: String) : Action[AnyContent] = Action.async{
+  def writeArticle(title: String, text: String, textlong: String, link: String, imagePath: String) : Action[AnyContent] = Action.async{
     // TODO ensure that only authorized people can write a front page article
-    val article = FeaturedArticle(BSONObjectID.generate(),title, text, link,imagePath,Some(DateTime.now()),None)
+    val article = FeaturedArticle(BSONObjectID.generate(),title, text, textlong,  link,imagePath,Some(DateTime.now()),None)
     writeArticleDatabase(article).map { wr =>
       if(wr.ok){
         Ok
