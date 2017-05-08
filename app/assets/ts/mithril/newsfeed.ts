38995650ceaa99a7278deb30ab2573a0abccf356
davidmrau <david.rau@student.uni-tuebingen.de>
2017-05-08 14:42:02 +0200

    [WIP] news page, [ADD] textlong to article model in order to display full text in news page

--- app/assets/ts/mithril/newsfeed.ts
+++ app/assets/ts/mithril/newsfeed.ts
@@ -1,25 +1,47 @@
 (<any>window).News = {
-
-    controller: function(args : any) {
+    controller: function (args: any) {
         document.title = "Toolkit Updates";
+        let getRecentArticlesRoute = jsRoutes.controllers.DataController.getRecentArticles(100);
+        return {
+            articles: m.request({
+                url: getRecentArticlesRoute.url,
+                method: getRecentArticlesRoute.method
+            })
+        };
     },
-    view: function(ctrl : any) {
+    view: function (ctrl: any) {
         return [
             m("div", {
-                    "class": "large-2 padded-column columns show-for-large",
-                    id: "sidebar",
-                    config: fadesIn
-                }, [
-                    // no content in sidebar for newsfeed
-                ]
-            ),
-            m("div", {
                     "class": "large-10 padded-column columns show-for-large",
-                    id: "content"}, [
-                    m("h3", "Toolkit Updates")
-                ]
-            )
+                    id: "content"
+                }, [
+                    m("span", {class: "newsfeed-title"}, "Toolkit Updates"),
+                    m("div", {
+                            "class": "row article_container small-up-1 medium-up-2 large-up-3"
+                        },
+                        ctrl.articles().map(function (article : any) { // TODO this javascript error existed before migration "Uncaught TypeError: Cannot read property 'map' of undefined"
+                            return m("div", {
+                                    "class": "column column-block news-tile_main_container"
+                                },
+                                m("div", {"class": "news-container"},
+                                    m("div", {"class": "news-title"},
+                                        m("a", article.title),
+                                        m("hr")),
+                                    m("div", {"class": "news-text"},
+                                        m("p", article.textlong),
+                                    ),
+                                    m("div", {class: "news-image", 'style': {'background-image': 'url(' + article.imagePath + ')'}})
+                                )
+                            )
+
+                        })
+                    )
+                    ]
+            ),
         ]
     }
-
 };
+
+
+
+
