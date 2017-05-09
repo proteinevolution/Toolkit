b68b9a2137037850acfe9d1783efdf30e2f87930
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-05-09 13:49:24 +0200

    require either hhsuitedb or proteomes to have one opt selected

--- app/assets/ts/mithril/helper.ts
+++ app/assets/ts/mithril/helper.ts
@@ -10,3 +10,31 @@ let closeShortcut = function() {
 let tabulated = function(element : any, isInit : boolean) : any {
     if (!isInit) { return $(element).tabs({ active: this.active, beforeLoad: function(event, ui){ ui.panel.addClass("result-panel")}});}
 };
+
+// clear input forms when clicking on a tool again
+
+$('.toolsec').on('click', 'a', function() { /*m.route(this.href)*/ });
+
+
+
+let select2Config = function(elem : any, isInit : boolean) : any {
+
+    if(!isInit) {
+
+        $(elem).select2();
+
+        return  $(elem).on("change", function () {
+
+            if ($('#hhsuitedb').val().length == 0 && $('#proteomes').val().length == 0 ) {
+                $('#hhsuitedb').prop("required", true);
+                $('#proteomes').prop("required", true);
+            } else {
+                $('#hhsuitedb').prop("required", false);
+                $('#proteomes').prop("required", false);
+            }
+
+        });
+
+    }
+
+};
