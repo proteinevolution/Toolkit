6f29e68f13fd957f1d43e46ac901faf230cf84fd
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-05-09 11:06:24 +0200

    remove old backend stuff

--- app/controllers/Backend.scala
+++ app/controllers/Backend.scala
@@ -35,52 +35,7 @@ final class Backend @Inject()(webJarAssets                                      
   // Maps Session ID to Actor Ref of corresponding WebSocket
   val connectedUsers = new scala.collection.mutable.HashMap[BSONObjectID, DateTime]
 
-  /**
-    * Redirect the user to the
-    *
-    * @return
-    */
-  def access : Action[AnyContent] = Action.async { implicit request =>
-    getUser.map { user =>
-      if (user.isSuperuser) {
-        connectedUsers.put(user.userID, DateTime.now().plusMinutes(10))
-        NoCache(Redirect(routes.Backend.index()))
-      }
-      else {
-        Status(404)(views.html.errors.pagenotfound())
-      }
-    }
-  }
 
-  def logOut : Action[AnyContent] = Action.async { implicit request =>
-    getUser.map { user =>
-      val timeOpt = connectedUsers.remove(user.userID)
-      NoCache(Redirect(routes.Application.index()))
-    }
-  }
-
-
-  def settings : Action[AnyContent] = Action.async { implicit request =>
-    getUser.map { user =>
-      if (CheckBackendPath) {
-        NoCache(Ok(views.html.backend.backend(webJarAssets, views.html.backend.settings(settingsController.clusterMode), "Backend")))
-      } else {
-        Status(404)(views.html.errors.pagenotfound())
-      }
-    }
-  }
-
-
-  def edit : Action[AnyContent] = Action.async { implicit request =>
-    getUser.map { user =>
-      val content = "<h6> Recent Updates </h6>\n<hr>\n<b>February, 2016</b>\n <p class=\"message\" align=\"justify\"> PfamA 29.0 database is now also available for HHpred. </p>\n<b>January, 2016</b>\n<p class=\"message\" align=\"justify\"> BLAST+ has been updated to version 2.3.0 and MODELLER to version\n9.16. FRpred uses the BioJS Tree Viewer for displaying UPGMA dendrograms and the multiple sequence alignment\ntool GLProbs has been added to our alignment section. HHpred bugfix: probability cut-off is working again. New PatternSearch feature: found patterns are highlighted also in the export file.</p>\n<b>December, 2015</b>\n<p class=\"message\" align=\"justiy\"> ANCESCON and AlignmentViewer now use the BioJS Tree Viewer and\nthe BioJS MSA Viewer instead of the Archaeopteryx Tree Viewer applet and instead of the JalView applet,\nrespectively. Now, these tools are also fully functional in the Chrome browser. PSI-BLAST+ and ProtBLAST+ now use BLAST+ 2.2.31. </p>" // TODO get the current content from the db
-      if (CheckBackendPath) {
-        Ok(views.html.backend.backend(webJarAssets, views.html.backend.edit(webJarAssets, content), "Backend"))
-      } else {
-        Status(404)(views.html.errors.pagenotfound())
-      }
-    }
-  }
 
   //TODO currently working mithril routes for the backend
   def index : Action[AnyContent] = Action.async { implicit request =>
