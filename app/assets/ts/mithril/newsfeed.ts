a8572e33563d22557c96a083169e0fb3d0395d8d
Jonas Kübler <jonas.kuebler@tuebingen.mpg.de>
2017-05-10 18:24:11 +0200

    [CHANGE] newsfeed sizes

--- app/assets/ts/mithril/newsfeed.ts
+++ app/assets/ts/mithril/newsfeed.ts
@@ -12,12 +12,12 @@
     view: function (ctrl: any) {
         return [
             m("div", {
-                    "class": "large-10 padded-column columns show-for-large",
+                    "class": "large-12 padded-column columns show-for-large",
                     id: "content"
                 }, [
                     m("span", {class: "newsfeed-title"}, "Toolkit Updates"),
                     m("div", {
-                            "class": "row article_container small-up-1 medium-up-2 large-up-3"
+                            "class": "row news_container small-up-1 medium-up-2 large-up-3"
                         },
                         ctrl.articles().map(function (article : any) { // TODO this javascript error existed before migration "Uncaught TypeError: Cannot read property 'map' of undefined"
                             return m("div", {
