f92ae92a293dab678e770acc3b8d3a32a6255c9b
andrew <andrew.stephens@tuebingen.mpg.de>
2017-05-14 22:03:11 +0200

    [REFACTOR] changed the routes for submission, added Job Hash Search(WIP)

--- app/models/database/jobs/JobHash.scala
+++ app/models/database/jobs/JobHash.scala
@@ -1,5 +1,8 @@
 package models.database.jobs
 
+import models.search.JobDAO
+import modules.tel.env.Env
+import better.files._
 import org.joda.time.DateTime
 import reactivemongo.bson.{BSONDateTime, BSONDocument, BSONDocumentReader, BSONDocumentWriter, BSONObjectID}
 
@@ -57,6 +60,47 @@ object JobHash {
       TOOLHASH      -> jobHash.toolHash,
       DATECREATED   -> BSONDateTime(jobHash.dateCreated.fold(-1L)(_.getMillis)),
       JOBID         -> jobHash.jobID
+    )
+  }
+
+  /**
+    * Generates a JobHash for the job from the supplied parameters
+    * @param job
+    * @param params
+    * @return
+    */
+  def generateJobHash(job: Job, params: Map[String, String], env : Env, jobDAO: JobDAO): JobHash = {
+    // filter unique parameters
+    val paramsWithoutMainID = params - Job.ID - Job.IDDB - Job.JOBID - Job.EMAILUPDATE - "public"
+
+    // Create the job Hash depending on what db is used
+
+    val dbParam = params match {
+      case x if x isDefinedAt "standarddb" =>
+        val STANDARDDB = (env.get("STANDARD") + "/" + params.getOrElse("standarddb", "")).toFile
+        (Some("standarddb"), Some(STANDARDDB.lastModifiedTime.toString))
+
+      case x if x isDefinedAt "hhsuitedb" =>
+        val HHSUITEDB = env.get("HHSUITE").toFile
+        (Some("hhsuitedb"), Some(HHSUITEDB.lastModifiedTime.toString))
+
+      case x if x isDefinedAt "hhblitsdb" =>
+        val HHBLITSDB = env.get("HHBLITS").toFile
+        (Some("hhblitsdb"), Some(HHBLITSDB.lastModifiedTime.toString))
+
+      case _ => (Some("none"), Some("1970-01-01T00:00:00Z"))
+    }
+
+    JobHash(
+      mainID = job.mainID,
+      inputHash = jobDAO.generateHash(paramsWithoutMainID).toString(),
+      runscriptHash = jobDAO.generateRSHash(job.tool),
+      dbName = dbParam._1, // field must exist so that elasticsearch can do a bool query on multiple fields
+      dbMtime = dbParam._2, // use unix epoch time
+      toolName = job.tool,
+      toolHash = jobDAO.generateToolHash(job.tool),
+      dateCreated = job.dateCreated,
+      jobID = job.jobID
     )
   }
 }
