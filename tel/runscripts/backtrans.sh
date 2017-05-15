b0efa59d973cd04bafe86de7d3e9f872fa1c6029
Vikram Alva <vikram.alva@gmail.com>
2017-05-15 11:35:16 +0200

    Server-side validation for backstrans

--- tel/runscripts/backtrans.sh
+++ tel/runscripts/backtrans.sh
@@ -1,4 +1,33 @@
 JOBID=%jobid.content
+SEQ_COUNT=$(egrep '^>' ../params/alignment  -c)
+CHAR_COUNT=$(wc -m < ../params/alignment)
+
+if [ $CHAR_COUNT -gt "30000" ] ; then
+
+      echo "#Input contains more than 30000 characters." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      echo "error" >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      $?=1
+fi
+
+if [ $SEQ_COUNT -eq "0" ] ; then
+
+      echo "#Invalid input format. Input should be in FASTA format." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      echo "error" >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      $?=1
+fi
+
+if [ $SEQ_COUNT -gt "1" ] ; then
+
+      echo "#Input may not contain more than 1 protein sequence." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      echo "error" >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      $?=1
+fi
 
 if [ "%codon_table_organism.content" == "" ] ; then
         backtranslate.pl  -o=../results/${JOBID}.out \
