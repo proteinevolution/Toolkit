a044862549c3329e1b8150dd315ebfee43492fb0
Vikram Alva <vikram.alva@gmail.com>
2017-05-15 11:30:27 +0200

    Server-side validation for 6frametranslation

--- tel/runscripts/6frametranslation.sh
+++ tel/runscripts/6frametranslation.sh
@@ -1,5 +1,36 @@
 JOBID=%jobid.content
 
+SEQ_COUNT=$(egrep '^>' ../params/alignment  -c)
+CHAR_COUNT=$(wc -m < ../params/alignment)
+
+
+if [ $CHAR_COUNT -gt "30000" ] ; then
+
+      echo "#Input contains more than 30000 characters." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      echo "error" >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      $?=1
+fi
+
+if [ $SEQ_COUNT -eq "0" ] ; then
+
+      echo "#Invalid input format. Input should be in FASTA format." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      echo "error" >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      $?=1
+fi
+
+if [ $SEQ_COUNT -gt "1" ] ; then
+
+      echo "#Input may not contain more than 1 DNA sequence." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      echo "error" >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      $?=1
+fi
+
 java -Xmx4G -cp $BIOPROGS/tools/sixframe translate \
                       -i %alignment.path \
                       -o ../results/${JOBID}.out \
