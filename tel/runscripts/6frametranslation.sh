42dc8fd12669ded32a1c3ca5bcf8413c485a7493
Vikram Alva <vikram.alva@gmail.com>
2017-05-15 16:41:34 +0200

    Server-side input validation for all tools in the utils sections

--- tel/runscripts/6frametranslation.sh
+++ tel/runscripts/6frametranslation.sh
@@ -1,35 +1,29 @@
 JOBID=%jobid.content
 
-SEQ_COUNT=$(egrep '^>' ../params/alignment  -c)
+SEQ_COUNT=$(egrep '^>' ../params/alignment | wc -l)
 CHAR_COUNT=$(wc -m < ../params/alignment)
 
-
 if [ $CHAR_COUNT -gt "30000" ] ; then
-
       echo "#Input contains more than 30000 characters." >> ../results/process.log
       curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      echo "error" >> ../results/process.log
-      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      $?=1
+      kill -9
 fi
 
 if [ $SEQ_COUNT -eq "0" ] ; then
-
       echo "#Invalid input format. Input should be in FASTA format." >> ../results/process.log
       curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      echo "error" >> ../results/process.log
-      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      $?=1
+      kill -9
 fi
 
 if [ $SEQ_COUNT -gt "1" ] ; then
-
       echo "#Input may not contain more than 1 DNA sequence." >> ../results/process.log
       curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      echo "error" >> ../results/process.log
-      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      $?=1
+      kill -9
 fi
+
+
+echo "#Running 6FrameTranslation on the input DNA sequence." >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
 java -Xmx4G -cp $BIOPROGS/tools/sixframe translate \
                       -i %alignment.path \
@@ -37,3 +31,6 @@ java -Xmx4G -cp $BIOPROGS/tools/sixframe translate \
                       -seq %inc_nucl.content \
                       -mode %codon_table.content \
                       -annot %amino_nucl_rel.content &> ../results/status.log
+
+echo "done" >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
