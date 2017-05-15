8e817ed1c987957c621b897de8a3543e1fe6ba98
Vikram Alva <vikram.alva@gmail.com>
2017-05-15 19:29:23 +0200

    Server side validation for CLANS and MMseqs2

--- tel/runscripts/clans.sh
+++ tel/runscripts/clans.sh
@@ -1,9 +1,48 @@
 JOBID=%jobid.content
 
-SEQ_COUNT=$(egrep '^>' ../params/alignment  -c)
+SEQ_COUNT=$(egrep '^>' ../params/alignment | wc -l)
+CHAR_COUNT=$(wc -m < ../params/alignment)
 
-echo "#Input contains ${SEQ_COUNT} sequences." >> ../results/process.log
+if [ $CHAR_COUNT -gt "10000000" ] ; then
+      echo "#Input may not contain more than 10000000 characters." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      false
+fi
+
+if [ $SEQ_COUNT = "0" ] ; then
+      echo "#Invalid input format. Input should be in FASTA format." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      false
+fi
+
+if [ $SEQ_COUNT -lt "2" ] ; then
+      echo "#Input should contain at least 2 sequences." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      false
+fi
+
+OUTFORMAT=$(reformatValidator.pl fas ufas \
+	        $(readlink -f ../params/alignment) \
+            $(readlink -f ../params/alignment) \
+            -d 160 -uc -l 32000)
+
+if [ "$OUTFORMAT" = "ufas" ] ; then
+    SEQ_COUNT=$(egrep '^>' ../params/alignment | wc -l)
+    echo "#Read ${SEQ_COUNT} sequences." >> ../results/process.log
+    curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+else
+    echo "#Input is not in FASTA format." >> ../results/process.log
+    curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+    false
+fi
+echo "done"  >> ../results/process.log
 curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+
+if [ $SEQ_COUNT -gt "20000" ] ; then
+      echo "#Input contains more than 20000 sequences." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      false
+fi
 
 # set gapopen and gapextend costs depending on given matrix
 GAPOPEN=11
@@ -20,9 +59,6 @@ if [ "%matrix.content" = "BLOSUM45" ] ; then
     GAPOPEN=15
     GAPEXT=2
 fi
-
-echo "done" >> ../results/process.log
-curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
 #remove '-' from sequences, merge sequences split into multiple lines, create an indexed copy of the FASTA file
 prepareForClans.pl %alignment.path ../results/${JOBID}.0.fas ../results/${JOBID}.1.fas
