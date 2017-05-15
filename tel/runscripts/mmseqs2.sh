72f1de4caf766d1142ee379e878d82b3ab2fd9bd
Vikram Alva <vikram.alva@gmail.com>
2017-05-15 19:29:23 +0200

    Server side validation for CLANS and MMseqs2

--- tel/runscripts/mmseqs2.sh
+++ tel/runscripts/mmseqs2.sh
@@ -1,50 +1,54 @@
 JOBID=%jobid.content
 
-SEQ_COUNT=$(egrep '^>' ../params/alignment  -c)
+SEQ_COUNT=$(egrep '^>' ../params/alignment | wc -l)
+CHAR_COUNT=$(wc -m < ../params/alignment)
 
-FORMAT=$(reformatValidator.pl fas ufas \
-	$(readlink -f ../params/alignment) \
-    $(readlink -f ../params/alignment) \
-    -d 160 -uc -l 32000)
-
-if [ $FORMAT != "ufas" ] ; then
-
-      echo "#Input is not in FASTA format." >> ../results/process.log
+if [ $CHAR_COUNT -gt "10000000" ] ; then
+      echo "#Input may not contain more than 10000000 characters." >> ../results/process.log
       curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      echo "error" >> ../results/process.log
+      false
+fi
+
+if [ $SEQ_COUNT = "0" ] ; then
+      echo "#Invalid input format. Input should be in FASTA format." >> ../results/process.log
       curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      $?=1
+      false
 fi
 
 if [ $SEQ_COUNT -lt "2" ] ; then
-
-      echo "#Input contains less than 2 sequences." >> ../results/process.log
+      echo "#Input should contain at least 2 sequences." >> ../results/process.log
       curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      echo "error" >> ../results/process.log
-      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      $?=1
+      false
 fi
 
-if [ $SEQ_COUNT -gt "20000" ] ; then
+OUTFORMAT=$(reformatValidator.pl fas ufas \
+	        $(readlink -f ../params/alignment) \
+            $(readlink -f ../params/alignment) \
+            -d 160 -uc -l 32000)
 
+if [ "$OUTFORMAT" = "ufas" ] ; then
+    SEQ_COUNT=$(egrep '^>' ../params/alignment | wc -l)
+    echo "#Read ${SEQ_COUNT} sequences." >> ../results/process.log
+    curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+else
+    echo "#Input is not in FASTA format." >> ../results/process.log
+    curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+    false
+fi
+echo "done"  >> ../results/process.log
+curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+
+if [ $SEQ_COUNT -gt "20000" ] ; then
       echo "#Input contains more than 20000 sequences." >> ../results/process.log
       curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      echo "error" >> ../results/process.log
-      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-      $?=1
+      false
 fi
 
 THREADS_TO_USE=%THREADS
 
-echo "#Input contains ${SEQ_COUNT} sequences." >> ../results/process.log
-curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
-
 if [ $SEQ_COUNT -lt "100" ] ; then
     THREADS_TO_USE=1
 fi
-
-echo "done" >> ../results/process.log
-curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 
 echo "#Clustering down input set." >> ../results/process.log
 curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
