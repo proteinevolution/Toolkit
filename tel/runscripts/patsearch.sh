426d8931ca2c4af49b6d4c460ec7f838aa63e119
Vikram Alva <vikram.alva@gmail.com>
2017-05-17 17:23:50 +0200

    Validation for PatternSearch

--- tel/runscripts/patsearch.sh
+++ tel/runscripts/patsearch.sh
@@ -1,3 +1,18 @@
+SEQ_COUNT=$(wc -l < ../params/alignment)
+CHAR_COUNT=$(wc -m < ../params/alignment)
+
+if [ $CHAR_COUNT -gt "100" ] ; then
+      echo "#Input may not contain more than 100 characters." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      false
+fi
+
+if [ $SEQ_COUNT -gt "0" ] ; then
+      echo "#Invalid input. Please enter a PROSITE grammar or a regular expression." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      false
+fi
+
 PATTERN=$(less ../params/alignment)
 
 echo "#Searching %standarddb.content DB for sequences '${PATTERN}' pattern." >> ../results/process.log
