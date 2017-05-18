495505c5500c312d29d1881b2ae5739c61cb13e0
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-05-18 15:46:09 +0200

    rewrite reconnect method which saves some lines of code

--- app/assets/ts/websocket.ts
+++ app/assets/ts/websocket.ts
@@ -5,88 +5,64 @@
 
 let ws : WebSocket = null,
     connect      : Function,
-    connected    : boolean = false,
-    connecting   : boolean = false,
-    reconnecting : boolean = false,
-    connectionCountdown : Function,
-    connectionTimeRandomizer : Function,
     onClose   : Function,
     onError   : Function,
     onMessage : Function,
     onOpen    : Function,
-    reconnect : Function,
-    retryCount     : number  = 1,
-    retryCountdown : number  = 0,
-    addJob : Function,
-    retryCountdownInterval : number = null;
+    addJob : Function;
 
+let attempts = 1;
 
 connect = function() : any {
-    if (!connecting) {          // check if we are already connecting
-        reconnecting = false;   // we are not reconnecting
-        connecting   = true;    // we are connecting
-        clearInterval(retryCountdownInterval);  // Remove the timer
         let wsRoute = jsRoutes.controllers.Application.ws;
         let isSecure : boolean = location.protocol === "https:";
         ws = new WebSocket(wsRoute().webSocketURL(isSecure));   // create the new websocket
-        //console.log(ws);
-        //console.log(isSecure);
-        //console.log(location.protocol);
         ws.onopen    = function(evt : Event)        : any { return onOpen(evt); };
         ws.onclose   = function(evt : CloseEvent)   : any { return onClose(evt); };
         ws.onmessage = function(evt : MessageEvent) : any { return onMessage(evt); };
         ws.onerror   = function(evt : ErrorEvent)   : any { return onError(evt); };
         return
-    }
 };
 
-connectionCountdown = function() : any {
-    retryCountdown = retryCountdown - 1;            // count one down
-    $("#offline-alert-timer").text(retryCountdown); // render count down text
-    if (retryCountdown < 1) {   // we are at 0, retry now.
-        retryCountdown = 1;     // reset countdown.
-        clearInterval(retryCountdownInterval);  // remove the interval.
-        return connect();       // reconnect.
-    }
-};
+// use exponential backoff algorithm
 
-connectionTimeRandomizer = function() : any {   // Randomizes the countdown time
-    if (retryCount < 38) { retryCount++; }      // maximum of 38 reconnects before constant change
-    return parseInt("" + Math.random() * 3 * retryCount + 6);   // 38 * 3s + 6s = 120s = 2min
-};
+let generateInterval = function(k : number) {
+    let maxInterval = (Math.pow(2, k) - 1) * 1000;
 
-reconnect = function(force : any) : any{    // reconnect the websocket to the server.
-    if (force == null) { force = false; }   // force is there to ensure a reconnect
-    if ((!reconnecting && !connected && !connecting) || force) {
-        console.log("Trying to Reconnect Websocket.");
-        ws.close();             //close any remaining connection
-        connected = false;      // set the connected status to false
-        connecting = false;     // set the connecting status to false
-        reconnecting = true;    // set the reconnecting status to true
-        retryCountdown = parseInt(connectionTimeRandomizer());  // set the retry countdown
-        $("#offline-alert").fadeIn();   // show the "Reconnecting ..." message
-        connectionCountdown();  // start the countdown
-        retryCountdownInterval = setInterval(connectionCountdown, 1000);
+    if (maxInterval > 30*1000) {
+        maxInterval = 30*1000; // If the generated interval is more than 30 seconds, truncate it down to 30 seconds.
     }
+
+    // generate the interval to a random number between 0 and the maxInterval determined from above
+    return Math.random() * maxInterval;
 };
 
 onOpen = function(event : Event) : any {
+    attempts = 1;
     console.log("Websocket is Connected.");
-    clearInterval(retryCountdownInterval);  // Close all reconnection timers
-    connected = true;               // We are connected now.
-    connecting = false;             // No Longer connecting
-    retryCount = 1;                 // Reset the retry counter
     $("#offline-alert").fadeOut();  // Hide the Offline alert
 };
 
 onError = function(event : ErrorEvent) : any {
-    setTimeout(reconnect(true), 500);
+    $("#offline-alert").fadeIn();   // show the "Reconnecting ..." message
+    let time = generateInterval(attempts);
+    console.log("trying to reconnect in ... " + time);
+    // We've tried to reconnect so increment the attempts by 1
+    attempts++;
+    setTimeout(connect(), time);
 };
 
 onClose = function(event : CloseEvent) : any {
-    connected = false;
-    setTimeout(reconnect, 5000);
+    $("#offline-alert").fadeIn();   // show the "Reconnecting ..." message
+    let time = generateInterval(attempts);
+    console.log("trying to reconnect in ... " + time);
+    // We've tried to reconnect so increment the attempts by 1
+    attempts++;
+    //connected = false;
+    setTimeout(connect(), time);
 };
+
+
 
 onMessage = function(event : MessageEvent) : any {
     let message : any = JSON.parse(event.data);
