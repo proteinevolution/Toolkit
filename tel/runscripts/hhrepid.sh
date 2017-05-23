15d6d7a2175849529f0762dd148d78a896aff844
Vikram Alva <vikram.alva@gmail.com>
2017-05-23 23:26:34 +0200

    Refactoring of runscripts

--- tel/runscripts/hhrepid.sh
+++ tel/runscripts/hhrepid.sh
@@ -1,18 +1,65 @@
 JOBID=%jobid.content
+SEQ_COUNT=$(egrep '^>' ../params/alignment | wc -l)
+CHAR_COUNT=$(wc -m < ../params/alignment)
+FORMAT=$(head -1 ../params/alignment | egrep "^CLUSTAL" | wc -l)
 
-SEQ_COUNT=$(egrep '^>' ../params/alignment  -c)
+if [ ${CHAR_COUNT} -gt "10000000" ] ; then
+      echo "#Input may not contain more than 10000000 characters." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      false
+fi
 
+if [ ${SEQ_COUNT} = "0" ] && [ ${FORMAT} = "0" ] ; then
+      sed 's/[^a-z^A-Z]//g' ../params/alignment > ../params/alignment1
+      CHAR_COUNT=$(wc -m < ../params/alignment1)
 
-if [ $SEQ_COUNT -gt "1" ] ; then
+      if [ ${CHAR_COUNT} -gt "10000" ] ; then
+            echo "#Single protein sequence inputs may not contain more than 10000 characters." >> ../results/process.log
+            curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+            false
+      else
+            sed -i "1 i\>${JOBID}" ../params/alignment1
+            mv ../params/alignment1 ../params/alignment
+      fi
+fi
+
+if [ ${FORMAT} = "1" ] ; then
+      reformatValidator.pl clu fas \
+            $(readlink -f %alignment.path) \
+            $(readlink -f ../results/${JOBID}.fas) \
+            -d 160 -uc -l 32000
+else
+      reformatValidator.pl fas fas \
+            $(readlink -f %alignment.path) \
+            $(readlink -f ../results/${JOBID}.fas) \
+            -d 160 -uc -l 32000
+fi
+
+if [ ! -f ../results/${JOBID}.fas ]; then
+    echo "#Input is not in aligned FASTA/CLUSTAL format." >> ../results/process.log
+    curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+    false
+fi
+
+SEQ_COUNT=$(egrep '^>' ../results/${JOBID}.fas | wc -l)
+
+if [ ${SEQ_COUNT} -gt "2000" ] ; then
+      echo "#Input contains more than 2000 sequences." >> ../results/process.log
+      curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+      false
+fi
+
+if [ ${SEQ_COUNT} -gt "1" ] ; then
        echo "#Query is an MSA with ${SEQ_COUNT} sequences." >> ../results/process.log
        curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 else
        echo "#Query is a single protein sequence." >> ../results/process.log
        curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
 fi
-
 echo "done" >> ../results/process.log
 curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>&1
+
+mv ../results/${JOBID}.fas ../params/alignment
 
 #CHECK IF MSA generation is required or not
 if [ %msa_gen_max_iter.content == "0" ] && [ ${SEQ_COUNT} -gt "1" ] ; then
@@ -58,10 +105,10 @@ curl -X POST http://%HOSTNAME:%PORT/jobs/updateLog/%jobid.content > /dev/null 2>
 hhrepid -qsc 0.$i \
         -i ../results/query.a3m \
         -o ../results/query.hhrepid \
-        -d $HHREPIDPATH/cal_small.hhm \
+        -d ${HHREPIDPATH}/cal_small.hhm \
         -pdir ../results \
-        -tp $HHREPIDPATH/tp.dat \
-        -fp $HHREPIDPATH/fp.dat \
+        -tp ${HHREPIDPATH}/tp.dat \
+        -fp ${HHREPIDPATH}/fp.dat \
         -P %self_aln_pval_threshold.content \
         -T %rep_pval_threshold.content \
         -mrgr %merge_iters.content \
