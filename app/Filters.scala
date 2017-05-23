edfaeab3077e856ef7882f765164930fd71fbd2f
davidmrau <david.rau@student.uni-tuebingen.de>
2017-05-23 12:17:17 +0200

    [ADD] gzip compresssion, [LOG] headers

--- app/Filters.scala
+++ app/Filters.scala
@@ -2,23 +2,14 @@
   * Created by snam on 01.02.17.
   */
 import javax.inject.Inject
-import com.mohiva.play.htmlcompressor.HTMLCompressorFilter
-import com.mohiva.play.xmlcompressor.XMLCompressorFilter
+
 import play.filters.csrf.CSRFFilter
-import play.api.http.HttpFilters
-import play.api.mvc.EssentialFilter
+import play.api.http.DefaultHttpFilters
 import play.filters.gzip.GzipFilter
 
 final class Filters @Inject()(
     gzip: GzipFilter,
-    htmlCompressorFilter: HTMLCompressorFilter,
-    xmlCompressorFilter: XMLCompressorFilter,
     cSRFFilter: CSRFFilter
-) extends HttpFilters {
-
-  override def filters: Seq[EssentialFilter] = Seq(
-    htmlCompressorFilter,
-    xmlCompressorFilter
-  )
+) extends DefaultHttpFilters(gzip) {
 
 }
