6c0e0a3d5a3d3dec66cdb0160ce1833bc01393d6
davidmrau <david.rau@student.uni-tuebingen.de>
2017-05-24 14:36:01 +0200

    [NEW] queue parameter for each tool

--- tel/wrapper.sh
+++ tel/wrapper.sh
@@ -8,23 +8,30 @@ key=`date | md5sum | awk '{print $1}'`
 echo $key > ../key
 if [ "$HOSTNAME" = "olt" ] || [ "$HOSTNAME" = "rye" ]; then
 
-
-JOBID=$(basename $(dirname $(pwd))) 
-
+QUEUE=%QUEUE
+JOBID=$(basename $(dirname $(pwd)))
 
 curl -X POST http://%HOSTNAME:%PORT/jobs/queued/$JOBID/$key
 
 if [ "$HOSTNAME" = "olt" ]
   then
+    if [ $QUEUE = "short" ]; then QUEUE="short.q"; fi
+    if [ $QUEUE = "normal" ]; then QUEUE="long.q"; fi
+
     qsub -sync n \
+         -q $QUEUE \
          -l h_vmem=%MEMORY,h="node502|node503|node504|node505|node506|node507|node508|node509|node510|node511|node512|node513" \
          -cwd  \
          %r | grep -oE "[0-9]+" > jobIDCluster
 
 elif [ "$HOSTNAME" = "rye" ]
   then
+    if [ $QUEUE = "short" ]; then QUEUE="toolkit_immediate"; fi
+    if [ $QUEUE = "normal" ]; then QUEUE="toolkit_normal"; fi
+
       HOSTNAME="rye"
       qsub -sync n \
+               -q $QUEUE \
                -l h_vmem=%MEMORY,h="node33|node34|node35|node36" \
                -cwd  \
                %r | grep -oE "[0-9]+" > jobIDCluster
