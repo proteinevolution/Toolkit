8ed008184765c3e5098366a8055e6d2a9462004a
davidmrau <david.rau@student.uni-tuebingen.de>
2017-05-29 12:00:46 +0200

    [FIX] search tools highligthing on scroll

--- app/assets/ts/resulthelper.ts
+++ app/assets/ts/resulthelper.ts
@@ -10,63 +10,71 @@ let count = 0;
 
 // add scrollcontainer highlighting
 let followScroll = function(element : any) {
-    
-    $(element).on('scroll', function () {
-    let detached;
-
-        try {
-
-            let top = $(element).scrollTop();
-            if(typeof top !== 'undefined'){
-                if (top >= $('#visualization').position().top + 75) {
-                        // detached = $('#collapseMe').detach();
-                        // $('.scrollContainer').append(detached);
 
 
-                        $('.scrollContainer').addClass('fixed');
-                        $('.scrollContainer').removeClass('scrollContainerWhite');
-                        $('.scrollContainerDiv').removeClass('scrollContainerDivWhite');
 
-                } else {
-                    // detached = $('#collapseMe').detach();
-                    // $('.ui-widget-header').append(detached);
+    $(document).ready(function () {
+        $("#alignments").floatingScroll('init');
+        //smoothscroll
+        $('#scrollLinks a').on('click', function (e) {
+            e.preventDefault();
+            $(element).off("scroll");
 
+            $('a').each(function () {
+                $(this).removeClass('colorToggleBar');
+            });
+            $(this).addClass('colorToggleBar');
+        });
+    });
 
-                    $('.scrollContainer').removeClass('fixed');
-                    $('.scrollContainer').addClass('scrollContainerWhite');
-                    $('.scrollContainerDiv').addClass('scrollContainerDivWhite');
-
+    $(element).on("scroll", function(){
+    try {
+        let top = $(element).scrollTop();
+        if (typeof top !== 'undefined') {
+            if (top >= $('#visualization').position().top + 75) {
+                // detached = $('#collapseMe').detach();
+                // $('.scrollContainer').append(detached);
+                $('.scrollContainer').addClass('fixed');
+                $('.scrollContainer').removeClass('scrollContainerWhite');
+                $('.scrollContainerDiv').removeClass('scrollContainerDivWhite');
+            } else {
+                // detached = $('#collapseMe').detach();
+                // $('.ui-widget-header').append(detached);
+                $('.scrollContainer').removeClass('fixed');
+                $('.scrollContainer').addClass('scrollContainerWhite');
+                $('.scrollContainerDiv').addClass('scrollContainerDivWhite');
+            }
+        }
+        // trigger lazyload for loading alignment
+        if (top == $(this).height() - $(window).height()) {
+            if (!loading) {
+                let end = parseInt(shownHits) + parseInt(showMore);
+                end = end < numHits ? end : numHits;
+                if (shownHits != end) {
+                    getHits(shownHits, end, colorAAs).then(function () {
+                    });
                 }
+                shownHits = end;
             }
+        }
 
-            if ($('#flat-slider').visible()) {
-                $("#visualizationScroll").addClass("colorToggleBar");
-                $("#hitlistScroll").removeClass("colorToggleBar");
-                $("#alignmentsScroll").removeClass("colorToggleBar");
-            } else if ($('#alignments').visible(true)) {
-                $("#alignmentsScroll").addClass("colorToggleBar");
-                $("#hitlistScroll").removeClass("colorToggleBar");
-                $("#visualizationScroll").removeClass("colorToggleBar");
-            } else if ($('#htb').visible(true)) {
-                $("#hitlistScroll").addClass("colorToggleBar");
-                $("#alignmentsScroll").removeClass("colorToggleBar");
-                $("#visualizationScroll").removeClass("colorToggleBar");
+        $('#scrollLinks a').each(function () {
+            let currLink = $(this);
+            let  refElement = $(currLink.attr("name"));
+            if (refElement.position().top <= top && refElement.position().top + refElement.height() > top) {
+                $('#scrollLinks a').removeClass("colorToggleBar");
+                currLink.addClass("colorToggleBar");
             }
-            // trigger lazyload for loading alignment
-            if ($(this).scrollTop() == $(this).height() - $(window).height()) {
-                if (!loading) {
-                    let end = parseInt(shownHits) + parseInt(showMore);
-                    end = end < numHits ? end : numHits;
-                    if (shownHits != end) {
-                            getHits(shownHits, end, colorAAs).then(function () {
-                            });
-                    }
-                    shownHits = end;
-                }
+            else {
+                currLink.removeClass("colorToggleBar");
             }
-            $("#alignments").floatingScroll('init');
+        });
 
-        } catch(e) { console.warn(e); }
+    } catch (e) {
+        console.warn(e);
+    }
+
     });
 
 };
+
