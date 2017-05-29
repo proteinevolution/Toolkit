8feae7d160fcc5c0d287e8fbae15a54280572fae
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-05-29 22:48:09 +0200

    language

--- app/assets/ts/mithril/jobview/queued.ts
+++ app/assets/ts/mithril/jobview/queued.ts
@@ -7,7 +7,7 @@ let JobQueuedComponent = {
     },
     view: function(ctrl : any, args : any) {
         return m("div", { "class": "queued-panel", config: foundationConfig }, [
-            m('h5', "Your submission is queueing!"),
+            m('h5', "Your submission is being queued!"),
             m("div", {"class": "processJobIdContainer"},
                 m('p', "Job ID:"),
                 m('p', ' ' + args.job().jobID)),
