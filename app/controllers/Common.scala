afaf1a38948c325317e72a99f34ae38a76da52e3
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-05-30 02:07:27 +0200

    Websocket maintenance alert The maintenance alert can be triggered with a curl command by using the maintenance route Example: curl -u user:password -X POST 'http://olt:6500/maintenance' The user and password must match in the database and the user must be superUser with accountType 10

--- app/controllers/Common.scala
+++ app/controllers/Common.scala
@@ -1,18 +1,23 @@
 package controllers
 
 import java.nio.file.attribute.PosixFilePermission
+
+import modules.CommonModule
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
 import play.modules.reactivemongo.ReactiveMongoComponents
 import play.api.mvc.Controller
 import play.api.http.ContentTypes
+import reactivemongo.bson.BSONDocument
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
 
 /**
   *
   * Created by snam on 12.11.16.
   */
-private[controllers] trait Common extends Controller with ContentTypes with ReactiveMongoComponents {
+private[controllers] trait Common extends Controller with ContentTypes with ReactiveMongoComponents with CommonModule {
 
   var loggedOut = true
 
@@ -38,6 +43,35 @@ private[controllers] trait Common extends Controller with ContentTypes with Reac
       "checkboxes" -> seq(number)
     )(ForwardForm.apply)(ForwardForm.unapply)
   )
+
+  def matchSuperUserToPW(username: String, password: String): Future[Boolean] = {
+
+    findUser(BSONDocument("userData.nameLogin" -> username)).map {
+
+      case Some(user) if user.checkPassword(password) && user.isSuperuser => true
+      case None                                                           => false
+
+    }
+
+  }
+
+  def MaintenanceSecured[A]()(action: Action[A]): Action[A] = Action.async(action.parser) { request =>
+    request.headers
+      .get("Authorization")
+      .flatMap { authorization =>
+        authorization.split(" ").drop(1).headOption.filter { encoded =>
+          new String(org.apache.commons.codec.binary.Base64.decodeBase64(encoded.getBytes)).split(":").toList match {
+            case u :: p :: Nil if Await.result(matchSuperUserToPW(u, p), scala.concurrent.duration.Duration.Inf) => true
+            case _                                                                                               => false
+          }
+        }
+      }
+      .map(_ => action(request))
+      .getOrElse {
+        Future.successful(Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured Area""""))
+        //Future.successful(BadRequest())
+      }
+  }
 }
 // Exceptions
 case class FileException(message: String) extends Exception(message)
