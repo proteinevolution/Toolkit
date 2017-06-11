6574e2792111be5a61dec014fae84ffd5664ff9e
Vikram Alva <vikram.alva@gmail.com>
2017-06-11 18:54:29 +0200

    [REFACTOR] Pick queue using h_rt flag

--- tel/wrapper.sh
+++ tel/wrapper.sh
@@ -17,7 +17,6 @@ updateProcessLog () {
     done
 }
 
-QUEUE=%QUEUE
 JOBID=$(basename $(dirname $(pwd)))
 
 until $(curl -X POST --output /dev/null --silent --head --fail http://%HOSTNAME:%PORT/jobs/queued/$JOBID/$key); do
@@ -27,22 +26,17 @@ done
 
 if [ "$HOSTNAME" = "olt" ]
   then
-    if [ $QUEUE = "short" ]; then QUEUE="short.q"; fi
-    if [ $QUEUE = "normal" ]; then QUEUE="long.q"; fi
-
     qsub -sync n \
+         -l h_rt=%HARDRUNTIME \
          -l h_vmem=%MEMORY,h="node502|node503|node504|node505|node506|node507|node508|node509|node510|node511|node512|node513" \
          -cwd  \
          %r | grep -oE "[0-9]+" > jobIDCluster
 
 elif [ "$HOSTNAME" = "rye" ]
   then
-    if [ $QUEUE = "short" ]; then QUEUE="toolkit_immediate"; fi
-    if [ $QUEUE = "normal" ]; then QUEUE="toolkit_normal"; fi
-
       HOSTNAME="rye"
       qsub -sync n \
-               -q $QUEUE \
+               -l h_rt=%HARDRUNTIME \
                -l h_vmem=%MEMORY,h="node33|node34|node35|node36" \
                -cwd  \
                %r | grep -oE "[0-9]+" > jobIDCluster
