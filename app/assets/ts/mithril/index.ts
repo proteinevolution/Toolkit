b2e5a5384dc734a42a6627cc4b6dcb912a2e1083
Vikram Alva <vikram.alva@gmail.com>
2017-06-26 20:33:21 +0200

    [WIP] Make the 'Recent Updates' section responsive

--- app/assets/ts/mithril/index.ts
+++ app/assets/ts/mithril/index.ts
@@ -218,7 +218,7 @@ let tilescomponent = {
     },
     view: function (ctrl : any) {
         return m("div", {
-                "class": "row article_container small-up-1 medium-up-2 large-up-3"
+                "class": "row article_container small-up-1 medium-up-2 large-up-3 hide-for-small-only"
             },
             ctrl.articles().map(function (article : any) { // TODO this javascript error existed before migration "Uncaught TypeError: Cannot read property 'map' of undefined"
                 return m("div", {
