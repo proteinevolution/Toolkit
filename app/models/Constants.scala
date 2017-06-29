579873639856d1812aff326205a7157b60a07357
Vikram Alva <vikram.alva@gmail.com>
2017-06-29 14:42:52 +0200

    Initially query database for registered users

--- app/models/Constants.scala
+++ app/models/Constants.scala
@@ -21,6 +21,8 @@ trait Constants {
   val maxJobsWithin: Int              = 1 // time in minutes within the max number of jobs is applied
   val maxJobNumDay: Int               = 500 // max number of jobs that can be submitted from one ip within maxJobsWithinDay
   val maxJobsWithinDay: Int           = 1 // time in day within the max number of jobs is applied for a day
+  val deletionThresholdRegistered : Int    = 40 // all jobs of registered users that are older than the given number are permanently deleted everywhere
+  val deletionThreshold           : Int    = 2 // all jobs of non registered users that are older than the given number are permanently deleted everywhere
 
 }
 
