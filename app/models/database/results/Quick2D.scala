e4209ba021ed99c156b56bf6f22874249712703d
Vikram Alva <vikram.alva@gmail.com>
2017-07-17 16:22:19 +0200

    Some more work on Quick2D

--- app/models/database/results/Quick2D.scala
+++ app/models/database/results/Quick2D.scala
@@ -14,10 +14,19 @@ import play.api.libs.json._
 
 case class Quick2DResult(jobID: String,
                          query: Query,
-                         psipred: Psipred
+                         psipred: Psipred,
+                         marcoil: Marcoil,
+                         coils: Coils,
+                         pcoils: Pcoils,
+                         tmhmm: Tmhmm
                         )
 
 case class Psipred(name: String, seq: String, conf: String)
+case class Marcoil(name: String, seq: String)
+case class Coils(name: String, seq: String)
+case class Pcoils(name: String, seq: String)
+case class Tmhmm(name: String, seq: String)
+
 @Singleton
 class Quick2D @Inject()(general: General) {
 
@@ -26,7 +35,11 @@ class Quick2D @Inject()(general: General) {
       val jobID      = (obj \ "jobID").as[String]
       val query = general.parseQuery((obj \ "query").as[JsArray])
       val psipred = parsePsipred((obj \ jobID).as[JsObject])
-      Quick2DResult(jobID, query, psipred)
+      val marcoil = parseMarcoil((obj \ jobID).as[JsObject])
+      val coils = parseCoils((obj \ jobID).as[JsObject])
+      val pcoils = parsePcoils((obj \ jobID).as[JsObject])
+      val tmhmm = parseTmhmm((obj \ jobID).as[JsObject])
+      Quick2DResult(jobID, query, psipred, marcoil, coils, pcoils, tmhmm)
 
   }
 
@@ -35,4 +48,26 @@ class Quick2D @Inject()(general: General) {
     val seq        = (obj \ "psipred").getOrElse(Json.toJson("")).as[String]
     Psipred("psipred", seq, conf)
   }
+
+  def parseMarcoil(obj: JsObject) : Marcoil = {
+    val seq        = (obj \ "marcoil").getOrElse(Json.toJson("")).as[String]
+    Marcoil("marcoil", seq)
+  }
+
+  def parseCoils(obj: JsObject) : Coils = {
+    val seq        = (obj \ "coils_w28").getOrElse(Json.toJson("")).as[String]
+    Coils("coils", seq)
+  }
+
+  def parsePcoils(obj: JsObject) : Pcoils = {
+    val seq        = (obj \ "pcoils_w28").getOrElse(Json.toJson("")).as[String]
+    Pcoils("pcoils", seq)
+  }
+
+  def parseTmhmm(obj: JsObject) : Tmhmm = {
+    val seq        = (obj \ "tmhmm").getOrElse(Json.toJson("")).as[String]
+    Tmhmm("tmhmm", seq)
+  }
+
+
 }
