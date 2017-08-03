d57df9d5bb65e6390e82b59af0bb6aab9a9f5107
Vikram Alva <vikram.alva@gmail.com>
2017-08-03 16:29:54 +0200

    Raise job submission limits, as we now have an additional node

--- app/models/Constants.scala
+++ app/models/Constants.scala
@@ -19,9 +19,9 @@ class Constants {
   val nJobActors                            = 100
   val formMultiValueSeparator: String       = " "
   val modellerKey: String                   = "MODELIRANJE"
-  val maxJobNum: Int                        = 60 // max number of jobs that can be submitted from one ip within maxJobsWithin
+  val maxJobNum: Int                        = 100 // max number of jobs that can be submitted from one ip within maxJobsWithin
   val maxJobsWithin: Int                    = 1 // time in minutes within the max number of jobs is applied
-  val maxJobNumDay: Int                     = 1000 // max number of jobs that can be submitted from one ip within maxJobsWithinDay
+  val maxJobNumDay: Int                     = 1500 // max number of jobs that can be submitted from one ip within maxJobsWithinDay
   val maxJobsWithinDay: Int                 = 1 // time in days within the max number of jobs is applied for a day
   val deletionThresholdRegistered : Int     = 90 // all jobs of registered users that are older than the given number are permanently deleted everywhere
   val deletionThreshold           : Int     = 24 // all jobs of non registered users that are older than the given number are permanently deleted everywhere
