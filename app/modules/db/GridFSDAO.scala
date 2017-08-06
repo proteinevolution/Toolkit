95352598194458965722c319578ca0bd2fcf35b5
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-08-06 19:57:51 +0200

    first save method

--- app/modules/db/GridFSDAO.scala
+++ app/modules/db/GridFSDAO.scala
@@ -1,9 +1,15 @@
 package modules.db
 
-import reactivemongo.api.BSONSerializationPack
-import reactivemongo.api.gridfs.ReadFile
+import javax.inject.Inject
+
+import play.api.libs.iteratee.Enumerator
+import reactivemongo.api.{BSONSerializationPack, MongoConnection, MongoDriver}
+import reactivemongo.api.gridfs.{DefaultFileToSave, GridFS, ReadFile}
 import reactivemongo.bson.BSONValue
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.ExecutionContext.Implicits.global
+import reactivemongo.api.gridfs.Implicits._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 class GridFSDAO {
 
@@ -11,6 +17,32 @@ class GridFSDAO {
   type GFile = ReadFile[BSONSerializationPack.type, BSONValue]
 
 
+  private val dbname = "tkplay_dev"
+  private val uri = "mongodb://balata"
+  private val driver = new MongoDriver()
+  private val connection: Try[MongoConnection] = MongoConnection.parseURI(uri).map {
+    parsedUri => driver.connection(parsedUri)
+  }
+  private val db = connection.get.db(dbname)
+
+
+  def saveResultToGridFS(filename : String, contentType: Option[String], data: Enumerator[Array[Byte]]) : Future[GridFSDAO.this.GFile] = {
+
+    saveToGridFS(GridFS[BSONSerializationPack.type](db, "resultCollection"), filename, contentType, data)
+
+  }
+
+
+  def saveToGridFS( gridfs: GridFS[BSONSerializationPack.type],
+                    filename: String,
+                    contentType: Option[String],
+                    data: Enumerator[Array[Byte]]
+                  )(implicit ec: ExecutionContext): Future[GFile] = {
+    // Prepare the GridFS object to the file to be pushed
+    val gridfsObj = DefaultFileToSave(Some(filename), contentType)
+
+    gridfs.save(data, gridfsObj)
+  }
 
 
 }
