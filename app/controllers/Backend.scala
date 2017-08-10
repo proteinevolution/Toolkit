c740cf162a39af179544d8c44e5f084898a98aef
andrew <andrew.stephens@tuebingen.mpg.de>
2017-08-10 05:53:37 +0200

    [ADD] statistics are now generated from JobEventLogs

--- app/controllers/Backend.scala
+++ app/controllers/Backend.scala
@@ -1,21 +1,20 @@
 package controllers
 
-import javax.inject.{ Inject, Singleton }
+import javax.inject.{Inject, Singleton}
 
-import models.database.jobs.Job
-import models.database.statistics.{ JobEvent, JobEventLog, ToolStatistic }
+import models.database.statistics.{JobEvent, JobEventLog, StatisticsObject, ToolStatistic}
 import models.database.users.User
+import models.tools.ToolFactory
 import modules.LocationProvider
 import modules.db.MongoStore
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, DateTimeZone}
 import play.api.Logger
 import play.api.cache._
-import play.api.i18n.{ I18nSupport, MessagesApi }
-import play.api.libs.json
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.Json
-import play.api.mvc.{ Action, AnyContent, Controller }
+import play.api.mvc.{Action, AnyContent, Controller}
 import play.modules.reactivemongo.ReactiveMongoApi
-import reactivemongo.bson.{ BSONDateTime, BSONDocument, BSONObjectID }
+import reactivemongo.bson.{BSONDateTime, BSONDocument}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -29,6 +28,7 @@ final class Backend @Inject()(webJarAssets: WebJarAssets,
                               userSessions: UserSessions,
                               mongoStore: MongoStore,
                               @NamedCache("userCache") implicit val userCache: CacheApi,
+                              toolFactory: ToolFactory,
                               implicit val locationProvider: LocationProvider,
                               val reactiveMongoApi: ReactiveMongoApi,
                               val messagesApi: MessagesApi)
@@ -49,37 +49,98 @@ final class Backend @Inject()(webJarAssets: WebJarAssets,
 
   def statistics: Action[AnyContent] = Action.async { implicit request =>
     userSessions.getUser.flatMap { user =>
+      Logger.info("Statistics called. Access " + (if (user.isSuperuser) "granted." else "denied."))
       if (user.isSuperuser) {
-        val dateTimeFirstOfMonth: DateTime = DateTime.now().dayOfMonth().withMinimumValue().withTimeAtStartOfDay()
-        mongoStore
-          .findJobEventLogs(
-            BSONDocument(
-              JobEventLog.EVENTS ->
-              BSONDocument(
-                "$elemMatch" ->
-                BSONDocument(
-                  JobEvent.TIMESTAMP ->
-                  BSONDocument("$gte" -> BSONDateTime(dateTimeFirstOfMonth.minusMonths(1).getMillis),
-                               "$lt"  -> BSONDateTime(dateTimeFirstOfMonth.getMillis))
-                )
-              )
-            )
-          )
-          .foreach { jobEventList =>
-            Logger.info(
-              "Found " + jobEventList.length + " Jobs for the last Month (From " + dateTimeFirstOfMonth
-                .minusMonths(1) + " to " + dateTimeFirstOfMonth + ")"
-            )
-          }
+        // Get the first moment of the last month as a DateTime object
+        val firstOfLastMonth: DateTime = DateTime.now().withZone(DateTimeZone.getDefault).minusMonths(1).dayOfMonth().withMinimumValue().withTimeAtStartOfDay()
 
-        mongoStore.getStatistics.map { toolStatisticList: List[ToolStatistic] =>
-          NoCache(Ok(Json.toJson(toolStatisticList)))
+        // Grab the current statistics
+        Logger.info("Loading Statistics...")
+        val stats = mongoStore.getStats.map(_.getOrElse(StatisticsObject()))
+
+        // Ensure all tools are in the statistics, even if they have not been used yet
+        Logger.info("Statistics loaded.... checking for new tools")
+        val statsUpdated = stats.map(_.updateTools(toolFactory.values.values.map(_.toolNameShort).toList))
+
+        // Collect the job events up until the first of the last month
+        statsUpdated.flatMap { statistics =>
+          if (statistics.lastPushed.compareTo(firstOfLastMonth) < 0) {
+            mongoStore.findJobEventLogs(
+              BSONDocument(
+                JobEventLog.EVENTS ->
+                  BSONDocument(
+                    "$elemMatch" ->
+                      BSONDocument(
+                        JobEvent.TIMESTAMP ->
+                          BSONDocument("$lt" -> BSONDateTime(firstOfLastMonth.getMillis))
+                      )
+                  )
+              )
+            ).map { jobEventLogs =>
+              Logger.info("Collected " + jobEventLogs.length + " elements from the job event logs. Last Push: " + statistics.lastPushed)
+              statistics.addMonthsToTools(jobEventLogs, statistics.lastPushed.withZone(DateTimeZone.getDefault).plusMonths(1).dayOfMonth().withMinimumValue().withTimeAtStartOfDay(), firstOfLastMonth)
+            }.flatMap { statisticsObject =>
+              mongoStore.updateStats(statisticsObject).map {
+                case Some(statisticsObjectUpdated) =>
+                  Logger.info("Successfully pushed statistics for Months: " + statisticsObjectUpdated.datePushed.filterNot(a => statistics.datePushed.contains(a)).mkString(", "))
+                  // TODO add a way to remove the now collected elements from the JobEventLogs
+                  NoCache(Ok(Json.toJson(Json.obj("success" -> "new statistics added", "stat" -> statisticsObjectUpdated))))
+                case None =>
+                  Logger.info("Statistics generated, but it seems like the statistics could not be reloaded from the db")
+                  NoCache(Ok(Json.toJson(Json.obj("error" -> "could not reload new stats from DB", "stat" -> statisticsObject))))
+              }
+            }
+          } else {
+            Logger.info("No need to push statistics. Last Push: " + statistics.lastPushed)
+            Future.successful(NoCache(Ok(Json.toJson(Json.obj("success" -> "old statistics used", "stat" -> statistics)))))
+          }
         }
       } else {
         Future.successful(NotFound)
       }
     }
   }
+
+//  def statistics: Action[AnyContent] = Action.async { implicit request =>
+//    userSessions.getUser.flatMap { user =>
+//      if (user.isSuperuser) {
+//        val dateTimeFirstOfMonth: DateTime = DateTime.now().dayOfMonth().withMinimumValue().withTimeAtStartOfDay()
+//        val lastMonthQuery =
+//          BSONDocument("$gte" -> BSONDateTime(dateTimeFirstOfMonth.minusMonths(1).getMillis),
+//                       "$lt"  -> BSONDateTime(dateTimeFirstOfMonth.getMillis))
+//        val currentMonthJobs = mongoStore
+//          .findJobEventLogs(
+//            BSONDocument(
+//              JobEventLog.EVENTS ->
+//              BSONDocument(
+//                "$elemMatch" ->
+//                BSONDocument(
+//                  JobEvent.TIMESTAMP ->
+//                  BSONDocument("$gte" -> BSONDateTime(dateTimeFirstOfMonth.getMillis))
+//                )
+//              )
+//            )
+//          )
+//
+//        val jobEventsSortedByTool = currentMonthJobs.map{ jobEventList =>
+//          JobEventLog.toSortedMap(jobEventList)
+//        }
+//        jobEventsSortedByTool.map(_.map{ toolStatMap =>
+//          val toolName = toolStatMap._1
+//          val toolStat = toolStatMap._2
+//          ToolStatistic(toolName,
+//                        List(toolStat.length),
+//                        List(toolStat.count(_.hasFailed)),
+//                        List(toolStat.count(_.isDeleted)))
+//        })
+//        mongoStore.getStatistics.map { toolStatisticList: List[ToolStatistic] =>
+//          NoCache(Ok(Json.toJson(toolStatisticList)))
+//        }
+//      } else {
+//        Future.successful(NotFound)
+//      }
+//    }
+//  }
 
   def cms: Action[AnyContent] = Action.async { implicit request =>
     userSessions.getUser.flatMap { user =>
