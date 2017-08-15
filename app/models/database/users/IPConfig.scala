a8c6b8366d7ede95c6d67d613ff7bd9b455e22dd
andrew <andrew.stephens@tuebingen.mpg.de>
2017-08-15 07:26:23 +0200

    [ADD] added maximum allowed users per IP to IPConfig object

--- app/models/database/users/IPConfig.scala
+++ app/models/database/users/IPConfig.scala
@@ -9,41 +9,48 @@ import org.joda.time.format.DateTimeFormat
 import play.api.libs.json.{ JsObject, Json, Writes }
 import reactivemongo.bson._
 
-case class IPConfig(id          : BSONObjectID = BSONObjectID.generate(), // ID in MongoDB
-                    hashIP      : String,
-                    score       : Int = 0,
-                    scoreMax    : Int = IPConfig.scoreMaxDef,
-                    dateCreated : Option[DateTime] = Some(DateTime.now), // Creation date
-                    dateUpdated : Option[DateTime] = Some(DateTime.now)) { // Last used on
-
+case class IPConfig(id           : BSONObjectID = BSONObjectID.generate(), // ID in MongoDB
+                    ipHash       : String,
+                    openSessions : Int = 0,
+                    score        : Int = 0,
+                    scoreMax     : Int = IPConfig.scoreMaxDefault,
+                    dateCreated  : Option[DateTime] = Some(DateTime.now), // Creation date
+                    dateUpdated  : Option[DateTime] = Some(DateTime.now)) { // Last used on
+  def isInLimits : Boolean = {
+    score        <= scoreMax &&
+    openSessions <= IPConfig.sessionsMax
+  }
 }
 
 object IPConfig {
   // Standard computation scoring unit
-  final val scoreMaxDef        : Int = 1000
+  final val sessionsMax        : Int = Int.MaxValue - 1 // TODO we may need to limit the amount of sessions, but at the same time we need to reset them again - so a good limit has to be found
+  final val scoreMaxDefault    : Int = 1000
   final val scoreIgnoreRequest : Int = -1
 
-  // Constants for the JSON object identifiers
-  final val ID            = "id" // name for the ID in scala
-  final val IDDB          = "_id" //              ID in MongoDB
-  final val HASHIP        = "hashIP"
-  final val SCORE         = "score"
-  final val SCOREMAX      = "scoreMax"
-  final val DATECREATED   = "dateCreated"
-  final val DATEUPDATED   = "dateUpdated"
+  // Constants for the JSON object identifiers // Field Description
+  final val ID           = "id"                // ID in scala
+  final val IDDB         = "_id"               // ID in MongoDB
+  final val IPHASH       = "ipHash"            // hashed IP address
+  final val OPENSESSIONS = "openSessions"      // Created sessions so far
+  final val SCORE        = "score"             // Score for created jobs
+  final val SCOREMAX     = "scoreMax"          // Maximum score the user can have
+  final val DATECREATED  = "dateCreated"       // Date the object was created
+  final val DATEUPDATED  = "dateUpdated"       // Date the object was last updated
 
   /**
     * Define how the User object is formatted when turned into a json object
     */
   implicit object JobWrites extends Writes[IPConfig] {
     val dtf = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss")
-    def writes(user: IPConfig): JsObject = Json.obj(
-      ID            -> user.id.stringify,
-      HASHIP        -> user.hashIP,
-      SCORE         -> user.score,
-      SCOREMAX      -> user.scoreMax,
-      DATECREATED   -> user.dateCreated.map(dt => dtf.print(dt)),
-      DATEUPDATED   -> user.dateUpdated.map(dt => dtf.print(dt))
+    def writes(ipConfig: IPConfig): JsObject = Json.obj(
+      ID            -> ipConfig.id.stringify,
+      IPHASH        -> ipConfig.ipHash,
+      OPENSESSIONS  -> ipConfig.openSessions,
+      SCORE         -> ipConfig.score,
+      SCOREMAX      -> ipConfig.scoreMax,
+      DATECREATED   -> ipConfig.dateCreated.map(dt => dtf.print(dt)),
+      DATEUPDATED   -> ipConfig.dateUpdated.map(dt => dtf.print(dt))
     )
   }
 
@@ -53,24 +60,26 @@ object IPConfig {
   implicit object Reader extends BSONDocumentReader[IPConfig] {
     override def read(bson: BSONDocument): IPConfig =
       IPConfig(
-        id          = bson.getAs[BSONObjectID](IDDB).get,
-        hashIP      = bson.getAs[String](HASHIP).get,
-        score       = bson.getAs[Int](SCORE).get,
-        scoreMax    = bson.getAs[Int](SCOREMAX).get,
-        dateCreated = bson.getAs[BSONDateTime](DATECREATED).map(dt => new DateTime(dt.value)),
-        dateUpdated = bson.getAs[BSONDateTime](DATEUPDATED).map(dt => new DateTime(dt.value))
+        id           = bson.getAs[BSONObjectID](IDDB).get,
+        ipHash       = bson.getAs[String](IPHASH).get,
+        openSessions = bson.getAs[Int](OPENSESSIONS).getOrElse(0),
+        score        = bson.getAs[Int](SCORE).getOrElse(0),
+        scoreMax     = bson.getAs[Int](SCOREMAX).getOrElse(IPConfig.scoreMaxDefault),
+        dateCreated  = bson.getAs[BSONDateTime](DATECREATED).map(dt => new DateTime(dt.value)),
+        dateUpdated  = bson.getAs[BSONDateTime](DATEUPDATED).map(dt => new DateTime(dt.value))
       )
   }
 
   implicit object Writer extends BSONDocumentWriter[IPConfig] {
-    override def write(user: IPConfig): BSONDocument =
+    override def write(ipConfig: IPConfig): BSONDocument =
       BSONDocument(
-        IDDB          -> user.id,
-        HASHIP        -> user.hashIP,
-        SCORE         -> user.score,
-        SCOREMAX      -> user.scoreMax,
-        DATECREATED   -> BSONDateTime(user.dateCreated.fold(-1L)(_.getMillis)),
-        DATEUPDATED   -> BSONDateTime(user.dateUpdated.fold(-1L)(_.getMillis))
+        IDDB         -> ipConfig.id,
+        IPHASH       -> ipConfig.ipHash,
+        OPENSESSIONS -> ipConfig.openSessions,
+        SCORE        -> ipConfig.score,
+        SCOREMAX     -> ipConfig.scoreMax,
+        DATECREATED  -> BSONDateTime(ipConfig.dateCreated.fold(-1L)(_.getMillis)),
+        DATEUPDATED  -> BSONDateTime(ipConfig.dateUpdated.fold(-1L)(_.getMillis))
       )
   }
 }
