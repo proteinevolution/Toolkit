d0322f1c63a149ea4e1cf2ed95e2a6cab8a14c53
Felix Gabler <felix.gabler@tuebingen.mpg.de>
2017-08-22 15:43:07 +0200

    FIX webjars d3 error

--- app/controllers/Common.scala
+++ app/controllers/Common.scala
@@ -1,6 +1,5 @@
 package controllers
 
-
 import play.modules.reactivemongo.ReactiveMongoComponents
 import play.api.mvc._
 import play.api.http.ContentTypes
@@ -11,15 +10,11 @@ import play.api.http.ContentTypes
   */
 private[controllers] trait Common extends AbstractController with ContentTypes with ReactiveMongoComponents {
 
-
   protected def NoCache(res: Result): Result = res.withHeaders(
     CACHE_CONTROL -> "no-cache, no-store, must-revalidate",
     EXPIRES       -> "0"
   )
 
-
 }
 // Exceptions
 case class FileException(message: String) extends Exception(message)
-
-
