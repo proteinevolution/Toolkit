74b58e9511929a458f241f3d8c4eef4289b6bd5d
andrew <andrew.stephens@tuebingen.mpg.de>
2017-09-16 04:05:39 +0200

    [REMOVE] Sweep job service and DeletedJobs

--- app/Module.scala
+++ app/Module.scala
@@ -2,14 +2,13 @@
   * Created by snam on 22.05.17.
   */
 import com.google.inject.AbstractModule
-import services.{ Configuration, ConfigurationImpl, SweepJobs, SweepJobsImpl }
+import services.{ Configuration, ConfigurationImpl }
 
 class Module extends AbstractModule {
 
   override def configure() = {
 
     bind(classOf[Configuration]).to(classOf[ConfigurationImpl]).asEagerSingleton()
-    bind(classOf[SweepJobs]).to(classOf[SweepJobsImpl]).asEagerSingleton()
 
   }
 
