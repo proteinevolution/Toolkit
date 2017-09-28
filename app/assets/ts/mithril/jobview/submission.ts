ef86051e988356444f36d8f679177f84d87c9f5a
andrew <andrew.stephens@tuebingen.mpg.de>
2017-09-28 04:22:08 +0200

    [FIX] jobID check now checks against oldJobID

--- app/assets/ts/mithril/jobview/submission.ts
+++ app/assets/ts/mithril/jobview/submission.ts
@@ -1,23 +1,26 @@
 (<any>window).JobSubmissionComponent = {
     submitting      : false,    // Job is being sent if true
+    oldJobID        : null,     // jobID of the old job, if there is an old job
     currentJobID    : null,     // Currently entered jobID
     jobIDValid      : false,    // Is the current jobID valid?
     jobIDValidationTimeout : null,     // timer ID for the timeout
     jobIDRegExp     : new RegExp(/^([0-9a-zA-Z]{3,96})(_([0-9]{1,3}))?$/),
-    jobResubmit     : false,
     checkJobID : function (jobID : string) {
+        // ensure that the user can not send the job form
+        JobSubmissionComponent.jobIDValid = false;
         // clear all previous timeouts
         clearTimeout(JobSubmissionComponent.jobIDValidationTimeout);
-        // ensure that the user can not send the job form
-        JobSubmissionComponent.jobIDValid   = false;
-        // set the jobID to the new jobID
-        if (jobID != null) JobSubmissionComponent.currentJobID = jobID;
         // ignore checking if the field is empty as the server will generate a jobID in that case.
-        if (jobID !== "") {
+        if (jobID === "" || jobID == null) {
+            JobSubmissionComponent.currentJobID = "";
+            JobSubmissionComponent.jobIDValid   = true;
+        } else {
+            // set the jobID to the new jobID
+            JobSubmissionComponent.currentJobID = jobID;
             if (JobSubmissionComponent.jobIDRegExp.test(jobID)) {   // Check if the JobID is passing the Regex
                 let checkJobIDroute = jsRoutes.controllers.Search.checkJobID(
                                        JobSubmissionComponent.currentJobID,
-                                       JobSubmissionComponent.jobResubmit);
+                                       JobSubmissionComponent.oldJobID);
                 m.request({ method: checkJobIDroute.method, url: checkJobIDroute.url}).then(
                     function (data : any) {
                         console.log(data);
@@ -27,17 +30,15 @@
                             JobSubmissionComponent.jobIDValid = true;
                         }
                         console.log("Current JobID is:",    JobSubmissionComponent.currentJobID,
-                                    "suggested version",    data.version,
-                                    "valid:",               JobSubmissionComponent.jobIDValid,
-                                    "we are resubmitting?", JobSubmissionComponent.jobResubmit);
+                                    "Old jobID is:",        JobSubmissionComponent.oldJobID,
+                                    "Suggested version:",   data.version,
+                                    "Current jobID Valid?", JobSubmissionComponent.jobIDValid);
                     },
                     function(data : any) {
                         console.log(data);
                     }
                 );
             }
-        } else {
-            JobSubmissionComponent.jobIDValid = true;
         }
         return JobSubmissionComponent.currentJobID;
     },
@@ -63,12 +64,12 @@
         }
     },
     jobIDComponent : function (ctrl : any) {
-        let style : string = "jobid";
+        let style : string = "jobID";
         style += JobSubmissionComponent.currentJobID === "" ? " white" :
-            (JobSubmissionComponent.jobIDValid          ? " green" : " red");
+                (JobSubmissionComponent.jobIDValid          ? " green" : " red");
         return m("input", { type:        "text",
                             id:          "jobID",
-                            "class":       style,
+                            "class":     style,
                             placeholder: "Custom JobID",
                             onkeyup:     m.withAttr("value", JobSubmissionComponent.checkJobIDTimed(800)),
                             onchange:    m.withAttr("value", JobSubmissionComponent.checkJobID),
@@ -76,10 +77,22 @@
         });
     },
     controller: function(args : any) {
+        console.log("[submission.ts] controller startup: ", args.isJob, args.job());
+        if (args.isJob) {
+            if (JobSubmissionComponent.oldJobID !== args.job().jobID) {
+                JobSubmissionComponent.currentJobID = null;
+                JobSubmissionComponent.oldJobID     = null;
+            }
+        } else {
+            JobSubmissionComponent.oldJobID = null;
+        }
         if (JobSubmissionComponent.currentJobID == null) {
-            JobSubmissionComponent.jobResubmit = args.isJob;
-            JobSubmissionComponent.jobIDValid  = false;
-            JobSubmissionComponent.checkJobID(""); // ask server for new jobID
+            if (args.isJob) {
+                JobSubmissionComponent.oldJobID = args.job().jobID;
+                JobSubmissionComponent.checkJobID(JobSubmissionComponent.oldJobID); // ask server for new jobID
+            } else {
+                JobSubmissionComponent.checkJobID("");
+            }
         }
         return {
             submit: function(startJob : boolean) {
