43321f666d26b81bddbd6f2735d39704daf4a4c2
Felix Gabler <felix.gabler@tuebingen.mpg.de>
2017-12-23 13:45:39 +0100

    Add typescript default types for tooltip

--- app/assets/ui/configs.ts
+++ app/assets/ui/configs.ts
@@ -5,7 +5,7 @@ $(document).ready(function(){
     Foundation.Tooltip.defaults.clickOpen = false;
     Foundation.Tooltip.defaults.position = "bottom";
     Foundation.Tooltip.defaults.alignment = "center";
-    Foundation.Tooltip.defaults.allowBottomOverlap = "true";
+    Foundation.Tooltip.defaults.allowOverlap = true;
     $(document).foundation();
     $('#message-container').delay(2000).fadeOut('slow');
 });
