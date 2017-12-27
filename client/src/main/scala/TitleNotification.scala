d19581ccc1d77c2a2b2d58cde7546cbfbc4926b9
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2017-12-28 00:03:46 +0100

    Closes #233 : all code should be compliant with Apache 2.0 now

---
+++ client/src/main/scala/TitleNotification.scala
@@ -0,0 +1,27 @@
+import org.scalajs.dom.raw.Node
+import scala.scalajs.js.annotation._
+
+@JSExportTopLevel("TitleNotification")
+object TitleNotification {
+
+  private val title: Node = org.scalajs.dom.document.getElementsByTagName("title").item(0)
+  private val normalTitle = title.textContent
+  private var titleCounter = 0
+
+  @JSExport
+  def set(n: Int): Unit = {
+    if (titleCounter < Int.MaxValue) {
+      titleCounter += n
+      title.textContent = s"($titleCounter) " + normalTitle
+    }
+    else
+      title.textContent = s"($titleCounter +) " + normalTitle
+  }
+
+  @JSExport
+  def reset(): Unit = {
+    titleCounter = 0
+    title.textContent = normalTitle
+  }
+
+} 
