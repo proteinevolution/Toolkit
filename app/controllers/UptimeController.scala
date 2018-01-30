af4b186b6217a3339765d1c8d5694e181a41236d
Seung-Zin Nam <z.nam@envisia.de>
2018-01-30 22:16:22 +0100

    dedup buildinfo

--- app/controllers/UptimeController.scala
+++ app/controllers/UptimeController.scala
@@ -8,26 +8,13 @@ class UptimeController @Inject()(cc: ControllerComponents) extends AbstractContr
 
   private val startTime: Long = System.currentTimeMillis()
 
-  private val versionString = {
-    new java.lang.StringBuilder()
-      .append("Version: ")
-      .append(BuildInfo.version)
-      .append(" on Scala ")
-      .append(BuildInfo.scalaVersion)
-      .append(" with Sbt ")
-      .append(BuildInfo.sbtVersion)
-      .append(" and Play! ")
-      .append(BuildInfo.playVersion)
-      .toString
-  }
-
   def uptime = Action {
     val uptimeInMillis = System.currentTimeMillis() - startTime
     Ok(s"$uptimeInMillis ms")
   }
 
   def buildInfo = Action {
-    Ok(s"$versionString")
+    Ok(s"${BuildInfo.toString}")
   }
 
 }
