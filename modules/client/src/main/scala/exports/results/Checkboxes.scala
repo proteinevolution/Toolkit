eddf7240a6aaa8467a0c0fb1e27511a4f6eee9fd
Seung-Zin Nam <z.nam@envisia.de>
2018-01-31 00:05:45 +0100

    checkbox wip

--- modules/client/src/main/scala/exports/results/Checkboxes.scala
+++ modules/client/src/main/scala/exports/results/Checkboxes.scala
@@ -1,11 +1,13 @@
 package exports.results
 
 import scala.scalajs.js.annotation.{ JSExport, JSExportTopLevel }
+import org.scalajs.dom
+
 
 /**
  * Checkbox handler for result pages
- *
  */
+
 @JSExportTopLevel("Checkboxes")
 object Checkboxes {
 
@@ -13,6 +15,18 @@ object Checkboxes {
   def link() = {
 
     ???
+  }
+
+  @JSExport
+  def check(): Unit = {
+    val checkboxes = dom.document.querySelector("input:checkbox")
+    checkboxes.addEventListener(
+      "change", { (event: dom.Event) =>
+        event.preventDefault()
+        dom.console.log("clicked")
+      },
+      false
+    )
   }
 
 }
