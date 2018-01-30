f19c617dab54730ae8b0bca3a22598b4fd75a046
Seung-Zin Nam <z.nam@envisia.de>
2018-01-30 01:11:06 +0100

    shrink blastvisualization

--- public/javascripts/blast-visualization.js
+++ public/javascripts/blast-visualization.js
@@ -1,66 +1,7 @@
-function download(filename, text) {
-    var blob = new Blob([text], {type: "octet/stream"});
-    if (window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveBlob(blob, filename);
-    } else {
-        var a = document.createElement("a");
-        a.style = "display: none";
-        a.href = window.URL.createObjectURL(blob);
-        a.download = filename;
-        // Append anchor to body.
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(a.href);
-        // Remove anchor from body
-        a.remove();
-    }
-    $.LoadingOverlay("hide");
-}
-
-
-// load forwarded data into alignment field
-$(document).ready(function () {
-    var resultcookie = localStorage.getItem("resultcookie");
-    $('#alignment').val(resultcookie);
-    localStorage.removeItem("resultcookie");
-    $.LoadingOverlay("hide");
-});
-
-
 function selectFromArray(checkboxes){
     _.range(1, numHits+1).forEach(function (currentVal) {
         $('input:checkbox[value='+currentVal+'][name="alignment_elem"]').prop('checked', checkboxes.indexOf(currentVal) !== -1);
     })
-}
-
-function hitlistBaseFunctions(){
-    $(document).ready(function() {
-        // add tooltipster to visualization
-        $('#blastviz').find('area').tooltipster({
-            theme: ['tooltipster-borderless', 'tooltipster-borderless-customized'],
-            position: 'bottom',
-            animation: 'fade',
-            contentAsHTML: true,
-            debug: false,
-            maxWidth: $(this).innerWidth() * 0.6
-        });
-        $.LoadingOverlay("hide");
-        followScroll(document);
-
-        // add slider val
-        $('.slider').on('moved.zf.slider', function () {
-            $('#lefthandle').html($('#hidden1').val()).css({
-                'color': 'white',
-                'font-weight': 'bold',
-                'padding-left': '2px'
-            });
-            $('#righthandle').html($('#hidden2').val()).css({
-                'color': 'white',
-                'font-weight': 'bold',
-                'padding-left': '2px'
-            });
-        });
-    });
 }
 
 Array.prototype.removeDuplicates = function () {
