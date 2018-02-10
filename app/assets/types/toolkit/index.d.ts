ba5da939f63e070dde7798698e5c06fb5bbaf475
Felix Gabler <felix.gabler98@gmail.com>
2018-02-10 23:49:03 +0100

    Correct type def

--- app/assets/types/toolkit/index.d.ts
+++ app/assets/types/toolkit/index.d.ts
@@ -79,7 +79,7 @@ declare class Backend{
 
 declare class Toolkit{
     public static currentJobID : string;
-    public static trackedJobIDs: Array;
+    public static trackedJobIDs: Array<number>;
     public static notFoundJobID: string;
     public static controller : any;
     public static view: any;
