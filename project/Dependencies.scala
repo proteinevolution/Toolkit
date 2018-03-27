2895cb871165ab6d152c5b787231f735ab68d0be
Felix Gabler <felix.gabler98@gmail.com>
2018-03-27 23:28:42 +0200

    Bump frontend deps (#322)
    * Value for min query coverage was not set correctly (#296)
    
    * HHpred: new version of the ECOD database
    
    * Setting version to 0.9.7
    
    * Setting version to 0.9.8-SNAPSHOT
    
    * Ecod new version (#304)
    
    * HHpred: new version of the ECOD database
    
    * Setting version to 0.9.7
    
    * Setting version to 0.9.8-SNAPSHOT
    
    * HHpred: new version of SCOPe DB; v2.07
    
    * Setting version to 0.9.8
    
    * Setting version to 0.9.9-SNAPSHOT
    
    * SCOPe new version (#306)
    
    * HHpred: new version of the ECOD database
    
    * Setting version to 0.9.7
    
    * Setting version to 0.9.8-SNAPSHOT
    
    * HHpred: new version of SCOPe DB; v2.07
    
    * Setting version to 0.9.8
    
    * Setting version to 0.9.9-SNAPSHOT
    
    * New forwarding option for HHblits
    
    * Setting version to 0.9.9
    
    * Setting version to 0.9.10-SNAPSHOT
    
    * Updateversion (#312)
    
    * HHpred: new version of the ECOD database
    
    * Setting version to 0.9.7
    
    * Setting version to 0.9.8-SNAPSHOT
    
    * HHpred: new version of SCOPe DB; v2.07
    
    * Setting version to 0.9.8
    
    * Setting version to 0.9.9-SNAPSHOT
    
    * Setting version to 0.9.9
    
    * Setting version to 0.9.10-SNAPSHOT
    
    * Increase memory available to retseq
    
    * Setting version to 0.9.10
    
    * Setting version to 0.9.11-SNAPSHOT
    
    * MMseqs2: allow forwarding to MSA tools
    
    * HHrepID: modified default settings and MSA generation
    
    HHrepID can detect more divergent repeats now.
    
    * Setting version to 0.9.11
    
    * Setting version to 0.9.12-SNAPSHOT
    
    * #318, fix jobID blur
    * the tooltip will now be visible at all times if input has focus. I don't think this is necessarily bad.
    * checkJobID function extracted
    
    * #318, fix jobID blur (#320)
    
    * the tooltip will now be visible at all times if input has focus. I don't think this is necessarily bad.
    * checkJobID function extracted
    
    * #318, remove duplicate tooltip
    
    * Merge branch 'master' into customJobId
    
    sorry for the git mess
    
    # Conflicts:
    #	app/assets/ui/app/jobview/submission.ts
    
    * #321, tooltips should be consistently loaded now
    #225, improve foundation calls to get rid of the warnings
    
    * Bump frontend deps
    
    * #321, tooltip initialization reworked
    * Tooltips will now be rendered when mithril component is rendered (config-method). This way they only get initialized once.
    
    * Setting version to 0.9.12
    
    * Setting version to 0.9.13-SNAPSHOT
    
    * fix fmt (#325)
    
    * fix index css identifier (#326)
    
    * more (last) Scss changes (#323)
    
    * #273, more changes on jobview css
    
    * #273, more scss changes
    
    * #323, auth overlay not transparent anymore and more scss changes
    
    * Bump frontend deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,19 +42,19 @@ object Dependencies {
   )
 
   lazy val frontendDeps = Seq(
-    "org.webjars"       % "jquery"              % "3.2.1",
+    "org.webjars"       % "jquery"              % "3.3.1",
     "org.webjars.bower" % "jquery.lazyload"     % "1.9.7",
     "org.webjars"       % "jquery-ui"           % "1.12.1",
     "org.webjars.npm"   % "foundation-sites"    % "6.4.3",
     "org.webjars.npm"   % "mithril"             % "0.2.8", // 1.1.3 available
     "org.webjars.npm"   % "codemirror-minified" % "5.28.0",
     "org.webjars.bower" % "datatables"          % "1.10.16",
-    "org.webjars"       % "highcharts"          % "6.0.4",
+    "org.webjars"       % "highcharts"          % "6.0.7",
     "org.webjars.bower" % "velocity"            % "1.5.0",
-    "org.webjars"       % "font-awesome"        % "4.7.0",
+    "org.webjars"       % "font-awesome"        % "5.0.6",
     "org.webjars"       % "select2"             % "4.0.3",
     "org.webjars.npm"   % "tooltipster"         % "4.2.5",
-    "org.webjars"       % "momentjs"            % "2.18.1",
+    "org.webjars"       % "momentjs"            % "2.21.0",
     "org.webjars.bower" % "typeahead.js"        % "0.11.1",
     "org.webjars.npm"   % "snapsvg"             % "0.5.1",
     "org.webjars.npm"   % "jquery-nice-select"  % "1.1.0",
