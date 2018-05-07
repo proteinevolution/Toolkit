64b554559a9a18ded5d02bbfcc209979b70a118e
Felix Gabler <felix.gabler98@gmail.com>
2018-05-07 22:54:48 +0200

    Unify TemplateAlignmentModal #346

--- modules/client/src/main/scala/exports/results/TemplateAlignment.scala
+++ modules/client/src/main/scala/exports/results/TemplateAlignment.scala
@@ -1,16 +1,40 @@
 package exports.results
 
+import org.scalajs.dom.raw.HTMLSelectElement
 import org.scalajs.jquery._
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("TemplateAlignment")
-class TemplateAlignment(tool: String) {
+class TemplateAlignment(tool: String, forwardingEnabled: Boolean = true) {
+
+  private val templateAlignmentModal = jQuery("#templateAlignmentModal")
+  private val textArea = templateAlignmentModal.find("textarea.alignmentTemplateTextArea")
+  private val toolSelect = templateAlignmentModal.find(".alignmentTemplateToolSelect")
+
+  if (forwardingEnabled) {
+    templateAlignmentModal.find(".hide-for-forwarding-disabled").show()
+    toolSelect.on("click", { selectElement: HTMLSelectElement => {
+      val forwardData = textArea.value().toString
+      val tool = selectElement.value
+      if (tool != "") {
+        templateAlignmentModal.asInstanceOf[exports.facades.JQuery].foundation("close")
+        Forwarding.simple(tool, forwardData)
+      }
+    }
+    }: js.ThisFunction)
+  } else {
+    templateAlignmentModal.find(".hide-for-forwarding-disabled").hide()
+  }
+
+  templateAlignmentModal.on("open.zf.reveal", () => {
+    toolSelect.value("")
+    textArea.value("")
+  })
 
   @JSExport
   def get(jobID: String, accession: String): Unit = {
-    val textArea = jQuery("textarea.alignmentTemplateTextArea")
     textArea.asInstanceOf[exports.facades.JQuery].LoadingOverlay("show")
     val acc = accession.replace("#", "%23")
     val extension = tool match {
