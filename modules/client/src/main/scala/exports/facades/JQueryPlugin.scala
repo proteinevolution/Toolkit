e48f6ea56b426dbd50659f983373ec0e24782cf2
Seung-Zin Nam <zin@mailbox.org>
2018-05-08 13:21:59 +0200

    use implicits to monkey patch jquery plugins with nicer syntax (#356)

---
+++ modules/client/src/main/scala/exports/facades/JQueryPlugin.scala
@@ -0,0 +1,28 @@
+package exports.facades
+
+import scala.language.implicitConversions
+import org.scalajs.jquery.{ JQuery, JQueryStatic }
+
+import scala.scalajs.js
+
+object JQueryPlugin {
+
+  @js.native
+  trait JQueryStaticPlugin extends JQueryStatic {
+    def LoadingOverlay(action: String): JQueryStaticPlugin = js.native
+  }
+
+  @js.native
+  trait JQueryPlugin extends JQuery {
+    def LoadingOverlay(action: String): JQueryStaticPlugin         = js.native
+    def tooltipster(settings: js.Dictionary[js.Any]): JQueryPlugin = js.native
+    def foundation(action: String = null): JQueryPlugin            = js.native
+    def slider(options: js.Dictionary[Any]): JQueryPlugin          = js.native
+    def slider(action: String, target: String): JQueryPlugin       = js.native
+  }
+
+  implicit def jqPlugin(jq: JQuery): JQueryPlugin = jq.asInstanceOf[JQueryPlugin]
+
+  implicit def jqStaticPlugin(jq: JQueryStatic): JQueryStaticPlugin = jq.asInstanceOf[JQueryStaticPlugin]
+
+}
