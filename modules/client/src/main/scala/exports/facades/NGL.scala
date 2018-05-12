afbddc87a44a64af0f0c6505b16ad830b8e5227a
Felix Gabler <felix.gabler98@gmail.com>
2018-05-12 18:38:48 +0200

    move structuremodal into own component and load structure as blob

--- modules/client/src/main/scala/exports/facades/NGL.scala
+++ modules/client/src/main/scala/exports/facades/NGL.scala
@@ -1,5 +1,7 @@
 package exports.facades
 
+import org.scalajs.dom.raw.Blob
+
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
 
@@ -8,8 +10,12 @@ import scala.scalajs.js.annotation.JSGlobal
 object NGL extends js.Object {
 
   @js.native
-  class Stage(elementId: String) extends js.Any {
+  class Stage(elementId: String, settings: js.Dictionary[Any] = js.Dictionary()) extends js.Any {
+    def loadFile(url: Blob, options: js.Dictionary[Any]): js.native = js.native
+
     def loadFile(url: String, options: js.Dictionary[Any]): js.native = js.native
+
+    def dispose(): js.native = js.native
   }
 
 }
