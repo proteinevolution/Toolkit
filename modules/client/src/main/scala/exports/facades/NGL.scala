8e8076932cb60a74d05b1a19c00cd3e805bb7400
Felix Gabler <felix.gabler98@gmail.com>
2018-05-12 17:31:00 +0200

    create facade for ngl

---
+++ modules/client/src/main/scala/exports/facades/NGL.scala
@@ -0,0 +1,15 @@
+package exports.facades
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+@JSGlobal
+@js.native
+object NGL extends js.Object {
+
+  @js.native
+  class Stage(elementId: String) extends js.Any {
+    def loadFile(url: String, options: js.Dictionary[Any]): js.native = js.native
+  }
+
+}
