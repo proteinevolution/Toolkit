9c4e0b8e2d254654c264bf806f40e9416661f234
Felix Gabler <felix.gabler@tuebingen.mpg.de>
2018-05-20 16:35:11 +0200

    scalafmt

--- modules/client/src/main/scala/exports/extensions/JQueryExtensions.scala
+++ modules/client/src/main/scala/exports/extensions/JQueryExtensions.scala
@@ -18,11 +18,11 @@ object JQueryExtensions {
 
   implicit class JQueryIsOnScreen(val self: JQuery) extends AnyVal {
     def isOnScreen: Boolean = {
-      val $window = jQuery(dom.window)
-      val viewportTop = $window.scrollTop()
+      val $window        = jQuery(dom.window)
+      val viewportTop    = $window.scrollTop()
       val viewportBottom = viewportTop + $window.height()
-      val boundsTop = self.offset().asInstanceOf[JQueryPosition].top
-      val boundsBottom = boundsTop + self.outerHeight()
+      val boundsTop      = self.offset().asInstanceOf[JQueryPosition].top
+      val boundsBottom   = boundsTop + self.outerHeight()
       boundsTop <= viewportBottom && boundsBottom >= viewportTop
     }
   }
