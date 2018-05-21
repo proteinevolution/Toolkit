27f4d38d9779af3b77010e8f144e0e196412efc5
Seung-Zin Nam <z.nam@envisia.de>
2018-05-21 16:10:42 +0200

    stage can never be not null, so don't dispose * facade has type Unit instead of js.native

--- modules/client/src/main/scala/exports/facades/NGL.scala
+++ modules/client/src/main/scala/exports/facades/NGL.scala
@@ -12,11 +12,13 @@ object NGL extends js.Object {
 
   @js.native
   class Stage(element: dom.Element, settings: js.Dictionary[Any] = js.Dictionary()) extends js.Any {
-    def loadFile(url: Blob, options: js.Dictionary[Any]): js.native = js.native
 
-    def loadFile(url: String, options: js.Dictionary[Any]): js.native = js.native
+    def loadFile(url: Blob, options: js.Dictionary[Any]): Unit = js.native
 
-    def dispose(): js.native = js.native
+    def loadFile(url: String, options: js.Dictionary[Any]): Unit = js.native
+
+    def dispose(): Unit = js.native
+
   }
 
 }
