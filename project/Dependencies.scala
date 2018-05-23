c3d08ca3776594fa69e80c7292e0a4c256e0b35c
Seung-Zin Nam <seungzin.nam@tuebingen.mpg.de>
2018-05-23 16:36:31 +0200

    Scalajs 0.6.23 now officially released
    This reverts commit dc566b65e1d6e18fa93ab66be5bc981be47ee554.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,7 @@
 import play.sbt.PlayImport._
 import sbt._
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
+import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 
 object Dependencies {
 
