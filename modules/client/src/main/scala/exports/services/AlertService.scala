9f9ebc1f3074328de1e77560c5f2cabc28c080a5
Seung-Zin Nam <z.nam@envisia.de>
2018-05-26 19:26:44 +0200

    delay class removal correctly

--- modules/client/src/main/scala/exports/services/AlertService.scala
+++ modules/client/src/main/scala/exports/services/AlertService.scala
@@ -11,14 +11,18 @@ import scala.scalajs.js.annotation.{ JSExport, JSExportTopLevel }
 object AlertService {
 
   @JSExport
-  def alert(text: String, mode: String = ""): Unit = {
+  def alert(text: String, mode: String = "alert-danger"): Unit = {
     jQuery("#alert-service-msg")
-        .text(text)
-        .addClass(mode)
-        .fadeIn("fast")
-        .delay(4000)
-        .fadeOut("slow")
+      .addClass(mode)
+      .text(text)
+      .fadeIn("fast")
+      .delay(4000)
+      .fadeOut("slow")
+    import scala.scalajs.js.timers._
+    setTimeout(4500) {
+      jQuery("#alert-service-msg")
         .removeClass(mode)
+    }
   }
 
 }
