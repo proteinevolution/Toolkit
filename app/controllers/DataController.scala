4410f042a5961787b20adacd76e3b3a215c035e7
Seung-Zin Nam <zin@mailbox.org>
2018-06-10 20:35:39 +0200

    closes #449: show tool version in help modal of tool (#452)

--- app/controllers/DataController.scala
+++ app/controllers/DataController.scala
@@ -3,10 +3,12 @@ package controllers
 import javax.inject.Inject
 import de.proteinevolution.db.ResultFileAccessor
 import de.proteinevolution.models.ToolName
+import play.api.Configuration
 import play.api.mvc._
+
 import scala.concurrent.ExecutionContext
 
-class DataController @Inject()(cc: ControllerComponents, resultFiles: ResultFileAccessor)(
+class DataController @Inject()(cc: ControllerComponents, resultFiles: ResultFileAccessor, config: Configuration)(
     implicit ec: ExecutionContext
 ) extends AbstractController(cc) {
 
@@ -21,43 +23,48 @@ class DataController @Inject()(cc: ControllerComponents, resultFiles: ResultFile
   }
 
   def getHelp(toolname: String) = Action {
+    val version = getVersion(toolname)
     val help = toolname match {
-      case ToolName.PSIBLAST.value            => views.html.help.psiblast()
-      case ToolName.HHBLITS.value             => views.html.help.hhblits()
-      case ToolName.HHPRED.value              => views.html.help.hhpred()
-      case ToolName.HMMER.value               => views.html.help.hmmer()
-      case ToolName.PATSEARCH.value           => views.html.help.patsearch()
-      case ToolName.CLUSTALO.value            => views.html.help.clustalo()
-      case ToolName.KALIGN.value              => views.html.help.kalign()
-      case ToolName.MAFFT.value               => views.html.help.mafft()
-      case ToolName.MSAPROBS.value            => views.html.help.msaprobs()
-      case ToolName.MUSCLE.value              => views.html.help.muscle()
-      case ToolName.TCOFFEE.value             => views.html.help.tcoffee()
-      case ToolName.ALN2PLOT.value            => views.html.help.aln2plot()
-      case ToolName.HHREPID.value             => views.html.help.hhrepid()
-      case ToolName.MARCOIL.value             => views.html.help.marcoil()
-      case ToolName.PCOILS.value              => views.html.help.pcoils()
-      case ToolName.REPPER.value              => views.html.help.repper()
-      case ToolName.TPRPRED.value             => views.html.help.tprpred()
-      case ToolName.ALI2D.value               => views.html.help.ali2d()
-      case ToolName.QUICK2D.value             => views.html.help.quick2d()
-      case ToolName.MODELLER.value            => views.html.help.modeller()
-      case ToolName.SAMCC.value               => views.html.help.samcc()
-      case ToolName.ANCESCON.value            => views.html.help.ancescon()
-      case ToolName.CLANS.value               => views.html.help.clans()
-      case ToolName.MMSEQS2.value             => views.html.help.mmseqs2()
-      case ToolName.PHYML.value               => views.html.help.phyml()
-      case ToolName.SIXFRAMETRANSLATION.value => views.html.help.sixframe()
-      case ToolName.BACKTRANS.value           => views.html.help.backtrans()
-      case ToolName.HHFILTER.value            => views.html.help.hhfilter()
-      case ToolName.RETSEQ.value              => views.html.help.retseq()
-      case ToolName.SEQ2ID.value              => views.html.help.seq2id()
+      case ToolName.PSIBLAST.value            => views.html.help.psiblast(version)
+      case ToolName.HHBLITS.value             => views.html.help.hhblits(version)
+      case ToolName.HHPRED.value              => views.html.help.hhpred(version)
+      case ToolName.HMMER.value               => views.html.help.hmmer(version)
+      case ToolName.PATSEARCH.value           => views.html.help.patsearch(version)
+      case ToolName.CLUSTALO.value            => views.html.help.clustalo(version)
+      case ToolName.KALIGN.value              => views.html.help.kalign(version)
+      case ToolName.MAFFT.value               => views.html.help.mafft(version)
+      case ToolName.MSAPROBS.value            => views.html.help.msaprobs(version)
+      case ToolName.MUSCLE.value              => views.html.help.muscle(version)
+      case ToolName.TCOFFEE.value             => views.html.help.tcoffee(version)
+      case ToolName.ALN2PLOT.value            => views.html.help.aln2plot(version)
+      case ToolName.HHREPID.value             => views.html.help.hhrepid(version)
+      case ToolName.MARCOIL.value             => views.html.help.marcoil(version)
+      case ToolName.PCOILS.value              => views.html.help.pcoils(version)
+      case ToolName.REPPER.value              => views.html.help.repper(version)
+      case ToolName.TPRPRED.value             => views.html.help.tprpred(version)
+      case ToolName.ALI2D.value               => views.html.help.ali2d(version)
+      case ToolName.QUICK2D.value             => views.html.help.quick2d(version)
+      case ToolName.MODELLER.value            => views.html.help.modeller(version)
+      case ToolName.SAMCC.value               => views.html.help.samcc(version)
+      case ToolName.ANCESCON.value            => views.html.help.ancescon(version)
+      case ToolName.CLANS.value               => views.html.help.clans(version)
+      case ToolName.MMSEQS2.value             => views.html.help.mmseqs2(version)
+      case ToolName.PHYML.value               => views.html.help.phyml(version)
+      case ToolName.SIXFRAMETRANSLATION.value => views.html.help.sixframe(version)
+      case ToolName.BACKTRANS.value           => views.html.help.backtrans(version)
+      case ToolName.HHFILTER.value            => views.html.help.hhfilter(version)
+      case ToolName.RETSEQ.value              => views.html.help.retseq(version)
+      case ToolName.SEQ2ID.value              => views.html.help.seq2id(version)
     }
     Ok(help)
   }
 
   def recentUpdates = Action {
     Ok(views.html.elements.recentupdates())
+  }
+
+  private def getVersion(toolname: String): String = {
+    config.get[String](s"Tools.$toolname.version")
   }
 
 }
