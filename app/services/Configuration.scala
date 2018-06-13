ec4f75dfab47de4582ec17eec98f727baf0ee1b4
Seung-Zin Nam <z.nam@envisia.de>
2018-06-13 13:26:49 +0200

    delete hello world code from configuration

--- app/services/Configuration.scala
+++ app/services/Configuration.scala
@@ -1,35 +1,17 @@
 package services
 import javax.inject._
-
+import play.api.Logger
 import play.api.inject.ApplicationLifecycle
 import play.api.libs.ws.WSClient
 
-import scala.concurrent.Future
-
-sealed trait Configuration {
-
-  def hello(): Unit
-  def goodbye(): Unit
-
-}
+sealed trait Configuration
 
 @Singleton
 final class ConfigurationImpl @Inject()(appLifecycle: ApplicationLifecycle, ws: WSClient)() extends Configuration {
 
-  override def hello(): Unit = {
-    println("configuring hostname .... ")
-    val _ = ws.url("https://toolkit.tuebingen.mpg.de").get()
-  }
-
-  override def goodbye(): Unit = println("Goodbye!")
-
-  def start(): Unit = hello()
-
   appLifecycle.addStopHook { () =>
-    goodbye()
-    Future.successful(())
+    Logger.info("configuring hostname .... ")
+    ws.url("https://toolkit.tuebingen.mpg.de").get().map(_ => ())
   }
-
-  start()
 
 }
