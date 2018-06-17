13fb12acd7a085ef34750d2b211473947a4f8776
Seung-Zin Nam <z.nam@envisia.de>
2018-06-18 00:17:47 +0200

    more json automated

--- modules/common/src/main/scala/de/proteinevolution/models/forms/ToolForm.scala
+++ modules/common/src/main/scala/de/proteinevolution/models/forms/ToolForm.scala
@@ -1,24 +1,19 @@
 package de.proteinevolution.models.forms
 
 import de.proteinevolution.models.param.Param
-import play.api.libs.json.{ JsPath, Writes }
-import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 // Returned to the View if a tool is requested with the getTool route
-case class ToolForm(toolname: String,
-                    toolnameLong: String,
-                    toolnameAbbrev: String,
-                    category: String,
-                    params: Seq[(String, Seq[Param])])
+case class ToolForm(
+    toolname: String,
+    toolnameLong: String,
+    toolnameAbbrev: String,
+    category: String,
+    params: Seq[(String, Seq[Param])]
+)
 
 object ToolForm {
 
-  implicit val toolFormWrites: Writes[ToolForm] = (
-    (JsPath \ "toolname").write[String] and
-    (JsPath \ "toolnameLong").write[String] and
-    (JsPath \ "toolnameAbbrev").write[String] and
-    (JsPath \ "category").write[String] and
-    (JsPath \ "params").write[Seq[(String, Seq[Param])]]
-  )(unlift(ToolForm.unapply))
+  implicit val toolFormWrites: OWrites[ToolForm] = Json.writes[ToolForm]
 
 }
--- modules/common/src/main/scala/de/proteinevolution/models/param/Param.scala
+++ modules/common/src/main/scala/de/proteinevolution/models/param/Param.scala
@@ -1,16 +1,12 @@
 package de.proteinevolution.models.param
 
-import play.api.libs.functional.syntax.{ unlift, _ }
-import play.api.libs.json.{ JsPath, Writes }
+import play.api.libs.json._
 
 // A simple parameter with name and a type
 case class Param(name: String, paramType: ParamType, internalOrdering: Int, label: String)
 
 object Param {
-  implicit val paramWrites: Writes[Param] = (
-    (JsPath \ "name").write[String] and
-    (JsPath \ "paramType").write[ParamType] and
-    (JsPath \ "internalOrdering").write[Int] and
-    (JsPath \ "label").write[String]
-  )(unlift(Param.unapply))
+
+  implicit val paramWrites: OWrites[Param] = Json.writes[Param]
+
 }
