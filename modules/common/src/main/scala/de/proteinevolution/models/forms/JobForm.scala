281fa351f12d911137ca56dfc0eeb2ffaaf0d92d
Seung-Zin Nam <zin@mailbox.org>
2018-06-18 00:34:39 +0200

    more json automated (#471)

--- modules/common/src/main/scala/de/proteinevolution/models/forms/JobForm.scala
+++ modules/common/src/main/scala/de/proteinevolution/models/forms/JobForm.scala
@@ -1,26 +1,20 @@
 package de.proteinevolution.models.forms
 
 import de.proteinevolution.models.database.jobs.JobState._
-import play.api.libs.json.{ JsPath, Writes }
-import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 // Server returns such an object when asked for a job
-case class JobForm(jobID: String,
-                   state: JobState,
-                   dateCreated: String,
-                   toolitem: ToolForm,
-                   views: Seq[String],
-                   paramValues: Map[String, String])
+case class JobForm(
+    jobID: String,
+    state: JobState,
+    dateCreated: String,
+    toolitem: ToolForm,
+    views: Seq[String],
+    paramValues: Map[String, String]
+)
 
 object JobForm {
 
-  implicit val jobFormWrites: Writes[JobForm] = (
-    (JsPath \ "jobID").write[String] and
-    (JsPath \ "state").write[JobState] and
-    (JsPath \ "dateCreated").write[String] and
-    (JsPath \ "toolitem").write[ToolForm] and
-    (JsPath \ "views").write[Seq[String]] and
-    (JsPath \ "paramValues").write[Map[String, String]](play.api.libs.json.Writes.mapWrites[String])
-  )(unlift(JobForm.unapply))
+  implicit val jobFormWrites: OWrites[JobForm] = Json.writes[JobForm]
 
 }
