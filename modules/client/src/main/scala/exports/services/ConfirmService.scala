a9407fce833456ddd45fc965f3b6d818522ccb5b
Felix Gabler <felix.gabler98@gmail.com>
2018-06-30 12:33:32 +0200

    Css and confirm modal (#495)
    * allow for dynamic height changes
    #481
    
    * confirm modal
    #477
    
    * fix job still displayed in jobmanager error
    #477
    
    * scalafmt
    #477

---
+++ modules/client/src/main/scala/exports/services/ConfirmService.scala
@@ -0,0 +1,31 @@
+package exports.services
+
+import exports.facades.JQueryPlugin._
+import org.scalajs.jquery.{ jQuery, JQuery }
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.{ JSExport, JSExportTopLevel }
+
+@JSExportTopLevel("ConfirmService")
+object ConfirmService {
+
+  val $modal: JQuery = jQuery("#confirmModal")
+
+  @JSExport
+  def confirm(text: String, onSuccess: js.Function0[Unit], onError: js.Function0[Unit] = () => {}): Unit = {
+    $modal.find(".modal-text").text(text)
+    $modal.foundation("open")
+    $modal.find(".confirm-btn")
+      .off("click")
+      .on("click", () => {
+        $modal.foundation("close")
+        onSuccess()
+      })
+    $modal.find(".cancel-btn")
+      .off("click")
+      .on("click", () => {
+        $modal.foundation("close")
+        onError()
+      })
+  }
+}
