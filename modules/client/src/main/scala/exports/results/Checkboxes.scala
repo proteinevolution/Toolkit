fd7390860ad222bb5a5f12836e252378a567f16d
Felix Gabler <felix.gabler98@gmail.com>
2018-07-08 16:50:18 +0200

    541 checkboxes (#548)
    * Fix checkbox toggling
    #541
    
    * scalafmt
    #541

--- modules/client/src/main/scala/exports/results/Checkboxes.scala
+++ modules/client/src/main/scala/exports/results/Checkboxes.scala
@@ -1,12 +1,11 @@
 package exports.results
 
-import org.scalajs.dom.Element
 import org.scalajs.dom.raw.{ Event, HTMLInputElement }
-import org.scalajs.jquery.{ jQuery, JQuery }
+import org.scalajs.jquery.JQuery
 
 import scala.collection.mutable
-import scala.scalajs.js.JSConverters._
 import scala.scalajs.js
+import scala.scalajs.js.JSConverters._
 
 /**
  * Checkbox handler for result pages
@@ -38,17 +37,12 @@ class Checkboxes(private val outerContainer: JQuery) {
           val currentState = e.currentTarget.asInstanceOf[HTMLInputElement].checked
           checkedValues(currentVal.toString) = currentState // force string value
           // link checkboxes with same value
-          container
-            .find(s"input[type=checkbox][name=alignment_elem][value=$currentVal]")
-            .each((_: Int, checkbox: Element) => {
-              jQuery(checkbox).prop("checked", currentState)
-            })
+          container.find(s"input[type=checkbox][name=alignment_elem][value=$currentVal]").prop("checked", currentState)
         }
       )
   }
 
-  def toggleAll(max: Int): Unit = {
-    val $button = outerContainer.find(".selectAllSeqBar")
+  def toggleAll(max: Int, $button: JQuery): Unit = {
     if ($button.text().trim == "Select all".trim) {
       deselectAll(max)
     } else {
@@ -57,24 +51,12 @@ class Checkboxes(private val outerContainer: JQuery) {
   }
 
   def deselectAll(max: Int): Unit = {
-    outerContainer
-      .find("input[type=checkbox][name=alignment_elem]")
-      .each(
-        (_: Int, el: Element) => {
-          jQuery(el).prop("checked", false)
-        }
-      )
+    outerContainer.find("input[type=checkbox][name=alignment_elem]").prop("checked", false)
     (1 to max).foreach(i => checkedValues.update(i.toString, false))
   }
 
   def selectAll(max: Int): Unit = {
-    outerContainer
-      .find("input[type=checkbox][name=alignment_elem]")
-      .each(
-        (_: Int, el: Element) => {
-          jQuery(el).prop("checked", true)
-        }
-      )
+    outerContainer.find("input[type=checkbox][name=alignment_elem]").prop("checked", true)
     (1 to max).foreach(i => checkedValues.update(i.toString, true))
   }
 
