536cb9f9722d44a6e5d88cf061c03375e05792e6
Seung-Zin Nam <zin@mailbox.org>
2018-07-08 17:33:35 +0200

    upgrade betterfiles (#549)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     "com.typesafe.akka"    %% "akka-stream"              % akkaVersion,
     "com.typesafe.play"    %% "play-mailer"              % "6.0.1",
     "com.typesafe.play"    %% "play-mailer-guice"        % "6.0.1",
-    "com.github.pathikrit" %% "better-files"             % "3.4.0",
+    "com.github.pathikrit" %% "better-files"             % "3.5.0",
     "org.mindrot"          % "jbcrypt"                   % "0.3m",
     "org.reactivemongo"    %% "play2-reactivemongo"      % "0.14.0-play26",
     "org.reactivemongo"    %% "reactivemongo-akkastream" % "0.14.0",
