cae1feeab0ecb5449c770916ca9fc1ca72d49ef9
Seung-Zin Nam <zin@mailbox.org>
2018-07-17 18:50:16 +0200

    closes #564: make accession searchable in hmmer dt (#593)

--- modules/results/src/main/scala/de/proteinevolution/results/services/DTService.scala
+++ modules/results/src/main/scala/de/proteinevolution/results/services/DTService.scala
@@ -1,16 +1,22 @@
 package de.proteinevolution.results.services
 
 import de.proteinevolution.results.results.General.DTParam
+import de.proteinevolution.results.results.Hmmer.HmmerHSP
 import de.proteinevolution.results.results.{ HSP, SearchResult }
+import scala.reflect.runtime.universe._
 
 trait DTService {
 
-  def getHitsByKeyWord[T <: HSP](hits: SearchResult[T], params: DTParam): List[T] = {
+  def getHitsByKeyWord[T <: HSP](hits: SearchResult[T], params: DTParam)(implicit tool: TypeTag[T]): List[T] = {
     val hitList = hits.hitsOrderBy(params)
     if (params.searchValue.length > 0) {
-      hitList.filter(
-        hit => (hit.description + hit.template.accession).toUpperCase.contains(params.searchValue.toUpperCase)
-      )
+      hitList.filter { hit =>
+        val accession = tool match {
+          case t if t == typeTag[HmmerHSP] => hit.accession
+          case _                           => hit.template.accession
+        }
+        (hit.description + accession).toUpperCase.contains(params.searchValue.toUpperCase)
+      }
     } else {
       hitList
     }
