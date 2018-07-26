bd2741a2ace93331bc531b0833bc6262742531c5
Vikram Alva <vikram.alva@gmail.com>
2018-07-26 13:37:26 +0200

    Revert "issue #608: enable sentry (#634)"
    This reverts commit 776a203ca32f8fe14195fa8f4ba2ccfad1c959f6.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,8 +37,7 @@ object Dependencies {
     "com.dripower"         %% "play-circe"               % "2609.1",
     "org.webjars"          %% "webjars-play"             % "2.6.3",
     "org.scala-js"         %% "scalajs-stubs"            % scalaJSVersion % "provided",
-    "com.github.mpilquist" %% "simulacrum"               % "0.12.0",
-    "io.sentry"            % "sentry-logback"            % "1.7.5"
+    "com.github.mpilquist" %% "simulacrum"               % "0.12.0"
   )
 
   lazy val clientDeps: Def.Initialize[Seq[sbt.ModuleID]] = Def.setting(
