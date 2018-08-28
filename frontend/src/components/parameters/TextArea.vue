9f3a85726ab98852780454ab06a724d252a0a16c
Felix Gabler <felix.gabler98@gmail.com>
2018-08-28 11:44:36 +0200

    use class instead of type

--- frontend/src/components/parameters/TextArea.vue
+++ frontend/src/components/parameters/TextArea.vue
@@ -12,15 +12,15 @@
 </template>
 
 <script lang="ts">
-    import {Vue} from 'vue/types/vue';
+    import Vue from 'vue';
     import {TextAreaParameter} from '@/types/toolkit';
 
     export default Vue.extend({
         name: 'TextArea',
         props: {
             /*
-            Simply stating the interface type doesn't work, this is a workaround. See
-            https://frontendsociety.com/using-a-typescript-interfaces-and-types-as-a-prop-type-in-vuejs-508ab3f83480
+             Simply stating the interface type doesn't work, this is a workaround. See
+             https://frontendsociety.com/using-a-typescript-interfaces-and-types-as-a-prop-type-in-vuejs-508ab3f83480
              */
             parameter: Object as () => TextAreaParameter,
         },
