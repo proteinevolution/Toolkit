1799dc65439d66e9a9a149313159899ecd984a61
Sebastian Till <25408093+sesati@users.noreply.github.com>
2018-09-03 16:06:39 +0200

    Add unit tests for NavBar and store getters

---
+++ frontend/tests/unit/components/header/NavBar.spec.ts
@@ -0,0 +1,78 @@
+import {expect} from 'chai';
+import BootstrapVue from 'bootstrap-vue';
+import {createLocalVue, shallowMount} from '@vue/test-utils';
+import NavBar from '@/components/header/NavBar.vue';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+
+const tools = [
+    {
+        name: 'Tool1', longname: 'Tool1', section: 'Section1', forwarding: {alignment: [], multiSeq: []},
+    },
+    {
+        name: 'Tool2', longname: 'Tool1', section: 'Section2', forwarding: {alignment: [], multiSeq: []},
+    },
+    {
+        name: 'Tool3', longname: 'Tool1', section: 'Section2', forwarding: {alignment: [], multiSeq: []},
+    },
+];
+
+const sections: string[] = [];
+
+function initNavBar(toolNameRouteParam: string | undefined) {
+    const $route = {
+        params: {
+            toolName: toolNameRouteParam,
+        },
+    };
+
+    return shallowMount(NavBar, {
+        localVue,
+        mocks: {
+            $route,
+            $t: (arg: string) => arg,
+        },
+        computed: {
+            tools: () => tools,
+            sections: () => sections,
+            sectionColor: () => '#FFFFFF',
+        },
+    });
+}
+
+/*
+Note: Somehow finding an element by class doesn't work...
+        expect(wrapper.find('.active').text()).to.contain('Section1');
+        --> Error: find did not return .active, cannot call text() on empty Wrapper
+ */
+
+describe('NavBar', () => {
+    it('selects correct section if route parameter is set', () => {
+        const wrapper = initNavBar('Tool2');
+        expect((wrapper.vm as any).selectedSection).to.equal('Section2');
+    });
+
+    it('selects first section if invalid route parameter is set', () => {
+        const wrapper = initNavBar('ToolXYZ');
+        expect((wrapper.vm as any).selectedSection).to.equal('Section1');
+    });
+
+    it('selects first section if no route parameter is set', () => {
+        const wrapper = initNavBar(undefined);
+        expect((wrapper.vm as any).selectedSection).to.equal('Section1');
+    });
+
+    it('reacts on changes to the sections', () => {
+        const wrapper = initNavBar(undefined);
+        expect((wrapper.vm as any).selectedSection).to.equal('Section1');
+    });
+
+    it('updates manually selected selection correctly', () => {
+        const wrapper = initNavBar(undefined);
+        (wrapper.vm as any).selectSection('Section2');
+        expect((wrapper.vm as any).selectedSection).to.equal('Section2');
+        (wrapper.vm as any).selectSection('Section1');
+        expect((wrapper.vm as any).selectedSection).to.equal('Section1');
+    });
+});
---
+++ frontend/tests/unit/store/tools/getters.spec.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import toolGetters from '@/store/modules/tools/getters.ts';
+import {ToolState} from '@/store/types';
+
+const rootState = {
+    maintenanceMode: false,
+    reconnecting: false,
+};
+
+describe('tools/getters', () => {
+    it('should return the correct sections', () => {
+        const state: ToolState = {
+            tools: [
+                {
+                    name: 'Tool1', longname: 'Tool1', section: 'Section1', forwarding: {alignment: [], multiSeq: []},
+                },
+                {
+                    name: 'Tool2', longname: 'Tool1', section: 'Section2', forwarding: {alignment: [], multiSeq: []},
+                },
+                {
+                    name: 'Tool3', longname: 'Tool1', section: 'Section2', forwarding: {alignment: [], multiSeq: []},
+                },
+            ],
+        };
+
+        const result = toolGetters.sections(state, null, rootState, null);
+        expect(result).to.deep.equal(['Section1', 'Section2']);
+    });
+});
