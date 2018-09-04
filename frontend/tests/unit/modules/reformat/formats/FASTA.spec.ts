eb492466acfad0a1eda6c32a2d3f27d489ecda32
Sebastian Till <25408093+sesati@users.noreply.github.com>
2018-09-04 15:03:32 +0200

    Add testcases

---
+++ frontend/tests/unit/modules/reformat/formats/FASTA.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import {FASTA} from '../../../../../src/modules/reformat/formats/FASTA';
+
+describe('FASTA validation', () => {
+    it('should detect any invalid characters', () => {
+        expect(FASTA.validate('ABAB')).to.be.true;
+        expect(FASTA.validate('ABAB@')).to.be.false;
+    });
+});
