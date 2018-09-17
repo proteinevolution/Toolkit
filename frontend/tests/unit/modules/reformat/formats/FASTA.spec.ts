567f3aa8494334999a8b83ca65a77532d0f245a2
Felix Gabler <felix.gabler98@gmail.com>
2018-09-17 14:31:13 +0200

    tests

--- frontend/tests/unit/modules/reformat/formats/FASTA.spec.ts
+++ frontend/tests/unit/modules/reformat/formats/FASTA.spec.ts
@@ -6,4 +6,15 @@ describe('FASTA validation', () => {
         expect(FASTA.validate('ABAB')).to.be.true;
         expect(FASTA.validate('ABAB@')).to.be.false;
     });
+
+    it('should detect empty headers', () => {
+        expect(FASTA.validate(`
+        >
+        asd
+       `)).to.be.false;
+        expect(FASTA.validate(`
+        >a
+        asd
+       `)).to.be.true;
+    });
 });
