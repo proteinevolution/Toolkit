bf40ff21d5bae4a578d39f4744c1479c45494b04
Felix Gabler <felix.gabler98@gmail.com>
2018-09-24 15:37:54 +0200

    improve dev mode

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,7 +6,10 @@ import './assets/scss/main.scss';
 import i18n from './i18n';
 import './bootstrap.ts';
 
-Vue.config.productionTip = false;
+const devMode: boolean = process.env.NODE_ENV === 'development';
+Vue.config.productionTip = devMode;
+Vue.config.silent = !devMode;
+Vue.config.devtools = devMode;
 
 (window as any).vm = new Vue({
     router,
