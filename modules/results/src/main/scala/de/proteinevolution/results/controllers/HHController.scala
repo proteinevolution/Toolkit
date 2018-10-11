b7c004ac082b84d43d81a40944d3db5451fd3656
Seung-Zin Nam <z.nam@envisia.de>
2018-10-11 16:15:48 +0200

    finish hhstuff

--- modules/results/src/main/scala/de/proteinevolution/results/controllers/HHController.scala
+++ modules/results/src/main/scala/de/proteinevolution/results/controllers/HHController.scala
@@ -1,13 +1,10 @@
 package de.proteinevolution.results.controllers
 
 import de.proteinevolution.base.controllers.ToolkitController
-import de.proteinevolution.results.db.ResultFileAccessor
-import de.proteinevolution.models.ToolName._
 import de.proteinevolution.results.models.{ HHContext, ResultsForm }
 import de.proteinevolution.results.results.General.DTParam
-import de.proteinevolution.results.results._
-import de.proteinevolution.results.services.ResultsRepository.ResultsService
-import de.proteinevolution.results.services.{ DTService, HHService, ResultsRepository, ToolNameGetService }
+import de.proteinevolution.results.services.HHService
+import io.circe.syntax._
 import javax.inject.{ Inject, Singleton }
 import play.api.mvc.{ Action, AnyContent }
 
@@ -18,8 +15,7 @@ class HHController @Inject()(
     ctx: HHContext,
     service: HHService
 )(implicit ec: ExecutionContext)
-    extends ToolkitController(ctx.controllerComponents)
-    with DTService {
+    extends ToolkitController(ctx.controllerComponents) {
 
   def loadHits(jobId: String): Action[ResultsForm] = Action(circe.json[ResultsForm]).async { implicit request =>
     service.loadHits(jobId, request.body).value.map {
@@ -29,7 +25,7 @@ class HHController @Inject()(
   }
 
   def dataTable(jobId: String): Action[AnyContent] = Action.async { implicit request =>
-    /*val params = DTParam(
+    val params = DTParam(
       request.getQueryString("draw").getOrElse("1").toInt,
       request.getQueryString("search[value]").getOrElse(""),
       request.getQueryString("start").getOrElse("0").toInt,
@@ -37,46 +33,15 @@ class HHController @Inject()(
       request.getQueryString("order[0][column]").getOrElse("1").toInt,
       request.getQueryString("order[0][dir]").getOrElse("asc")
     )
-    getResults(jobId)
-      .run(resultsService)
-      .flatMap {
-        case Some(jsValue) =>
-          getTool(jobId).run(resultsService).map {
-            case HHBLITS =>
-              (resultCtx.hhblits.parseResult(jsValue),
-               getHitsByKeyWord[HHBlitsHSP](resultCtx.hhblits.parseResult(jsValue), params))
-            case HHOMP =>
-              (resultCtx.hhomp.parseResult(jsValue),
-               getHitsByKeyWord[HHompHSP](resultCtx.hhomp.parseResult(jsValue), params))
-            case HHPRED =>
-              (resultCtx.hhpred.parseResult(jsValue),
-               getHitsByKeyWord[HHPredHSP](resultCtx.hhpred.parseResult(jsValue), params))
-            case HMMER =>
-              (resultCtx.hmmer.parseResult(jsValue),
-               getHitsByKeyWord[HmmerHSP](resultCtx.hmmer.parseResult(jsValue), params))
-            case PSIBLAST =>
-              (resultCtx.psiblast.parseResult(jsValue),
-               getHitsByKeyWord[PSIBlastHSP](resultCtx.psiblast.parseResult(jsValue), params))
-            case _ => throw new IllegalArgumentException("datatables not implemented for this tool")
-          }
-        case None => throw new IllegalStateException("no result found")
-      }
-      .map {
-        case (result, hits) =>
-          Ok(
-            Json
-              .toJson(Map("draw" -> params.draw, "recordsTotal" -> result.num_hits, "recordsFiltered" -> hits.length))
-              .as[JsObject]
-              .deepMerge(
-                Json.obj(
-                  "data" -> hits
-                    .slice(params.displayStart, params.displayStart + params.pageLength)
-                    .map(_.toDataTable(result.db))
-                )
-              )
-          )
-      } */
-    fuccess(Ok)
+    service.dataTable(jobId, params).value.map {
+      case Right((hits, result)) =>
+        val config = Map("draw" -> params.draw, "recordsTotal" -> result.num_hits, "recordsFiltered" -> hits.length)
+        val data = "data" -> hits
+          .slice(params.displayStart, params.displayStart + params.pageLength)
+          .map(_.toDataTable(result.db))
+        Ok((config + data).asJson)
+      case Left(_) => BadRequest
+    }
   }
 
 }
