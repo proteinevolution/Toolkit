07347ff26c3b7497990b6c070a7a30ecf90a4c38
Seung-Zin Nam <z.nam@envisia.de>
2018-10-21 17:22:50 +0200

    fix tprpred

--- modules/results/src/main/scala/de/proteinevolution/results/results/TPRPredResult.scala
+++ modules/results/src/main/scala/de/proteinevolution/results/results/TPRPredResult.scala
@@ -1,24 +1,47 @@
 package de.proteinevolution.results.results
 
-import io.circe.{ DecodingFailure, Json }
+import de.proteinevolution.results.results.TPRPredResult._
+import io.circe.{ Decoder, DecodingFailure, HCursor, Json }
 
-case class TPRPredResult(info: List[Option[String]], hits: List[Option[String]])
+case class TPRPredResult(info: List[Desc], hits: List[Hit])
 
 object TPRPredResult {
 
   implicit def tprpredDecoder(jobId: String, json: Json): Either[DecodingFailure, TPRPredResult] =
     for {
-      info   <- json.hcursor.downField(jobId).downField("desc").as[Json]
-      hits   <- json.hcursor.downField(jobId).downField("hits").as[Json]
-      info_0 <- info.hcursor.downField("0").as[Option[String]]
-      info_1 <- info.hcursor.downField("1").as[Option[String]]
-      hits_0 <- hits.hcursor.downField("0").as[Option[String]]
-      hits_1 <- hits.hcursor.downField("1").as[Option[String]]
-      hits_2 <- hits.hcursor.downField("2").as[Option[String]]
-      hits_3 <- hits.hcursor.downField("3").as[Option[String]]
-      hits_4 <- hits.hcursor.downField("4").as[Option[String]]
+      info <- json.hcursor.downField(jobId).downField("desc").downArray.as[List[Desc]]
+      hits <- json.hcursor.downField(jobId).downField("hits").downArray.as[List[Hit]]
     } yield {
-      new TPRPredResult(List(info_0, info_1), List(hits_0, hits_1, hits_2, hits_3, hits_4))
+      new TPRPredResult(info, hits)
     }
+
+  case class Desc(title: Option[String], value: Option[String])
+
+  object Desc {
+    implicit val descDecoder: Decoder[Desc] = (c: HCursor) =>
+      for {
+        title <- c.right.as[Option[String]]
+        value <- c.first.as[Option[String]]
+      } yield new Desc(title, value)
+  }
+
+  case class Hit(
+      alignment: Option[String],
+      repeat: Option[String],
+      begin: Option[String],
+      end: Option[String],
+      pValue: Option[String]
+  )
+
+  object Hit {
+    implicit val hitDecoder: Decoder[Hit] = (c: HCursor) =>
+      for {
+        alignment <- c.first.as[Option[String]]
+        repeat    <- c.right.as[Option[String]]
+        begin     <- c.rightN(2).as[Option[String]]
+        end       <- c.rightN(3).as[Option[String]]
+        pValue    <- c.rightN(4).as[Option[String]]
+      } yield new Hit(alignment, repeat, begin, end, pValue)
+  }
 
 }
