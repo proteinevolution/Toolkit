90415f17bd61e43990d475409fd3b7fcbafba2a9
Seung-Zin Nam <z.nam@envisia.de>
2018-10-22 00:15:31 +0200

    try to fix hhblits

--- modules/results/src/main/scala/de/proteinevolution/results/results/HHBlitsQuery.scala
+++ modules/results/src/main/scala/de/proteinevolution/results/results/HHBlitsQuery.scala
@@ -1,7 +1,6 @@
 package de.proteinevolution.results.results
 
-import io.circe.Decoder
-import io.circe.generic.semiauto.deriveDecoder
+import io.circe.{ Decoder, HCursor }
 
 case class HHBlitsQuery(
     consensus: String,
@@ -14,6 +13,14 @@ case class HHBlitsQuery(
 
 object HHBlitsQuery {
 
-  implicit val hhblitsQueryDecoder: Decoder[HHBlitsQuery] = deriveDecoder[HHBlitsQuery]
+  implicit val hhblitsQueryDecoder: Decoder[HHBlitsQuery] = (c: HCursor) =>
+    for {
+      consensus <- c.downField("consensus").as[String]
+      end       <- c.downField("end").as[Int]
+      accession <- c.downField("name").as[String]
+      ref       <- c.downField("ref").as[Int]
+      seq       <- c.downField("seq").as[String]
+      start     <- c.downField("start").as[Int]
+    } yield new HHBlitsQuery(consensus, end, accession, ref, seq, start)
 
 }
