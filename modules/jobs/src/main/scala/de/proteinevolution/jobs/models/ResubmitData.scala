6b609ecbaad8358d1f67a3d79456c71faebd662e
Seung-Zin Nam <zin@mailbox.org>
2018-10-28 19:26:21 +0100

    replace play-json in submission controller (#716)

--- modules/jobs/src/main/scala/de/proteinevolution/jobs/models/ResubmitData.scala
+++ modules/jobs/src/main/scala/de/proteinevolution/jobs/models/ResubmitData.scala
@@ -1,11 +1,12 @@
 package de.proteinevolution.jobs.models
 
-import play.api.libs.json._
+import io.circe.Encoder
+import io.circe.generic.semiauto.deriveEncoder
 
-final private[jobs] case class ResubmitData(exists: Boolean, version: Option[Int], suggested: Option[String])
+case class ResubmitData(exists: Boolean, version: Option[Int], suggested: Option[String])
 
 object ResubmitData {
 
-  implicit val resubmitDataWrites: OWrites[ResubmitData] = Json.writes[ResubmitData]
+  implicit val resubmitDataEncoder: Encoder[ResubmitData] = deriveEncoder
 
 }
