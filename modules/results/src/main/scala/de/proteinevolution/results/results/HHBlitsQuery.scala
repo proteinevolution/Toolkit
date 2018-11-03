1f67ecc5b0686551905c4ac72beec526ad3e7324
Seung-Zin Nam <zin@mailbox.org>
2018-11-03 11:17:49 +0100

    generic decoder for hhblits query (#734)

--- modules/results/src/main/scala/de/proteinevolution/results/results/HHBlitsQuery.scala
+++ modules/results/src/main/scala/de/proteinevolution/results/results/HHBlitsQuery.scala
@@ -1,26 +1,23 @@
 package de.proteinevolution.results.results
 
-import io.circe.{ Decoder, HCursor }
+import io.circe.Decoder
+import io.circe.generic.semiauto.deriveDecoder
 
 case class HHBlitsQuery(
     consensus: String,
     end: Int,
-    accession: String,
+    name: String,
     ref: Int,
     seq: String,
     start: Int
-)
+) {
+
+  def accession: String = name
+
+}
 
 object HHBlitsQuery {
 
-  implicit val hhblitsQueryDecoder: Decoder[HHBlitsQuery] = (c: HCursor) =>
-    for {
-      consensus <- c.downField("consensus").as[String]
-      end       <- c.downField("end").as[Int]
-      accession <- c.downField("name").as[String]
-      ref       <- c.downField("ref").as[Int]
-      seq       <- c.downField("seq").as[String]
-      start     <- c.downField("start").as[Int]
-    } yield new HHBlitsQuery(consensus, end, accession, ref, seq, start)
+  implicit val hhblitsQueryDecoder: Decoder[HHBlitsQuery] = deriveDecoder
 
 }
