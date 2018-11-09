4accd7718973d7a078ed70fafc4aa85ed3c3095f
Seung-Zin Nam <z.nam@envisia.de>
2018-11-09 13:26:47 +0100

    test DI with AppLoader

--- app/ToolkitAppLoader.scala
+++ app/ToolkitAppLoader.scala
@@ -5,25 +5,21 @@ import play.api.ApplicationLoader.Context
 import play.api.inject.bind
 import play.api.inject.guice.{ GuiceApplicationBuilder, GuiceApplicationLoader, GuiceableModule }
 import play.api.libs.concurrent.MaterializerProvider
+import javax.inject.{ Inject, Singleton }
 
-import com.typesafe.config.ConfigFactory
-
-class ToolkitAppLoader extends GuiceApplicationLoader {
+@Singleton
+class ToolkitAppLoader @Inject()(mongobeeRunner: MongobeeRunner) extends GuiceApplicationLoader {
 
   private val materializerOverrides: Seq[GuiceableModule] = Seq(
     bind[Materializer].toProvider[MaterializerProvider]
   )
-
-  private val config = ConfigFactory.load()
-
-  private val mongoUri = config.getString("mongodb.uri")
 
   protected override def overrides(context: ApplicationLoader.Context): Seq[GuiceableModule] = {
     GuiceApplicationLoader.defaultOverrides(context) ++ materializerOverrides
   }
 
   override def builder(context: Context): GuiceApplicationBuilder = {
-    new MongobeeRunner(mongoUri).run()
+    mongobeeRunner.run()
     super.builder(context)
   }
 
--- modules/migrations/src/main/scala/de/proteinevolution/migrations/services/MongobeeRunner.scala
+++ modules/migrations/src/main/scala/de/proteinevolution/migrations/services/MongobeeRunner.scala
@@ -1,13 +1,14 @@
 package de.proteinevolution.migrations.services
 
 import com.github.mongobee.Mongobee
-import javax.inject.Singleton
+import javax.inject.{ Inject, Singleton }
+import play.api.Configuration
 
 @Singleton
-class MongobeeRunner(mongoDBUri: String) {
+final class MongobeeRunner @Inject()(config: Configuration) {
 
   def run(): Unit = {
-    val runner = new Mongobee(mongoDBUri)
+    val runner = new Mongobee(config.get[String]("mongodb.uri"))
     runner.setChangeLogsScanPackage("de.proteinevolution.migrations.changelogs")
     runner.execute()
   }
