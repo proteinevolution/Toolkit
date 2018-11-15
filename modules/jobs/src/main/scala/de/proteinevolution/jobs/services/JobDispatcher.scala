69e1afa83fc525cbaad262c90c184e86c302a8b9
Seung-Zin Nam <zin@mailbox.org>
2018-11-15 09:29:41 +0100

    refactor job dispatcher (#755)

--- modules/jobs/src/main/scala/de/proteinevolution/jobs/services/JobDispatcher.scala
+++ modules/jobs/src/main/scala/de/proteinevolution/jobs/services/JobDispatcher.scala
@@ -3,6 +3,7 @@ package de.proteinevolution.jobs.services
 import java.security.MessageDigest
 import java.time.ZonedDateTime
 
+import better.files._
 import cats.data.EitherT
 import cats.implicits._
 import de.proteinevolution.auth.UserSessions
@@ -16,12 +17,11 @@ import play.api.Logger
 import play.api.libs.Files
 import play.api.mvc.MultipartFormData
 import reactivemongo.bson.BSONDocument
-import better.files._
 
 import scala.concurrent.{ ExecutionContext, Future }
 
 @Singleton
-class JobDispatcher @Inject()(
+final class JobDispatcher @Inject()(
     jobDao: JobDao,
     constants: ConstantsV2,
     jobIdProvider: JobIdProvider,
@@ -29,15 +29,34 @@ class JobDispatcher @Inject()(
     userSessions: UserSessions
 )(implicit ec: ExecutionContext) {
 
-  private val logger = Logger(this.getClass)
+  private[this] val logger = Logger(this.getClass)
 
   def submitJob(
       toolName: String,
       form: MultipartFormData[Files.TemporaryFile],
       user: User
   ): EitherT[Future, JobSubmitError, Job] = {
+    if (!modellerKeyIsValid(toolName, user)) {
+      EitherT.leftT(JobSubmitError.ModellerKeyInvalid)
+    } else {
+      for {
+        parts           <- EitherT.pure[Future, JobSubmitError](readForm(form))
+        generatedId     <- generateJobId(parts)
+        _               <- validateJobId(generatedId)
+        _               <- EitherT(checkNotAlreadyTaken(generatedId))
+        job             <- EitherT.pure[Future, JobSubmitError](generateJob(toolName, generatedId, parts, user))
+        isFromInstitute <- EitherT.pure[Future, JobSubmitError](user.getUserData.eMail.matches(".+@tuebingen.mpg.de"))
+        _               <- EitherT.liftF(jobDao.insertJob(job))
+        _               <- EitherT.liftF(assignJob(user, job))
+        _               <- EitherT.pure[Future, JobSubmitError](jobIdProvider.trash(generatedId))
+        _               <- EitherT.pure[Future, JobSubmitError](send(generatedId, job, parts, isFromInstitute))
+      } yield job
+    }
+  }
+
+  private[this] def readForm(form: MultipartFormData[Files.TemporaryFile]): Map[String, String] = {
     var parts        = form.dataParts.mapValues(_.mkString(constants.formMultiValueSeparator)) - "file"
-    val allowedFiles = List("alignment", "alignment_two")
+    val allowedFiles = "alignment" :: "alignment_two" :: Nil
     for {
       file <- form.files.filter(file => allowedFiles.contains(file.key))
       in   <- File(file.ref.path).newInputStream.autoClosed
@@ -45,52 +64,31 @@ class JobDispatcher @Inject()(
       val value = in.lines.mkString("\n")
       if (value.nonEmpty) parts = parts.updated(file.key, value)
     }
-    // extract parent id
-    val parentID = parts.get("parent_id")
     // remove empty parameter
     parts.get("alignment_two").foreach { alignment =>
       if (alignment.isEmpty) parts = parts - "alignment_two"
     }
-    if (!modellerKeyIsValid(toolName, user)) {
-      EitherT.leftT(JobSubmitError.ModellerKeyInvalid)
-    } else {
-      for {
-        generatedId     <- generateJobId(parts)
-        _               <- validateJobId(generatedId)
-        _               <- EitherT(checkNotAlreadyTaken(generatedId))
-        job             <- EitherT.pure[Future, JobSubmitError](generateJob(toolName, generatedId, parentID, parts, user))
-        isFromInstitute <- EitherT.pure[Future, JobSubmitError](user.getUserData.eMail.matches(".+@tuebingen.mpg.de"))
-        _               <- EitherT.liftF(jobDao.insertJob(job))
-        _               <- EitherT.liftF(assignJob(user, job))
-        _               <- EitherT.pure[Future, JobSubmitError](jobIdProvider.trash(generatedId))
-        _ <- EitherT.pure[Future, JobSubmitError](
-          jobActorAccess.sendToJobActor(generatedId, PrepareJob(job, parts, startJob = false, isFromInstitute))
-        )
-      } yield {
-        job
-      }
-    }
+    parts
   }
 
-  private def modellerKeyIsValid(toolName: String, user: User): Boolean = {
+  private[this] def send(gid: String, job: Job, parts: Map[String, String], isFromInstitute: Boolean): Unit = {
+    jobActorAccess.sendToJobActor(gid, PrepareJob(job, parts, startJob = false, isFromInstitute))
+  }
+
+  private[this] def modellerKeyIsValid(toolName: String, user: User): Boolean = {
     !(toolName == ToolName.MODELLER.value && !user.userConfig.hasMODELLERKey)
   }
 
-  private def assignJob(user: User, job: Job): Future[Option[User]] = {
+  private[this] def assignJob(user: User, job: Job): Future[Option[User]] = {
     userSessions.modifyUserWithCache(
       BSONDocument(User.IDDB   -> user.userID),
       BSONDocument("$addToSet" -> BSONDocument(User.JOBS -> job.jobID))
     )
   }
 
-  private def isJobId(id: String): Boolean = {
-    id match {
-      case constants.jobIDVersionOptionPattern(_, _) => true
-      case _                                         => false
-    }
-  }
+  private[this] def isJobId(id: String): Boolean = constants.jobIDVersionOptionPattern.pattern.matcher(id).matches
 
-  private def generateJobId(parts: Map[String, String]): EitherT[Future, JobSubmitError, String] = {
+  private[this] def generateJobId(parts: Map[String, String]): EitherT[Future, JobSubmitError, String] = {
     if (parts.get("jobID").isEmpty) {
       EitherT.liftF(jobIdProvider.provide)
     } else {
@@ -98,7 +96,7 @@ class JobDispatcher @Inject()(
     }
   }
 
-  private def validateJobId(jobId: String): EitherT[Future, JobSubmitError, Boolean] = {
+  private[this] def validateJobId(jobId: String): EitherT[Future, JobSubmitError, Boolean] = {
     if (!isJobId(jobId)) {
       logger.warn("job id is invalid")
       EitherT.leftT[Future, Boolean](JobSubmitError.InvalidJobID)
@@ -107,17 +105,16 @@ class JobDispatcher @Inject()(
     }
   }
 
-  private def checkNotAlreadyTaken(jobId: String): Future[Either[JobSubmitError, Boolean]] = {
+  private[this] def checkNotAlreadyTaken(jobId: String): Future[Either[JobSubmitError, Boolean]] = {
     jobDao.selectJob(jobId).map {
       case Some(_) => Left(JobSubmitError.AlreadyTaken)
       case None    => Right(true)
     }
   }
 
-  private def generateJob(
+  private[this] def generateJob(
       toolName: String,
       jobID: String,
-      parentID: Option[String],
       form: Map[String, String],
       user: User
   ): Job = {
@@ -126,7 +123,7 @@ class JobDispatcher @Inject()(
     new Job(
       jobID = jobID,
       ownerID = Some(user.userID),
-      parentID = parentID,
+      parentID = form.get("parent_id"),
       isPublic = form.get("public").isDefined || user.accountType == User.NORMALUSER,
       emailUpdate = form.get("emailUpdate").isDefined,
       tool = toolName,
