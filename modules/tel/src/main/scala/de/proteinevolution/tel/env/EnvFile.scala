068c9821e423c4591ca12ffce669fa7fb1971017
Seung-Zin Nam <zin@mailbox.org>
2018-11-16 10:16:58 +0100

    change log level

--- modules/tel/src/main/scala/de/proteinevolution/tel/env/EnvFile.scala
+++ modules/tel/src/main/scala/de/proteinevolution/tel/env/EnvFile.scala
@@ -62,7 +62,7 @@ class PropFile(path: String, config: Configuration) extends EnvFile(path) {
             updated = updated.replace("perllib_foo", config.get[String]("PERLLIB"))
           case x if x.startsWith("standarddb_bar") =>
             updated = updated.replace("standarddb_bar", config.get[String]("STANDARDDB"))
-          case _ => logger.warn("Env file has no preconfigured key in the configs")
+          case _ => logger.debug("Env file has no preconfigured key in the configs")
         }
         a.updated(spt(0).trim(), updated)
     }
