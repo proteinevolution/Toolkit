421aae28c423ee8d4cc0ea2373e7068b0414c328
Seung-Zin Nam <zin@mailbox.org>
2018-11-16 09:55:59 +0100

    fix env problem by downgrading better files (#757)

--- modules/tel/src/main/scala/de/proteinevolution/tel/env/EnvFile.scala
+++ modules/tel/src/main/scala/de/proteinevolution/tel/env/EnvFile.scala
@@ -4,7 +4,6 @@ import better.files._
 import de.proteinevolution.tel.Subject
 import play.api.{ Configuration, Logger }
 
-import scala.collection.immutable.StringLike
 import scala.sys.process.Process
 import scala.util.matching.Regex
 
@@ -31,7 +30,7 @@ class ExecFile(path: String) extends EnvFile(path) {
       throw EnvFileException(s"File ${f.name} is not executable.")
     }
     // ambiguous implicit conversion because of method `lines` in better.files.package.StringOps
-    Process(f.pathAsString).!!.asInstanceOf[StringLike[String]].lines.map { line =>
+    Process(f.pathAsString).!!.lines.map { line =>
       val spt = line.split('=')
       spt(0).trim() -> spt(1).trim()
     }.toMap
