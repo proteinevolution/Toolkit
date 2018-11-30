4b9f25d6c239b6fc52d112222ea78d0fc9e0e370
Felix Gabler <felix.gabler98@gmail.com>
2018-11-30 16:39:46 +0100

    stringify all submission parameters

--- modules/jobs/src/main/scala/de/proteinevolution/jobs/controllers/SubmissionController.scala
+++ modules/jobs/src/main/scala/de/proteinevolution/jobs/controllers/SubmissionController.scala
@@ -2,15 +2,15 @@ package de.proteinevolution.jobs.controllers
 
 import de.proteinevolution.auth.UserSessions
 import de.proteinevolution.base.controllers.ToolkitController
-import de.proteinevolution.jobs.actors.JobActor.{CheckIPHash, Delete}
+import de.proteinevolution.jobs.actors.JobActor.{ CheckIPHash, Delete }
 import de.proteinevolution.jobs.dao.JobDao
 import de.proteinevolution.jobs.services._
 import de.proteinevolution.models.ConstantsV2
 import de.proteinevolution.tools.ToolConfig
-import javax.inject.{Inject, Singleton}
+import javax.inject.{ Inject, Singleton }
 import play.api.Logger
-import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import io.circe.{Decoder, Json, JsonObject}
+import play.api.mvc.{ Action, AnyContent, ControllerComponents }
+import io.circe.{ Json, JsonObject }
 import io.circe.syntax._
 
 import scala.concurrent.ExecutionContext
@@ -56,40 +56,37 @@ class SubmissionController @Inject()(
     }
   }
 
-  implicit def eitherDecoder[A,B](implicit a: Decoder[A], b: Decoder[B]): Decoder[Either[A,B]] = {
-    val l: Decoder[Either[A,B]]= a.map(Left.apply)
-    val r: Decoder[Either[A,B]]= b.map(Right.apply)
-    l or r
-  }
-
-  def submitJob(toolName: String): Action[Map[String, Either[Seq[String], String]]] =
-    Action(circe.json[Map[String, Either[Seq[String], String]]]).async { implicit request =>
-      val parts = request.body.mapValues {
-        case Left(list) => list.mkString(constants.formMultiValueSeparator)
-        case Right(str) => str
-      }
-      userSessions.getUser.flatMap { user =>
-        jobDispatcher
-          .submitJob(
-            toolName,
-            parts,
-            user
-          )
-          .value
-          .map {
-            case Right(job) =>
-              Ok(
-                JsonObject(
-                  "successful" -> Json.fromBoolean(true),
-                  "code"       -> Json.fromInt(0),
-                  "message"    -> Json.fromString("Submission successful."),
-                  "jobID"      -> Json.fromString(job.jobID)
-                ).asJson
-              ).withSession(userSessions.sessionCookie(request, user.sessionID.get))
-            case Left(error) => BadRequest(errors(error.msg))
-          }
-      }
+  def submitJob(toolName: String): Action[Json] = Action(circe.json).async { implicit request =>
+    request.body.asObject match {
+      case None => fuccess(BadRequest)
+      case Some(obj) =>
+        val parts = for {
+          (key, json) <- obj.toIterable
+          str = if (json.isArray) json.asArray.mkString(constants.formMultiValueSeparator) else json.toString
+        } yield (key, str)
+        userSessions.getUser.flatMap { user =>
+          jobDispatcher
+            .submitJob(
+              toolName,
+              parts.toMap,
+              user
+            )
+            .value
+            .map {
+              case Right(job) =>
+                Ok(
+                  JsonObject(
+                    "successful" -> Json.fromBoolean(true),
+                    "code"       -> Json.fromInt(0),
+                    "message"    -> Json.fromString("Submission successful."),
+                    "jobID"      -> Json.fromString(job.jobID)
+                  ).asJson
+                ).withSession(userSessions.sessionCookie(request, user.sessionID.get))
+              case Left(error) => BadRequest(errors(error.msg))
+            }
+        }
     }
+  }
 
   def resubmitJob(newJobID: String, resubmitForJobID: Option[String]): Action[AnyContent] = Action.async {
     implicit request =>
