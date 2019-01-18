24a3cd8af155c91ffa8d2bbc33b732f1fc51943b
Seung-Zin Nam <zin@mailbox.org>
2019-01-18 11:38:42 +0100

    update scalajs deps (#806)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,10 +50,10 @@ object Dependencies {
     Seq(
       "org.scala-js"  %%% "scalajs-dom"     % "0.9.6",
       "com.tgf.pizza" %%% "scalajs-mithril" % "1.1.5",
-      "be.doeraene"   %%% "scalajs-jquery"  % "0.9.3",
+      "be.doeraene"   %%% "scalajs-jquery"  % "0.9.4",
       "org.querki"    %%% "jquery-facade"   % "1.2",
       "org.typelevel" %%% "cats-core"       % catsV,
-      "com.lihaoyi"   %%% "upickle"         % "0.6.5"
+      "com.lihaoyi"   %%% "upickle"         % "0.6.7"
     )
   )
 
