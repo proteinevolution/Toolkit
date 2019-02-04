67f657300d914982dfc78e5a3e06279c5af92bd3
Felix <felix.gabler98@gmail.com>
2019-02-04 16:50:43 +0100

    Refactor tel

--- app/modules/TELEnvProvider.scala
+++ app/modules/TELEnvProvider.scala
@@ -1,32 +1,15 @@
 package modules
 
-import javax.inject.{ Inject, Provider }
-import better.files._
-import de.proteinevolution.tel.env.{ ExecFile, PropFile, TELEnv }
-import play.api.{ Configuration, Logger }
+import de.proteinevolution.tel.env.Env
+import javax.inject.{Inject, Provider}
+import play.api.Configuration
 
-class TELEnvProvider @Inject()(tv: TELEnv, configuration: Configuration) extends Provider[TELEnv] {
+import scala.collection.mutable
 
-  private val logger = Logger(this.getClass)
+class TELEnvProvider @Inject()(configuration: Configuration) extends Provider[Env] {
 
-  override def get(): TELEnv = {
-    configuration
-      .get[Option[String]]("tel.env")
-      .getOrElse {
-        val fallBackFile = "tel/env"
-        logger.warn(s"Key 'tel.env' was not found in configuration. Fall back to '$fallBackFile'")
-        fallBackFile
-      }
-      .toFile
-      .list
-      .foreach { file =>
-        file.extension match {
-          case Some(".prop") => new PropFile(file.pathAsString, configuration).addObserver(tv)
-          case Some(".sh")   => new ExecFile(file.pathAsString).addObserver(tv)
-          case _             => ()
-        }
-      }
-    tv
+  override def get(): Env = {
+    mutable.Map(configuration.get[Map[String, String]]("tel.env_vars").toSeq: _*)
   }
 
 }
