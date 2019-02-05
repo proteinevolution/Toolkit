7c31fa7020e93f907bcdd930678ef673a5da81d7
Felix <felix.gabler98@gmail.com>
2019-02-05 21:40:36 +0100

    add missing imports

--- app/modules/EnvProvider.scala
+++ app/modules/EnvProvider.scala
@@ -1,10 +1,14 @@
 package modules
 
+import de.proteinevolution.tel.env.Env
+import javax.inject.{Inject, Provider}
+import play.api.Configuration
+
 import scala.collection.mutable
 
 class EnvProvider @Inject()(configuration: Configuration) extends Provider[Env] {
 
-  val m = mutable.Map(configuration.get[Map[String, String]]("tel.env_vars").toSeq: _*)
+  private val m = mutable.Map(configuration.get[Map[String, String]]("tel.env_vars").toSeq: _*)
 
   override def get(): Env = new Env {
     def get(key: String): String = m.getOrElse(key, "")
