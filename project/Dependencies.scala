0977d5d621019395f925f06a2cc6f7a8025c0d07
Seung-Zin Nam <zin@mailbox.org>
2019-02-11 13:04:21 +0100

    update scalatest (#822)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
   lazy val testDeps: Seq[ModuleID] = Seq(
     "com.typesafe.akka"        %% "akka-testkit"        % akkaVersion % Test,
     "com.typesafe.akka"        %% "akka-stream-testkit" % akkaVersion % Test,
-    "org.scalatestplus.play"   %% "scalatestplus-play"  % "3.1.2"     % Test,
+    "org.scalatestplus.play"   %% "scalatestplus-play"  % "4.0.1"     % Test,
     "org.mockito"              % "mockito-core"         % "2.11.0"    % Test,
     "com.softwaremill.macwire" %% "macros"              % "2.3.1"     % Test,
     "org.awaitility"           % "awaitility"           % "3.0.0"     % Test
