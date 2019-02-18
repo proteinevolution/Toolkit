15fec1401fbf423dafa5d0bfac9f22c3013e6e73
Seung-Zin Nam <z.nam@envisia.de>
2019-02-18 11:20:39 +0100

    simplify datatables query matching

--- modules/results/src/main/scala/de/proteinevolution/results/services/DTService.scala
+++ modules/results/src/main/scala/de/proteinevolution/results/services/DTService.scala
@@ -5,8 +5,8 @@ import de.proteinevolution.results.results.{ Accession, HSP, SearchResult }
 
 trait DTService {
 
-  def getHitsByKeyWord[T <: HSP](hits: SearchResult[T], params: DTParam)(implicit accession: Accession[T]): List[T] = {
-    val hitList = hits.hitsOrderBy(params)
+  def getHitsByKeyWord[T <: HSP](hits: SearchResult[_], params: DTParam)(implicit accession: Accession[T]): List[T] = {
+    val hitList = hits.asInstanceOf[SearchResult[T]].hitsOrderBy(params)
     if (params.searchValue.length > 0) {
       hitList.filter { hit =>
         (hit.description + accession.value(hit)).toUpperCase.contains(params.searchValue.toUpperCase)
