fb7bc7f29772ad8ceaccf21455e703e5b4d32e71
Felix <felix.gabler98@gmail.com>
2019-08-08 16:28:56 +0200

    implement language dropdown but hide it for now

--- frontend/src/i18n/lang/de.ts
+++ frontend/src/i18n/lang/de.ts
@@ -1,5 +1,22 @@
 export default {
     de: {
-
+        back: 'Zurück',
+        cancel: 'Abbrechen',
+        submit: 'Abschicken',
+        loading: 'Lädt...',
+        fetching: 'Fetching...',
+        reconnecting: 'Verbindungsaufbau...',
+        maintenanceWarning: 'Wartungsarbeiten stehen bevor!',
+        language: {
+            lang: 'Sprache',
+            de: 'Deutsch',
+            en: 'English',
+        },
+        index: {
+            welcomeTitle: 'Willkommen beim Bioinformatik Toolkit',
+            welcomeBody: 'des Max Planck Instituts für Entwicklungsbiologie in Tübingen, Deutschland.',
+            loadBarLabel: 'CLuster-Last: {load}%',
+            searchPlaceholder: 'Suche nach Job ID oder Name des Tools',
+        },
     },
 };
