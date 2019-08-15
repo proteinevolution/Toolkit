0ccf3429d8cbc0baad76cd9b528b1782b87704e5
vikramalva <vikram.alva@gmail.com>
2019-08-15 12:34:09 +0200

    handle clean up in the perl script

--- tel/runscripts/hhpred_manual.sh
+++ tel/runscripts/hhpred_manual.sh
@@ -9,15 +9,7 @@ checkTemplates.pl -i   ../params/%parentid.content.hhr \
                   -pir ../results/tomodel.pir \
                   -cif %CIFALL \
                   -o   $(readlink -f ../results/cif) \
-                  -m   %templates.content  >   ../results/results.out
+                  -m   %templates.content \
+                  -l ../results/results.out
 
 echo "done" >> ../results/process.log
-
-# Remove line which reveals a path from the result
-sed -i '/create/d' $(readlink -f ../results/results.out)
-
-# Remove whitespace lines and whitespace at line start
-sed -i 's/^\s+//' $(readlink -f ../results/tomodel.pir)
-sed -i '/^\s+$/d' $(readlink -f ../results/tomodel.pir)
-
-
