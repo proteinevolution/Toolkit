c570d7b44ffa8c524d94015bec72cd32bf5a5860
Vikram Alva <vikram.alva@gmail.com>
2019-09-20 18:46:30 +0200

    tslint

--- frontend/src/i18n/lang/extras/modals/toolHelp/hhpred_manual.ts
+++ frontend/src/i18n/lang/extras/modals/toolHelp/hhpred_manual.ts
@@ -4,13 +4,13 @@ export default {
     en: {
         toolHelpModals: {
             hhpred_manual: {
-                overview: `HHpred-TemplateSelection generates a multiple sequence alignment in PIR format containing 
-                hits forwarded from an HHpred run. The generated PIR alignment can be forwarded to MODELLER for the 
+                overview: `HHpred-TemplateSelection generates a multiple sequence alignment in PIR format containing
+                hits forwarded from an HHpred run. The generated PIR alignment can be forwarded to MODELLER for the
                 calculation of a structural model.`,
                 parameters: [
                     {
                         title: 'Input',
-                        content: `HHpred-TemplateSelection accepts an HHR file produced by HHpred as input and converts 
+                        content: `HHpred-TemplateSelection accepts an HHR file produced by HHpred as input and converts
                         the selected hits into a PIR alignment.`,
                     },
                 ],
