72ee147a6b140ff7621749585f3981db1b8992e8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-17 17:39:41 +0100

    Update macros to 2.3.3 (#999)
    Co-authored-by: Seung-Zin Nam <zin@mailbox.org>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
     "com.typesafe.akka"        %% "akka-stream-testkit" % akkaVersion % Test,
     "org.scalatestplus.play"   %% "scalatestplus-play"  % "4.0.1"     % Test,
     "org.mockito"              % "mockito-core"         % "2.28.2"    % Test,
-    "com.softwaremill.macwire" %% "macros"              % "2.3.1"     % Test,
+    "com.softwaremill.macwire" %% "macros"              % "2.3.3"     % Test,
     "org.awaitility"           % "awaitility"           % "3.1.6"     % Test
   )
 
