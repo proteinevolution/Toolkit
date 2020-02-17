8ab7749bf37b3abab6130639faf272f9fbe11c1c
Scala Steward <me@scala-steward.org>
2020-02-17 19:06:38 +0100

    Update play-mailer-guice to 8.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     "com.typesafe.akka"    %% "akka-slf4j"           % akkaVersion,
     "com.typesafe.akka"    %% "akka-stream"          % akkaVersion,
     "com.typesafe.play"    %% "play-mailer"          % "8.0.0",
-    "com.typesafe.play"    %% "play-mailer-guice"    % "7.0.1",
+    "com.typesafe.play"    %% "play-mailer-guice"    % "8.0.0",
     "com.github.pathikrit" %% "better-files"         % "3.8.0",
     "org.mindrot"          % "jbcrypt"               % "0.4",
     ("org.reactivemongo" %% "play2-reactivemongo" % "0.16.2-play27")
