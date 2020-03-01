6fb094467b1e9d5b593c33ee5e88e26c19755922
Scala Steward <me@scala-steward.org>
2020-03-01 22:48:28 +0100

    Update cats-effect to 2.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
       .exclude("com.typesafe.akka", "*") // provided
       .exclude("com.typesafe.play", "*"), // provided
     "org.typelevel" %% "cats-core"            % catsV,
-    "org.typelevel" %% "cats-effect"          % "1.4.0",
+    "org.typelevel" %% "cats-effect"          % "2.1.2",
     "com.chuusai"   %% "shapeless"            % "2.3.3",
     "com.mohiva"    %% "play-html-compressor" % "0.7.1",
     "com.dripower"  %% "play-circe"           % "2711.0",
