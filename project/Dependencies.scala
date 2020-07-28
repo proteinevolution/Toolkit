b2b0e32dbfd1c44b0e657bf1b712a2f7213f0499
Felix Gabler <felix.gabler98@gmail.com>
2020-07-28 23:39:58 +0200

    scalafmt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,33 +21,33 @@ object Dependencies {
     "com.typesafe.play"    %% "play-mailer"          % "8.0.1",
     "com.typesafe.play"    %% "play-mailer-guice"    % "8.0.1",
     "com.github.pathikrit" %% "better-files"         % "3.9.1",
-    "org.mindrot"          % "jbcrypt"               % "0.4",
-    ("org.reactivemongo" %% "play2-reactivemongo" % "0.20.12-play27")
+    "org.mindrot"           % "jbcrypt"              % "0.4",
+    ("org.reactivemongo"   %% "play2-reactivemongo"  % "0.20.12-play27")
       .exclude("com.typesafe.akka", "*")  // provided
       .exclude("com.typesafe.play", "*"), // provided
     ("org.reactivemongo" %% "reactivemongo-akkastream" % "0.20.12")
       .exclude("com.typesafe.akka", "*")  // provided
       .exclude("com.typesafe.play", "*"), // provided
-    "org.typelevel" %% "cats-core"            % catsV,
-    "org.typelevel" %% "cats-effect"          % "2.1.4",
-    "com.chuusai"   %% "shapeless"            % "2.3.3",
-    "com.mohiva"    %% "play-html-compressor" % "0.7.1",
-    "com.dripower"  %% "play-circe"           % "2712.0",
-    "io.circe"      %% "circe-generic"        % circeV,
-    "io.circe"      %% "circe-generic-extras" % circeV,
-    ("io.circe" %% "circe-bson" % "0.4.0").exclude("org.reactivemongo", "*"), // provided by play2-reactivemongo
-    "org.typelevel"        %% "simulacrum"    % "1.0.0",
-    "com.github.mongobee"  % "mongobee"       % "0.13",
-    "com.maxmind.geoip2"   % "geoip2"         % "2.13.0"
+    "org.typelevel"      %% "cats-core"            % catsV,
+    "org.typelevel"      %% "cats-effect"          % "2.1.4",
+    "com.chuusai"        %% "shapeless"            % "2.3.3",
+    "com.mohiva"         %% "play-html-compressor" % "0.7.1",
+    "com.dripower"       %% "play-circe"           % "2712.0",
+    "io.circe"           %% "circe-generic"        % circeV,
+    "io.circe"           %% "circe-generic-extras" % circeV,
+    ("io.circe"          %% "circe-bson"           % "0.4.0").exclude("org.reactivemongo", "*"), // provided by play2-reactivemongo
+    "org.typelevel"      %% "simulacrum"           % "1.0.0",
+    "com.github.mongobee" % "mongobee"             % "0.13",
+    "com.maxmind.geoip2"  % "geoip2"               % "2.13.0"
   )
 
   lazy val testDeps: Seq[ModuleID] = Seq(
     "com.typesafe.akka"        %% "akka-testkit"        % akkaVersion % Test,
     "com.typesafe.akka"        %% "akka-stream-testkit" % akkaVersion % Test,
     "org.scalatestplus.play"   %% "scalatestplus-play"  % "5.1.0"     % Test,
-    "org.mockito"              % "mockito-core"         % "3.4.4"     % Test,
+    "org.mockito"               % "mockito-core"        % "3.4.4"     % Test,
     "com.softwaremill.macwire" %% "macros"              % "2.3.7"     % Test,
-    "org.awaitility"           % "awaitility"           % "4.0.3"     % Test
+    "org.awaitility"            % "awaitility"          % "4.0.3"     % Test
   )
 
 }
