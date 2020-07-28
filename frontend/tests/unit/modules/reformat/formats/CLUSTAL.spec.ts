e81a18077724db3aa721ea5ead988e879747b3d1
Felix Gabler <felix.gabler98@gmail.com>
2020-07-29 00:15:37 +0200

    #1097 fix lint warnings

--- frontend/tests/unit/modules/reformat/formats/CLUSTAL.spec.ts
+++ frontend/tests/unit/modules/reformat/formats/CLUSTAL.spec.ts
@@ -1,20 +1,20 @@
-import {expect} from 'chai';
-import {CLUSTAL} from '@/modules/reformat/formats/CLUSTAL';
-
-const example1: string = `
-CLUSTAL W (1.82) multiple sequence alignment
-
-
-FOSB_MOUSE      MFQAFPGDYDSGSRCSSSPSAESQYLSSVDSFGSPPTAAASQECAGLGEMPGSFVPTVTA 60
-FOSB_HUMAN      MFQAFPGDYDSGSRCSSSPSAESQYLSSVDSFGSPPTAAASQECAGLGEMPGSFVPTVTA 60
-                ************************************************************
-
-FOSB_MOUSE      ITTSQDLQWLVQPTLISSMAQSQGQPLASQPPAVDPYDMPGTSYSTPGLSAYSTGGASGS 120
-FOSB_HUMAN      ITTSQDLQWLVQPTLISSMAQSQGQPLASQPPVVDPYDMPGTSYSTPGMSGYSSGGASGS 120
-                ********************************.***************:*.**:******
-
-FOSB_MOUSE      GGPSTSTTTSGPVSARPARARPRRPREETLTPEEEEKRRVRRERNKLAAAKCRNRRRELT 180
-FOSB_HUMAN      GGPSTSGTTSGPGPARPARARPRRPREETLTPEEEEKRRVRRERNKLAAAKCRNRRRELT 180
-                ****** ***** .**********************************************
-`;
+// import {expect} from 'chai';
+// import {CLUSTAL} from '@/modules/reformat/formats/CLUSTAL';
+//
+// const example1: string = `
+// CLUSTAL W (1.82) multiple sequence alignment
+//
+//
+// FOSB_MOUSE      MFQAFPGDYDSGSRCSSSPSAESQYLSSVDSFGSPPTAAASQECAGLGEMPGSFVPTVTA 60
+// FOSB_HUMAN      MFQAFPGDYDSGSRCSSSPSAESQYLSSVDSFGSPPTAAASQECAGLGEMPGSFVPTVTA 60
+//                 ************************************************************
+//
+// FOSB_MOUSE      ITTSQDLQWLVQPTLISSMAQSQGQPLASQPPAVDPYDMPGTSYSTPGLSAYSTGGASGS 120
+// FOSB_HUMAN      ITTSQDLQWLVQPTLISSMAQSQGQPLASQPPVVDPYDMPGTSYSTPGMSGYSSGGASGS 120
+//                 ********************************.***************:*.**:******
+//
+// FOSB_MOUSE      GGPSTSTTTSGPVSARPARARPRRPREETLTPEEEEKRRVRRERNKLAAAKCRNRRRELT 180
+// FOSB_HUMAN      GGPSTSGTTSGPGPARPARARPRRPREETLTPEEEEKRRVRRERNKLAAAKCRNRRRELT 180
+//                 ****** ***** .**********************************************
+// `;
 
