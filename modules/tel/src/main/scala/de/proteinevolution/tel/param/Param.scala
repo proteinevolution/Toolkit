ef803ae254648c12bdd92c0371ae4d07f680ad9c
Felix Gabler <felix.gabler98@gmail.com>
2021-04-03 00:54:46 -0700

    Reload Params on File Change (#1196)
    * WIP listen to paramspec files and reload tools conf
    
    * finish watching for param file changes
    
    Co-authored-by: Felix Gabler <felix.gabler98@gmail.com>
    
    * use implicit execution context
    
    * scalafmt
    
    Co-authored-by: Vikram Alva <vikram.alva@gmail.com>

--- modules/tel/src/main/scala/de/proteinevolution/tel/param/Param.scala
+++ modules/tel/src/main/scala/de/proteinevolution/tel/param/Param.scala
@@ -25,11 +25,11 @@ abstract class Param(val name: String) extends TELRegex
 abstract class PredicativeParam(name: String) extends Param(name) {
 
   /**
-    * Decides whether or not the value for this parameter is allowed
-    *
-    * @param value The value that should be tested for its validity
-    * @return Whether the provided value is valid for this parameter
-    */
+   * Decides whether or not the value for this parameter is allowed
+   *
+   * @param value The value that should be tested for its validity
+   * @return Whether the provided value is valid for this parameter
+   */
   def validate(value: String): Boolean
 }
 
@@ -40,4 +40,5 @@ abstract class GenerativeParam(name: String) extends PredicativeParam(name) {
 
   def generate: ListMap[String, String]
 
+  def load(): Unit
 }
