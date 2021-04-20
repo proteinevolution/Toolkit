f43be1077c0bfa46a1fba642cd5b14c226aa4793
joeldomke <joeldomke@aol.de>
2021-04-20 13:11:09 +0200

    Move all tour related strings to i18n file

--- frontend/src/mixins/TourMixin.ts
+++ frontend/src/mixins/TourMixin.ts
@@ -12,7 +12,7 @@ const TourMixin = Vue.extend({
                     header: {
                         title: 'Toolbar',
                     },
-                    content: this.$t('tour.toolBar'),
+                    content: this.$t('tour.content.toolBar'),
                     params: {
                         enableScrolling: false,
                     },
@@ -29,7 +29,7 @@ const TourMixin = Vue.extend({
                     // the image is loaded. The image then pushes the search bar down, away from
                     // the tour element
                     target: '[data-v-step="search-bar"]',
-                    content: this.$t('tour.searchBar'),
+                    content: this.$t('tour.content.searchBar'),
                     params: {
                         enableScrolling: false,
                     },
@@ -49,14 +49,14 @@ const TourMixin = Vue.extend({
                     // TODO: make sure that the "Search" Tab is selected. Otherwise
                     // the tooltip won't show the HHpred tool
                     target: '[data-v-step="tool"]',
-                    content: this.$t('tour.tool'),
+                    content: this.$t('tour.content.tool'),
                     params: {
                         enableScrolling: false,
                     },
                 },
                 {
                     target: '[data-v-step="input"]',
-                    content: this.$t('tour.input'),
+                    content: this.$t('tour.content.input'),
                     params: {
                         enableScrolling: false,
                         placement: 'top'
@@ -78,7 +78,7 @@ const TourMixin = Vue.extend({
                 },
                 {
                     target: '[data-v-step="paste"]',
-                    content: this.$t('tour.paste'),
+                    content: this.$t('tour.content.paste'),
                     params: {
                         placement: 'left',
                     },
@@ -91,7 +91,7 @@ const TourMixin = Vue.extend({
                 },
                 {
                     target: '[data-v-step="structural-domain-database"]',
-                    content: this.$t('tour.structuralDomainDatabase'),
+                    content: this.$t('tour.content.structuralDomainDatabase'),
                     params: {
                         placement: 'top',
                     },
@@ -104,14 +104,14 @@ const TourMixin = Vue.extend({
                 },
                 {
                     target: '[data-v-step="proteomes"]',
-                    content: this.$t('tour.proteomes'),
+                    content: this.$t('tour.content.proteomes'),
                     params: {
                         placement: 'top',
                     },
                 },
                 {
                     target: '.tour-tab-Parameters',
-                    content: this.$t('tour.parametersTab'),
+                    content: this.$t('tour.content.parametersTab'),
                     params: {
                         placement: 'right',
                     },
@@ -120,20 +120,28 @@ const TourMixin = Vue.extend({
                     // since every tab has its own buttons, we have to select the ones in
                     // the active tab
                     target: '.tab-pane[aria-hidden=false] [data-v-step="job-id"]',
-                    content: this.$t('tour.jobId'),
+                    content: this.$t('tour.content.jobId'),
                 },
                 {
                     target: '.tab-pane[aria-hidden=false] [data-v-step="submit"]',
-                    content: this.$t('tour.submit'),
+                    content: this.$t('tour.content.submit'),
                 },
                 {
                     target: '[data-v-step="job-manager"]',
-                    content: this.$t('tour.jobManager'),
+                    content: this.$t('tour.content.jobManager'),
                     params: {
                         placement: 'right'
                     }
                 },
             ],
+            options: {
+                labels: {
+                    buttonSkip: this.$t('tour.labels.buttonSkip'),
+                    buttonPrevious: this.$t('tour.labels.buttonPrevious'),
+                    buttonNext: this.$t('tour.labels.buttonNext'),
+                    buttonStop: this.$t('tour.labels.buttonStop'),
+                }
+            }
         };
     },
     computed: {
