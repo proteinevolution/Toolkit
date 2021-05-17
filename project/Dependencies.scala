4b3ccce17e502bbfe7bbed86ac264956d1cc1c82
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-17 14:33:12 +0200

    Update reactivemongo-akkastream to 1.0.4 (#1234)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
     ("org.reactivemongo"   %% "play2-reactivemongo"        % "1.0.4-play28")
       .exclude("com.typesafe.akka", "*")  // provided
       .exclude("com.typesafe.play", "*"), // provided
-    ("org.reactivemongo" %% "reactivemongo-akkastream" % "1.0.3")
+    ("org.reactivemongo" %% "reactivemongo-akkastream" % "1.0.4")
       .exclude("com.typesafe.akka", "*")  // provided
       .exclude("com.typesafe.play", "*"), // provided
     "org.typelevel"       %% "cats-core"            % "2.6.1",
