0be46dd19edafe9cb3f09c8da8f5b3ab8dd0b6ef
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-07-31 10:26:42 +0200

    Update cats-effect to 3.2.1 (#1268)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
       .exclude("com.typesafe.akka", "*")  // provided
       .exclude("com.typesafe.play", "*"), // provided
     "org.typelevel"       %% "cats-core"            % "2.6.1",
-    "org.typelevel"       %% "cats-effect"          % "3.2.0",
+    "org.typelevel"       %% "cats-effect"          % "3.2.1",
     "co.fs2"              %% "fs2-core"             % "3.0.6",
     "co.fs2"              %% "fs2-io"               % "3.0.6",
     "com.chuusai"         %% "shapeless"            % "2.3.7",
