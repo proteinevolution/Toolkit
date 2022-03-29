180e13509588eabc460f100b5b2331e097af26d9
Scala Steward <me@scala-steward.org>
2022-03-30 00:53:31 +0200

    Update fs2-core, fs2-io to 3.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,8 +31,8 @@ object Dependencies {
       .exclude("com.typesafe.play", "*"), // provided
     "org.typelevel" %% "cats-core"            % "2.7.0",
     "org.typelevel" %% "cats-effect"          % "3.3.9",
-    "co.fs2"        %% "fs2-core"             % "3.2.5",
-    "co.fs2"        %% "fs2-io"               % "3.2.5",
+    "co.fs2"        %% "fs2-core"             % "3.2.6",
+    "co.fs2"        %% "fs2-io"               % "3.2.6",
     "com.chuusai"   %% "shapeless"            % "2.3.9",
     "com.dripower"  %% "play-circe"           % "2814.2",
     "io.circe"      %% "circe-generic"        % circeV,
