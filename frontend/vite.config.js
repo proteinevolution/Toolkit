acd914cc88bbc1bc001e85a183d0d319ed5816cf
Felix Gabler <felix.gabler98@gmail.com>
2022-07-30 13:22:51 +0200

    refactor: correctly import path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import {defineConfig, loadEnv} from 'vite';
 import vue from '@vitejs/plugin-vue2';
 import viteImagemin from 'vite-plugin-imagemin';
-/* eslint-disable @typescript-eslint/no-var-requires */
-// eslint-disable-next-line no-undef
-const path = require("path");
-
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
